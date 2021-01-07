359326e638854456a9325f5f1e875d2a75adb24a
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-07 13:06:17 +0100

    Add files via upload

--- script/c53790572.lua
+++ script/c53790572.lua
@@ -1,5 +1,6 @@
---真青眼の究極竜
-function c53790572.initial_effect(c)
+--Dunkeläugiger Ultimativer Wyrm
+local s,id=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,true,true,53790570,3)
@@ -7,10 +8,10 @@ function c53790572.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_DAMAGE_STEP_END)
-	e1:SetCountLimit(2,53790572)
-	e1:SetCondition(c53790572.atcon)
-	e1:SetCost(c53790572.atcost)
-	e1:SetOperation(c53790572.atop)
+	e1:SetCountLimit(2,id)
+	e1:SetCondition(s.atcon)
+	e1:SetCost(s.atcost)
+	e1:SetOperation(s.atop)
 	c:RegisterEffect(e1)
 	--Negate
 	local e2=Effect.CreateEffect(c)
@@ -19,51 +20,47 @@ function c53790572.initial_effect(c)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e2:SetCondition(c53790572.condition)
-	e2:SetCost(c53790572.cost)
-	e2:SetTarget(c53790572.target)
-	e2:SetOperation(c53790572.operation)
+	e2:SetCondition(s.condition)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
-function c53790572.costfilter(c)
+function s.costfilter(c)
 	return c:IsSetCard(0x12BE) and c:IsType(TYPE_FUSION) and c:IsAbleToGraveAsCost()
 end
-function c53790572.atcon(e,tp,eg,ep,ev,re,r,rp)
+function s.atcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return bit.band(c:GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
-		and Duel.GetAttacker()==c and c:IsChainAttackable(0)
+	return c:IsSummonType(SUMMON_TYPE_FUSION)
+		and Duel.GetAttacker()==c and c:CanChainAttack(0)
 		and not Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,c)
 end
-function c53790572.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c53790572.costfilter,tp,LOCATION_EXTRA,0,1,nil) end
+function s.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_EXTRA,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c53790572.costfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
 end
-function c53790572.atop(e,tp,eg,ep,ev,re,r,rp)
+function s.atop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChainAttack()
 end
-function c53790572.filter(c,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsSetCard(0x12BE)
+function s.filter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsSetCard(0xdd)
 end
-function c53790572.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return g and g:IsExists(c53790572.filter,1,nil,tp)
+	return g and g:IsExists(s.filter,1,nil,tp)
 		and Duel.IsChainNegatable(ev)
 end
-function c53790572.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
-end
-function c53790572.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function c53790572.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
