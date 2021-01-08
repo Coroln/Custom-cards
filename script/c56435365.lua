e8dec186437e95ef25e0000742e6c5090ebdbee1
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-08 11:46:15 +0100

    Add files via upload

--- script/c56435365.lua
+++ script/c56435365.lua
@@ -1,5 +1,6 @@
-
-function c56435365.initial_effect(c)
+--Ein Reich im Himmel
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
