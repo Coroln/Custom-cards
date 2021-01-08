07ad33cafd00a1ab56c40a8606d86a8494642178
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-08 10:54:05 +0100

    Add files via upload

--- script/c53790708.lua
+++ script/c53790708.lua
@@ -1,47 +1,48 @@
 --Demonking Naraku
-function c53790708.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--synchro summon
-	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x5EB),1,1,Synchro.NonTunerEx(Card.IsSetCard,0x5EB),1,99)
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x5EB),aux.NonTuner(Card.IsSetCard,0x5EB),1)
 	c:EnableReviveLimit()
 	--negate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(53790708,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c53790708.condition)
-	e1:SetTarget(c53790708.target)
-	e1:SetOperation(c53790708.operation)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--(3) Gain ATK/DEF
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(53790708,1))
+    e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_BATTLE_DESTROYED)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCondition(c53790708.atkcon)
-    e2:SetTarget(c53790708.atktg)
-    e2:SetOperation(c53790708.atkop)
+    e2:SetCondition(s.atkcon1)
+    e2:SetTarget(s.atktg1)
+    e2:SetOperation(s.atkop1)
     c:RegisterEffect(e2)
 	--
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
 	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e3:SetCondition(c53790708.atkcon)
-	e3:SetOperation(c53790708.atkop)
+	e3:SetCondition(s.atkcon)
+	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
 end
-function c53790708.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=re:GetHandler()
 	return re:IsActiveType(TYPE_MONSTER) and rc~=c and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
-function c53790708.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
@@ -49,7 +50,7 @@ function c53790708.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 --(3) Gain ATK/DEF
-function c53790708.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function s.atkcon1(e,tp,eg,ep,ev,re,r,rp)
   local des=eg:GetFirst()
   local rc=des:GetReasonCard()
   if des:IsType(TYPE_XYZ) then
@@ -61,12 +62,12 @@ function c53790708.atkcon(e,tp,eg,ep,ev,re,r,rp)
   end
   return rc and rc:IsSetCard(0x5EB) and rc:IsControler(tp) and rc:IsRelateToBattle() and des:IsReason(REASON_BATTLE) 
 end
-function c53790708.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.atktg1(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then return true end
   Duel.Hint(HINT_OPSELECTED,tp,e:GetDescription())
   Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end
-function c53790707.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop1(e,tp,eg,ep,ev,re,r,rp)
   local c=e:GetHandler()
   if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
   local e1=Effect.CreateEffect(c)
@@ -79,7 +80,7 @@ function c53790707.atkop(e,tp,eg,ep,ev,re,r,rp)
   e2:SetCode(EFFECT_UPDATE_DEFENSE)
   c:RegisterEffect(e2)
 end
-function c53790708.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=re:GetHandler()
 	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) and Duel.Destroy(rc,REASON_EFFECT)~=0 and rc:GetBaseAttack()>=0
@@ -92,12 +93,12 @@ function c53790708.operation(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1)
 	end
 end
-function c53790708.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
 	return bc and bc:IsLevelAbove(5) and bc:IsControler(1-tp)
 end
-function c53790708.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
 	if c:IsRelateToBattle() and c:IsFaceup() and bc:IsRelateToBattle() and bc:IsFaceup() then
