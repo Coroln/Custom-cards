f78c1f008310a8834df0c7bd1a92c6b1711f88ee
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-14 14:03:16 +0100

    Add files via upload

--- script/All Rising Phoenix scripts/c100001185.lua
+++ script/All Rising Phoenix scripts/c100001185.lua
@@ -1,27 +1,28 @@
 --sccripted and created by rising phoenix
-function c100001185.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e11=Effect.CreateEffect(c)
-	e11:SetDescription(aux.Stringid(100001185,0))
+	e11:SetDescription(aux.Stringid(id,0))
 	e11:SetType(EFFECT_TYPE_IGNITION)
 	e11:SetRange(LOCATION_MZONE)
 	e11:SetCountLimit(1)
-	e11:SetTarget(c100001185.target)
-	e11:SetOperation(c100001185.activate)
+	e11:SetTarget(s.target)
+	e11:SetOperation(s.activate)
 	c:RegisterEffect(e11)
-		--summon with 3 tribute
+	--summon with 3 tribute
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
-	e1:SetCondition(c100001185.ttcon)
-	e1:SetOperation(c100001185.ttop)
+	e1:SetCondition(s.ttcon)
+	e1:SetOperation(s.ttop)
 	e1:SetValue(SUMMON_TYPE_TRIBUTE)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_LIMIT_SET_PROC)
-	e2:SetCondition(c100001185.setcon)
+	e2:SetCondition(s.setcon)
 	c:RegisterEffect(e2)
 	--summon
 	local e3=Effect.CreateEffect(c)
@@ -33,7 +34,7 @@ function c100001185.initial_effect(c)
 	local e6=Effect.CreateEffect(c)
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e6:SetCode(EVENT_SUMMON_SUCCESS)
-	e6:SetOperation(c100001185.sumsuc)
+	e6:SetOperation(s.sumsuc)
 	c:RegisterEffect(e6)
 	--cannot special summon
 	local e5=Effect.CreateEffect(c)
@@ -41,85 +42,93 @@ function c100001185.initial_effect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
 	e5:SetCode(EFFECT_SPSUMMON_CONDITION)
 	c:RegisterEffect(e5)
-		--search
+	--search
 	local e4=Effect.CreateEffect(c)
 	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_LEAVE_FIELD)
-	e4:SetCondition(c100001185.conditiongr)
-	e4:SetTarget(c100001185.targetgr)
-	e4:SetOperation(c100001185.operationgr)
+	e4:SetCondition(s.conditiongr)
+	e4:SetTarget(s.targetgr)
+	e4:SetOperation(s.operationgr)
 	c:RegisterEffect(e4)
-		--tribute check
+	--tribute check
 	local e22=Effect.CreateEffect(c)
 	e22:SetType(EFFECT_TYPE_SINGLE)
 	e22:SetCode(EFFECT_MATERIAL_CHECK)
-	e22:SetValue(c100001185.valcheck)
+	e22:SetValue(s.valcheck)
 	c:RegisterEffect(e22)
 	--give atk effect only when summon
 	local e31=Effect.CreateEffect(c)
 	e31:SetType(EFFECT_TYPE_SINGLE)
 	e31:SetCode(EFFECT_SUMMON_COST)
-	e31:SetOperation(c100001185.facechk)
+	e31:SetOperation(s.facechk)
 	e31:SetLabelObject(e22)
 	c:RegisterEffect(e31)
-		--tribute check
+	--tribute check
 	local e23=Effect.CreateEffect(c)
 	e23:SetType(EFFECT_TYPE_SINGLE)
 	e23:SetCode(EFFECT_MATERIAL_CHECK)
-	e23:SetValue(c100001185.valcheck2)
+	e23:SetValue(s.valcheck2)
 	c:RegisterEffect(e23)
 	--give def effect only when summon
 	local e32=Effect.CreateEffect(c)
 	e32:SetType(EFFECT_TYPE_SINGLE)
 	e32:SetCode(EFFECT_SUMMON_COST)
-	e32:SetOperation(c100001185.facechk2)
+	e32:SetOperation(s.facechk2)
 	e32:SetLabelObject(e23)
 	c:RegisterEffect(e32)
+	--change name
+	local e13=Effect.CreateEffect(c)
+	e13:SetType(EFFECT_TYPE_SINGLE)
+	e13:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e13:SetCode(EFFECT_CHANGE_CODE)
+	e13:SetRange(LOCATION_HAND)
+	e13:SetValue(10000010)
+	c:RegisterEffect(e13)
 end
-function c100001185.filtersss(c)
+function s.filtersss(c)
 	return (c:IsCode(10000040) or c:IsCode(10000010) or c:IsCode(10000090) or c:IsCode(10000080)) and c:IsAbleToHand()
 end
-function c100001185.targetgr(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100001185.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+function s.targetgr(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function c100001185.operationgr(e,tp,eg,ep,ev,re,r,rp)
+function s.operationgr(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100001185.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then end
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 end
-function c100001185.conditiongr(e,tp,eg,ep,ev,re,r,rp)
+function s.conditiongr(e,tp,eg,ep,ev,re,r,rp)
 	return not e:GetHandler():IsReason(REASON_RETURN)
 end
-function c100001185.ttcon(e,c,minc)
+function s.ttcon(e,c,minc)
 	if c==nil then return true end
 	return minc<=3 and Duel.CheckTribute(c,3)
 end
-function c100001185.ttop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.ttop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.SelectTribute(tp,c,3,3)
 	c:SetMaterial(g)
 	Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)
 end
-function c100001185.setcon(e,c,minc)
+function s.setcon(e,c,minc)
 	if not c then return true end
 	return false
 end
-function c100001185.genchainlm(c)
+function s.genchainlm(c)
 	return	function (e,rp,tp)
 				return e:GetHandler()==c
 			end
 end
-function c100001185.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SetChainLimitTillChainEnd(c100001185.genchainlm(e:GetHandler()))
 end
-function c100001185.bantg(e,c)
+function s.bantg(e,c)
 	return c:IsCode(e:GetLabel())
 end
-function c100001185.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,564)
 	local ac=Duel.AnnounceCard(tp)
@@ -127,7 +136,7 @@ function c100001185.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD)
 	Duel.SetChainLimit(aux.FALSE)
 end
-function c100001185.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	e:GetHandler():SetHint(CHINT_CARD,ac)
 	--forbidden
@@ -136,12 +145,12 @@ function c100001185.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	e1:SetCode(EFFECT_FORBIDDEN)
 	e1:SetTargetRange(0,0x7f)
-	e1:SetTarget(c100001185.bantg)
+	e1:SetTarget(s.bantg)
 	e1:SetLabel(ac)
 	e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
 	Duel.RegisterEffect(e1,tp)
 end
-function c100001185.valcheck(e,c)
+function s.valcheck(e,c)
 	local g=c:GetMaterial()
 	local tc=g:GetFirst()
 	local atk=0
@@ -160,13 +169,13 @@ function c100001185.valcheck(e,c)
 		c:RegisterEffect(e1)
 	end
 end
-function c100001185.facechk(e,tp,eg,ep,ev,re,r,rp)
+function s.facechk(e,tp,eg,ep,ev,re,r,rp)
 	e:GetLabelObject():SetLabel(1)
 end
-function c100001185.facechk2(e,tp,eg,ep,ev,re,r,rp)
+function s.facechk2(e,tp,eg,ep,ev,re,r,rp)
 	e:GetLabelObject():SetLabel(1)
 end
-function c100001185.valcheck2(e,c)
+function s.valcheck2(e,c)
 	local g=c:GetMaterial()
 	local tc=g:GetFirst()
 	local def=0
--- script/All Rising Phoenix scripts/c100001186.lua
+++ script/All Rising Phoenix scripts/c100001186.lua
@@ -1,18 +1,19 @@
 --sccripted and created by rising phoenix
-function c100001186.initial_effect(c)
-		--summon with 3 tribute
+local s,id=GetID()
+function s.initial_effect(c)
+	--summon with 3 tribute
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
-	e1:SetCondition(c100001186.ttcon)
-	e1:SetOperation(c100001186.ttop)
+	e1:SetCondition(s.ttcon)
+	e1:SetOperation(s.ttop)
 	e1:SetValue(SUMMON_TYPE_TRIBUTE)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_LIMIT_SET_PROC)
-	e2:SetCondition(c100001186.setcon)
+	e2:SetCondition(s.setcon)
 	c:RegisterEffect(e2)
 	--summon
 	local e3=Effect.CreateEffect(c)
@@ -24,7 +25,7 @@ function c100001186.initial_effect(c)
 	local e6=Effect.CreateEffect(c)
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e6:SetCode(EVENT_SUMMON_SUCCESS)
-	e6:SetOperation(c100001186.sumsuc)
+	e6:SetOperation(s.sumsuc)
 	c:RegisterEffect(e6)
 	--cannot special summon
 	local e5=Effect.CreateEffect(c)
@@ -32,33 +33,41 @@ function c100001186.initial_effect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
 	e5:SetCode(EFFECT_SPSUMMON_CONDITION)
 	c:RegisterEffect(e5)
-		--search
+	--search
 	local e4=Effect.CreateEffect(c)
 	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_LEAVE_FIELD)
-	e4:SetCondition(c100001186.conditiongr)
-	e4:SetTarget(c100001186.targetgr)
-	e4:SetOperation(c100001186.operationgr)
+	e4:SetCondition(s.conditiongr)
+	e4:SetTarget(s.targetgr)
+	e4:SetOperation(s.operationgr)
 	c:RegisterEffect(e4)
-		--negate
+	--negate
 	local e12=Effect.CreateEffect(c)
-	e12:SetDescription(aux.Stringid(100001186,0))
+	e12:SetDescription(aux.Stringid(id,0))
 	e12:SetCategory(CATEGORY_NEGATE+CATEGORY_REMOVE)
 	e12:SetType(EFFECT_TYPE_QUICK_O)
 	e12:SetCode(EVENT_CHAINING)
 	e12:SetCountLimit(1)
 	e12:SetRange(LOCATION_MZONE)
-	e12:SetCondition(c100001186.discon)
-	e12:SetTarget(c100001186.distg)
-	e12:SetOperation(c100001186.disop)
+	e12:SetCondition(s.discon)
+	e12:SetTarget(s.distg)
+	e12:SetOperation(s.disop)
 	c:RegisterEffect(e12)
+	--change name
+	local e13=Effect.CreateEffect(c)
+	e13:SetType(EFFECT_TYPE_SINGLE)
+	e13:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e13:SetCode(EFFECT_CHANGE_CODE)
+	e13:SetRange(LOCATION_HAND)
+	e13:SetValue(10000000)
+	c:RegisterEffect(e13)
 end
-function c100001186.discon(e,tp,eg,ep,ev,re,r,rp)
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
-function c100001186.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsAbleToRemove() and re:GetHandler():IsRelateToEffect(re) then
@@ -66,46 +75,46 @@ function c100001186.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 			Duel.SetChainLimit(aux.FALSE)
 	end
 end
-function c100001186.disop(e,tp,eg,ep,ev,re,r,rp)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateActivation(ev)
 	if re:GetHandler():IsRelateToEffect(re) then end
 		Duel.Remove(eg,POS_FACEDOWN,REASON_EFFECT)
 	end
-function c100001186.filtersss(c)
+function s.filtersss(c)
 	return (c:IsCode(10000040) or c:IsCode(10000000)) and c:IsAbleToHand()
 end
-function c100001186.targetgr(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100001186.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+function s.targetgr(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function c100001186.operationgr(e,tp,eg,ep,ev,re,r,rp)
+function s.operationgr(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100001186.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filtersss,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then end
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 end
-function c100001186.conditiongr(e,tp,eg,ep,ev,re,r,rp)
+function s.conditiongr(e,tp,eg,ep,ev,re,r,rp)
 	return not e:GetHandler():IsReason(REASON_RETURN)
 end
-function c100001186.ttcon(e,c,minc)
+function s.ttcon(e,c,minc)
 	if c==nil then return true end
 	return minc<=3 and Duel.CheckTribute(c,3)
 end
-function c100001186.ttop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.ttop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.SelectTribute(tp,c,3,3)
 	c:SetMaterial(g)
 	Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)
 end
-function c100001186.setcon(e,c,minc)
+function s.setcon(e,c,minc)
 	if not c then return true end
 	return false
 end
-function c100001186.genchainlm(c)
+function s.genchainlm(c)
 	return	function (e,rp,tp)
 				return e:GetHandler()==c
 			end
 end
-function c100001186.sumsuc(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(c100001186.genchainlm(e:GetHandler()))
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(s.genchainlm(e:GetHandler()))
 end
