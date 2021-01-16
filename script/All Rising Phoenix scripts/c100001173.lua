b58ea69b72de65230e973f8cd8e350b2c0214029
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-17 00:02:31 +0100

    Update c100001173.lua

--- script/All Rising Phoenix scripts/c100001173.lua
+++ script/All Rising Phoenix scripts/c100001173.lua
@@ -1,5 +1,6 @@
 --Created and scripted by Rising Phoenix
-function c100001173.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--control
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -7,16 +8,16 @@ function c100001173.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
 	c:RegisterEffect(e1)
-		--splimit
+	--splimit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(1,0)
-	e2:SetTarget(c100001173.splimit)
+	e2:SetTarget(s.splimit)
 	c:RegisterEffect(e2)
-		--cannot release
+	--cannot release
 	local e12=Effect.CreateEffect(c)
 	e12:SetType(EFFECT_TYPE_FIELD)
 	e12:SetRange(LOCATION_MZONE)
@@ -24,31 +25,31 @@ function c100001173.initial_effect(c)
 	e12:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
 	e12:SetTargetRange(1,0)
 	c:RegisterEffect(e12)
-		--cannot special summon
+	--cannot special summon
 	local e11=Effect.CreateEffect(c)
 	e11:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e11:SetType(EFFECT_TYPE_SINGLE)
 	e11:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e11:SetValue(aux.FALSE)
 	c:RegisterEffect(e11)
-		--special summon
+	--special summon
 	local e21=Effect.CreateEffect(c)
-	e21:SetDescription(aux.Stringid(100001173,0))
+	e21:SetDescription(aux.Stringid(id,0))
 	e21:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e21:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e21:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e21:SetCode(EVENT_SUMMON_SUCCESS)
-	e21:SetTarget(c100001173.sptg)
-	e21:SetOperation(c100001173.spop)
+	e21:SetTarget(s.sptg)
+	e21:SetOperation(s.spop)
 	c:RegisterEffect(e21)
 end
-function c100001173.splimit(e,c)
+function s.splimit(e,c)
 	return not (c:IsSetCard(0x75A) or c:IsCode(100001193))
 end
-function c100001173.filter(c,e)
+function s.filter(c,e,tp)
 	return c:IsCanBeSpecialSummoned(e,0,tp,true,false) and c:IsType(TYPE_MONSTER)
 end
-function c100001173.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_DECK) and c100001173.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(c100001173.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
@@ -56,7 +57,7 @@ function c100001173.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,c100001173.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100001173.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then end
 		Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
@@ -65,11 +66,11 @@ function c100001173.spop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c100001173.splimit2)
+	e1:SetTarget(s.splimit2)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	end
-function c100001173.splimit2(e,c,sump,sumtype,sumpos,targetp)
+function s.splimit2(e,c,sump,sumtype,sumpos,targetp)
 	return c:IsLocation(LOCATION_HAND) or c:IsLocation(LOCATION_DECK) or c:IsLocation(LOCATION_GRAVE)
 end
 
