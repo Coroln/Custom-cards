e19d772ad430d4ef17e10254e4f894db7f2c7332
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-20 12:41:26 +0100

    Add files via upload

--- script/c80000012.lua
+++ script/c80000012.lua
@@ -1,36 +1,37 @@
 --Britannia,Königin der Alchemie
-function c80000012.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 --fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcCode3(c,80000005,80000002,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19ba),1,true,true)
+	Fusion.AddProcMix(c,80000005,80000002,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19ba),1,true,true)
 --pendulum summon
 	aux.EnablePendulumAttribute(c)
 --special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(80000012,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetTarget(c80000012.target)
-	e1:SetOperation(c80000012.operation)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--spsummon condition
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e2:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e2:SetValue(c80000012.splimit)
+	e2:SetValue(s.splimit)
 	c:RegisterEffect(e2)
 	--special summon rule
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(80000012,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_SPSUMMON_PROC)
 	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e3:SetRange(LOCATION_EXTRA)
-	e3:SetCondition(c80000012.sprcon)
-	e3:SetOperation(c80000012.sprop)
+	e3:SetCondition(s.sprcon)
+	e3:SetOperation(s.sprop)
 	c:RegisterEffect(e3)
 	--cannot be fusion material
 	local e4=Effect.CreateEffect(c)
@@ -41,69 +42,69 @@ function c80000012.initial_effect(c)
 	c:RegisterEffect(e4)
 	--special summon (extra)
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(80000012,2))
+	e5:SetDescription(aux.Stringid(id,2))
 	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e5:SetType(EFFECT_TYPE_IGNITION)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCountLimit(1)
-	e5:SetTarget(c80000012.esptg)
-	e5:SetOperation(c80000012.espop)
+	e5:SetTarget(s.esptg)
+	e5:SetOperation(s.espop)
 	c:RegisterEffect(e5)
 	--pendulum
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(80000012,3))
+	e6:SetDescription(aux.Stringid(id,3))
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e6:SetCode(EVENT_DESTROYED)
 	e6:SetProperty(EFFECT_FLAG_DELAY)
-	e6:SetCondition(c80000012.pencon)
-	e6:SetTarget(c80000012.pentg)
-	e6:SetOperation(c80000012.penop)
+	e6:SetCondition(s.pencon)
+	e6:SetTarget(s.pentg)
+	e6:SetOperation(s.penop)
 	c:RegisterEffect(e6)
 end
-function c80000012.filter(c,e,sp)
+function s.filter(c,e,sp)
 	return c:IsSetCard(0x19ba) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
 end
-function c80000012.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c80000012.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
-function c80000012.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c80000012.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c80000012.splimit(e,se,sp,st)
+function s.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
-function c80000012.spfilter1(c,tp)
+function s.spfilter1(c,tp)
 	return c:IsFusionCode(80000005) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
-		and Duel.IsExistingMatchingCard(c80000012.spfilter2,tp,LOCATION_MZONE,0,1,c)
+		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_MZONE,0,1,c)
 end
-function c80000012.spfilter2(c,tp)
+function s.spfilter2(c,tp)
 	return c:IsFusionCode(80000002) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
-		and Duel.IsExistingMatchingCard(c80000012.spfilter3,tp,LOCATION_MZONE,0,1,c)
+		and Duel.IsExistingMatchingCard(s.spfilter3,tp,LOCATION_MZONE,0,1,c)
 end
-function c80000012.spfilter3(c)
+function s.spfilter3(c)
 	return c:IsFusionSetCard(0x19ba) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
 end
-function c80000012.sprcon(e,c)
+function s.sprcon(e,c)
 	if c==nil then return true end 
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-3
-		and Duel.IsExistingMatchingCard(c80000012.spfilter1,tp,LOCATION_ONFIELD,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_ONFIELD,0,1,nil,tp)
 end
-function c80000012.sprop(e,tp,eg,ep,ev,re,r,rp,c)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(80000012,1))
-	local g1=Duel.SelectMatchingCard(tp,c80000012.spfilter1,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(80000012,2))
-	local g2=Duel.SelectMatchingCard(tp,c80000012.spfilter2,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(80000012,3))
-	local g3=Duel.SelectMatchingCard(tp,c80000012.spfilter3,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
+function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local g2=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+	local g3=Duel.SelectMatchingCard(tp,s.spfilter3,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
 	g1:Merge(g2)
 	local tc=g1:GetFirst()
 	while tc do
@@ -112,31 +113,31 @@ function c80000012.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	end
 	Duel.SendtoDeck(g1,nil,nil,3,REASON_COST)
 end
-function c80000012.espfilter(c,e,tp)
-	return c:IsSetCard(0x19ba) and c:IsType(TYPE_FUSION) and not c:IsCode(80000012)
+function s.espfilter(c,e,tp)
+	return c:IsSetCard(0x19ba) and c:IsType(TYPE_FUSION) and not c:IsCode(id)
 		and c:IsCanBeSpecialSummoned(e,122,tp,true,false)
 end
-function c80000012.esptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.esptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c80000012.espfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.espfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c80000012.espop(e,tp,eg,ep,ev,re,r,rp)
+function s.espop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c80000012.espfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.espfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,122,tp,tp,true,false,POS_FACEUP)
 	end
 end
-function c80000012.pencon(e,tp,eg,ep,ev,re,r,rp)
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
 end
-function c80000012.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLocation(tp,LOCATION_SZONE,6) or Duel.CheckLocation(tp,LOCATION_SZONE,7) end
 end
-function c80000012.penop(e,tp,eg,ep,ev,re,r,rp)
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.CheckLocation(tp,LOCATION_SZONE,6) and not Duel.CheckLocation(tp,LOCATION_SZONE,7) then return false end
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
--- script/c80000013.lua
+++ script/c80000013.lua
@@ -1,14 +1,15 @@
 --Sterling,Silbergeneral der Alchemie
-function c80000013.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcCodeFun(c,80000003,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19ba),1,true,true)
+	Fusion.AddProcCodeMix(c,80000003,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19ba),1,true,true)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(c80000013.splimit)
+	e1:SetValue(s.splimit)
 	c:RegisterEffect(e1)
 	--special summon rule
 	local e2=Effect.CreateEffect(c)
@@ -16,41 +17,41 @@ function c80000013.initial_effect(c)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_EXTRA)
-	e2:SetCondition(c80000013.sprcon)
-	e2:SetOperation(c80000013.sprop)
+	e2:SetCondition(s.sprcon)
+	e2:SetOperation(s.sprop)
 	c:RegisterEffect(e2)
 	--destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(80000013,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetTarget(c80000013.destg)
-	e3:SetOperation(c80000013.desop)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
 end
-function c80000013.splimit(e,se,sp,st)
+function s.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
-function c80000013.spfilter1(c,tp)
+function s.spfilter1(c,tp)
 	return c:IsFusionCode(80000003) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
-		and Duel.IsExistingMatchingCard(c80000013.spfilter2,tp,LOCATION_MZONE,0,1,c)
+		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_MZONE,0,1,c)
 end
-function c80000013.spfilter2(c)
+function s.spfilter2(c)
 	return c:IsFusionSetCard(0x19ba) and c:IsCanBeFusionMaterial() and c:IsAbleToDeckOrExtraAsCost()
 end
-function c80000013.sprcon(e,c)
+function s.sprcon(e,c)
 	if c==nil then return true end 
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
-		and Duel.IsExistingMatchingCard(c80000013.spfilter1,tp,LOCATION_ONFIELD,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_ONFIELD,0,1,nil,tp)
 end
-function c80000013.sprop(e,tp,eg,ep,ev,re,r,rp,c)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(80000013,2))
-	local g1=Duel.SelectMatchingCard(tp,c80000013.spfilter1,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(80000013,3))
-	local g2=Duel.SelectMatchingCard(tp,c80000013.spfilter2,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
+function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+	local g2=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
 	g1:Merge(g2)
 	local tc=g1:GetFirst()
 	while tc do
@@ -59,14 +60,14 @@ function c80000013.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	end
 	Duel.SendtoDeck(g1,nil,2,REASON_COST)
 end
-function c80000013.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,2,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
-function c80000013.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
 	if tg:GetCount()>0 then
