8785ef55088eb053cd72e60248a33e149ee2c236
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-22 18:31:09 +0100

    Update c80000309.lua

--- script/c80000309.lua
+++ script/c80000309.lua
@@ -1,16 +1,22 @@
 --Noble Hero Jeanne
 local s,id=GetID()
 function s.initial_effect(c)
-    --special summon from hand
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e2:SetCode(EFFECT_SPSUMMON_PROC)
-    e2:SetRange(LOCATION_HAND)
-    e2:SetCondition(s.dircon)
-    e2:SetTarget(s.hsptg)
-    e2:SetOperation(s.hspop)
-    c:RegisterEffect(e2)
+    ----cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
     --atk
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,0))
@@ -36,12 +42,12 @@ function s.initial_effect(c)
     e6:SetValue(s.damlimit)
     c:RegisterEffect(e6)
 end
-function s.dircon(e)
-	return Duel.IsEnvironment(80000315)
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.CheckReleaseGroup(c:GetControler(),Card.IsSetCard,1,false,1,true,c,c:GetControler(),nil,false,nil,0xBDF)
 end
-function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,2,2,false,true,true,c,nil,nil,false,nil,0x8A3)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,false,true,true,c,nil,nil,false,nil,0xBDF)
 	if g then
 		g:KeepAlive()
 		e:SetLabelObject(g)
@@ -49,7 +55,7 @@ function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
 	end
 	return false
 end
-function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.Release(g,REASON_COST)
