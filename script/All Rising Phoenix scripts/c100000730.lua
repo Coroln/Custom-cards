4707121ad815f59394fc42e0c1f6e553d4f49b58
Coroln <76746930+Coroln@users.noreply.github.com>
2021-01-31 15:11:04 +0100

    Update c100000730.lua
    Setcode Fixed

--- script/All Rising Phoenix scripts/c100000730.lua
+++ script/All Rising Phoenix scripts/c100000730.lua
@@ -1,40 +1,42 @@
-function c100000730.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	--Activate
-		local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e2)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-		e1:SetCountLimit(1)
-		e1:SetRange(LOCATION_SZONE)
-	e1:SetTarget(c100000730.target)
-	e1:SetOperation(c100000730.activate)
-	e1:SetCondition(c100000730.dacon)
 	c:RegisterEffect(e1)
+	--Activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
+	e2:SetCondition(s.dacon)
+	c:RegisterEffect(e2)
+	
 end
-function c100000730.filterdi(c)
+function s.filterdi(c)
 	return c:IsFacedown()
 end
-function c100000730.dacon(e)
-	return Duel.IsExistingMatchingCard(c100000730.filterdi,e:GetHandlerPlayer(),0,LOCATION_ONFIELD,1,nil)
+function s.dacon(e)
+	return Duel.IsExistingMatchingCard(s.filterdi,e:GetHandlerPlayer(),0,LOCATION_ONFIELD,1,nil)
 end
-function c100000730.filter(c,e,tp)
-	return c:IsSetCard(0x766) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x76B) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
 end
-function c100000730.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_HAND) and c100000730.filter(chkc,e,tp) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_HAND) and s.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100000730.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100000730.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100000730.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
