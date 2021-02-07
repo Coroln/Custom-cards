8ac8f572324785f1e34b886f3640b4168e143ca4
Coroln <76746930+Coroln@users.noreply.github.com>
2021-02-07 16:20:08 +0100

    Update c99960420.lua
    Link Function fixed

--- script/C.Studio/c99960420.lua
+++ script/C.Studio/c99960420.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
   c:EnableReviveLimit()
   --Link Summon
-  aux.AddLinkProcedure(c,nil,2,nil,s.matcheck)
+  aux.AddLinkProcedure(c,nil,2,nil,s.lcheck)
   --(1) Search
   local e1=Effect.CreateEffect(c)
   e1:SetDescription(aux.Stringid(id,0))
@@ -44,8 +44,8 @@ function s.initial_effect(c)
   c:RegisterEffect(e4)
 end
 --Link Summon
-function s.matcheck(g,lc,tp)
-  return g:IsExists(Card.IsLinkSetCard,1,nil,0x996)
+function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsSetCard,1,nil,0x996,lc,sumtype,tp)
 end
 --(1) Search
 function s.thfilter(c)
