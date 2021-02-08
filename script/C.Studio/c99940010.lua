c45a02f4baf736afd3b282ff6a88e67784339b0c
Coroln <76746930+Coroln@users.noreply.github.com>
2021-02-08 16:35:14 +0100

    Update c99940010.lua

--- script/C.Studio/c99940010.lua
+++ script/C.Studio/c99940010.lua
@@ -1,7 +1,7 @@
 --NGNL Sora
 --Scripted by Raivost
 function c99940010.initial_effect(c)
-  aux.EnablePendulumAttribute(c)
+  Pendulum.AddProcedure(c)
   --Pendulum Effects
   --(1) Scale change
   local e1=Effect.CreateEffect(c)
