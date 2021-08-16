95d08bb943d75f6c1af5bb34243a0def7ae5f75e
Coroln <janpackebusch0077@gmail.com>
2021-08-16 09:55:08 +0200

    Update c80000039.lua

--- script/c80000039.lua
+++ script/c80000039.lua
@@ -1,5 +1,6 @@
 --Daedalus, Ober-Ozeanherrscherin der Meere
-function c80000039.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 --xyz summon
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x1864),4,3)
 	c:EnableReviveLimit()
@@ -11,36 +12,36 @@ function c80000039.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Activate
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(80000039,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCountLimit(1,80000039)
-	e2:SetCondition(c80000039.setcon)
-	e2:SetOperation(c80000039.setop)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.setcon)
+	e2:SetOperation(s.setop)
 	c:RegisterEffect(e2)
 	--remove
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(80000039,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
-	e3:SetCost(c80000039.cost)
-	e3:SetTarget(c80000039.target)
-	e3:SetOperation(c80000039.operation)
+	e3:SetCost(s.cost)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
 end
-function c80000039.setcon(e,tp,eg,ep,ev,re,r,rp)
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_XYZ
 end
-function c80000039.filter(c)
+function s.filter(c)
 	return c:IsCode(22702055) and c:GetActivateEffect():IsActivatable(tp)
 end
-function c80000039.setop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(80000039,0))
-	local tc=Duel.SelectMatchingCard(tp,c80000039.filter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
 	if tc then
-		local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+		local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
 		if fc then
 			Duel.SendtoGrave(fc,REASON_RULE)
 			Duel.BreakEffect()
@@ -53,32 +54,32 @@ function c80000039.setop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RaiseEvent(tc,4179255,te,0,tp,tp,Duel.GetCurrentChain())
 	end
 end
-function c80000039.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c80000039.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD+LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,1,nil) end
 	local off=1
 	local ops={}
 	local opval={}
 	if Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) then
-		ops[off]=aux.Stringid(80000039,1)
+		ops[off]=aux.Stringid(id,1)
 		opval[off-1]=1
 		off=off+1
 	end
 	if Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil) then
-		ops[off]=aux.Stringid(80000039,2)
+		ops[off]=aux.Stringid(id,2)
 		opval[off-1]=2
 		off=off+1
 	end
 	if Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) then
-		ops[off]=aux.Stringid(80000039,3)
+		ops[off]=aux.Stringid(id,3)
 		opval[off-1]=3
 		off=off+1
 	end
 	if Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_DECK,1,nil) then
-		ops[off]=aux.Stringid(80000039,4)
+		ops[off]=aux.Stringid(id,4)
 		opval[off-1]=4
 		off=off+1
 	end
@@ -86,7 +87,7 @@ function c80000039.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local op=Duel.SelectOption(tp,table.unpack(ops))
 	e:SetLabel(opval[op])
 end
-function c80000039.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
 	if op==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
