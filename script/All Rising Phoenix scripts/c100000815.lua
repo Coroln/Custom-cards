7463af6185b3d3fd4eddb0ada00910625fdebfe8
Coroln <janpackebusch0077@gmail.com>
2021-11-10 12:01:39 +0100

    Update c100000815.lua

--- script/All Rising Phoenix scripts/c100000815.lua
+++ script/All Rising Phoenix scripts/c100000815.lua
@@ -1,14 +1,15 @@
---ptah
-function c100000815.initial_effect(c)
+--Apophis
+local s,id=GetID()
+function s.initial_effect(c)
 	--lp
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100000815,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
-	e1:SetTarget(c100000815.htg)
-	e1:SetOperation(c100000815.hop)
+	e1:SetTarget(s.htg)
+	e1:SetOperation(s.hop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
@@ -18,14 +19,14 @@ function c100000815.initial_effect(c)
 	c:RegisterEffect(e3)
 		--spsummon
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100000815,0))
+	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_SUMMON_SUCCESS)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e4:SetCountLimit(1,100000815)
-	e4:SetTarget(c100000815.sptg)
-	e4:SetOperation(c100000815.spop)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
 	local e5=e4:Clone()
 	e5:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
@@ -40,7 +41,7 @@ function c100000815.initial_effect(c)
 	e7:SetRange(LOCATION_MZONE)
 	e7:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e7:SetTargetRange(1,0)
-	e7:SetValue(c100000815.damval)
+	e7:SetValue(s.damval)
 	c:RegisterEffect(e7)
 	local e8=e7:Clone()
 	e8:SetCode(EFFECT_NO_EFFECT_DAMAGE)
@@ -56,72 +57,72 @@ function c100000815.initial_effect(c)
 	e10:SetType(EFFECT_TYPE_SINGLE)
 	e10:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
 	e10:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e10:SetValue(c100000815.synlimit)
+	e10:SetValue(s.synlimit)
 	c:RegisterEffect(e10)
 	--damage
 	local e11=Effect.CreateEffect(c)
 	e11:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e11:SetDescription(aux.Stringid(100000815,1))
+	e11:SetDescription(aux.Stringid(id,1))
 	e11:SetCategory(CATEGORY_DAMAGE)
 	e11:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e11:SetRange(LOCATION_MZONE)
 	e11:SetCountLimit(1)
-	e11:SetCondition(c100000815.damcon)
-	e11:SetTarget(c100000815.damtg)
-	e11:SetOperation(c100000815.damop)
+	e11:SetCondition(s.damcon)
+	e11:SetTarget(s.damtg)
+	e11:SetOperation(s.damop)
 	c:RegisterEffect(e11)
 		--cannot link material
 	local e20=Effect.CreateEffect(c)
 	e20:SetType(EFFECT_TYPE_SINGLE)
 	e20:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e20:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
-	e20:SetValue(c100000815.synlimit)
+	e20:SetValue(s.synlimit)
 	c:RegisterEffect(e20)
 end
-function c100000815.htg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.htg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1000)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,1000)
 end
-function c100000815.hop(e,tp,eg,ep,ev,re,r,rp)
+function s.hop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Recover(p,d,REASON_EFFECT)
 end
-function c100000815.filter(c,e,tp)
+function s.filter(c,e,tp)
 	return c:IsSetCard(0x758)  and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100000815.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100000815.filter(chkc,e,tp) end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100000815.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100000815.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c100000815.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then end
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-end	
-function c100000815.damval(e,re,val,r,rp,rc)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.damval(e,re,val,r,rp,rc)
 	if bit.band(r,REASON_EFFECT)~=0 and rp~=e:GetOwnerPlayer() then return 0 
 	else return val end
 end
-function c100000815.synlimit(e,c)
+function s.synlimit(e,c)
 	if not c then return false end
 	return not c:IsSetCard(0x758)
 end
-function c100000815.damcon(e,tp,eg,ep,ev,re,r,rp)
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c100000815.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1000)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,0,0,tp,1000)
 end
-function c100000815.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
