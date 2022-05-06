95a81923cb4cb507d181adbbb4a0bed9682e9f63
Coroln <76746930+Coroln@users.noreply.github.com>
2022-05-06 12:44:09 +0200

    Update c14582572.lua

--- script/c14582572.lua
+++ script/c14582572.lua
@@ -1,22 +1,23 @@
 --Arcanit Power ball
-function c14582572.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCategory(CATEGORY_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(c14582572.damcost)
-	e1:SetTarget(c14582572.damtg)
-	e1:SetOperation(c14582572.damop)
+	e1:SetCost(s.damcost)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
 	c:RegisterEffect(e1)
 end
-function c14582572.filter(c)
+function s.filter(c)
 	return c:GetCounter(0x1)>0
 end
-function c14582572.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c14582572.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(c14582572.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+function s.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	local tc=g:GetFirst()
 	local s=0
 	while tc do
@@ -27,13 +28,13 @@ function c14582572.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	e:SetLabel(s*200)
 end
-function c14582572.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(e:GetLabel())
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,e:GetLabel())
 end
-function c14582572.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
