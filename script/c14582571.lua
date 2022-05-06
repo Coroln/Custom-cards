6d701e71008e195fc517fbe525b90a6f380b0230
Coroln <76746930+Coroln@users.noreply.github.com>
2022-05-06 12:42:47 +0200

    Update c14582571.lua

--- script/c14582571.lua
+++ script/c14582571.lua
@@ -1,5 +1,6 @@
 --Mage of the Spell Circle
-function c14582571.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	c:EnableCounterPermit(0x1)
 	--add counter
 	local e0=Effect.CreateEffect(c)
@@ -13,7 +14,7 @@ function c14582571.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e1:SetCode(EVENT_CHAIN_SOLVED)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetOperation(c14582571.acop)
+	e1:SetOperation(s.acop)
 	c:RegisterEffect(e1)
 	--Gain ATK
 	local e2=Effect.CreateEffect(c)
@@ -21,42 +22,42 @@ function c14582571.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(c14582571.atkval)
+	e2:SetValue(s.atkval)
 	c:RegisterEffect(e2)
 	--to hand
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(14582571,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
-	e3:SetCost(c14582571.thcost)
-	e3:SetTarget(c14582571.thtg)
-	e3:SetOperation(c14582571.thop)
+	e3:SetCost(s.thcost)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 	end
-	function c14582571.acop(e,tp,eg,ep,ev,re,r,rp)
+	function s.acop(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
 		e:GetHandler():AddCounter(0x1,2)
 	end
 end
-function c14582571.atkval(e,c)
+function s.atkval(e,c)
 	return Duel.GetCounter(1,1,1,0x1)*500
 end
-function c14582571.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,1,3,0,0x1,nil,REASON_COST) end
 	e:GetHandler():RemoveCounter(tp,1,3,0,0x1,nil,REASON_COST)
 end
-function c14582571.filter(c)
+function s.filter(c)
 	return c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
-function c14582571.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c14582571.filter,tp,LOCATION_DECK,0,1,nil) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c14582571.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c14582571.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
 		Duel.ConfirmCards(1-tp,g)
 		Duel.ShuffleHand(tp)
