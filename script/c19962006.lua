c3d7446832a564b12e64f919cb961b2b8661fade
Coroln <76746930+Coroln@users.noreply.github.com>
2022-05-06 12:45:15 +0200

    Update c19962006.lua

--- script/c19962006.lua
+++ script/c19962006.lua
@@ -1,12 +1,13 @@
 --PM Mega Glurak x
-function c19962006.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--skip
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c19962006.skipcon)
-	e1:SetOperation(c19962006.skipop)
+	e1:SetCondition(s.skipcon)
+	e1:SetOperation(s.skipop)
 	c:RegisterEffect(e1)
 	--disable
 	local e2=Effect.CreateEffect(c)
@@ -14,7 +15,7 @@ function c19962006.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e2:SetCode(EFFECT_DISABLE)
-	e2:SetTarget(c19962006.distg)
+	e2:SetTarget(s.distg)
 	c:RegisterEffect(e2)
 	--search
 	local e3=Effect.CreateEffect(c)
@@ -22,9 +23,9 @@ function c19962006.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCondition(c19962006.condition)
-	e3:SetTarget(c19962006.target)
-	e3:SetOperation(c19962006.operation)
+	e3:SetCondition(s.condition)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
 	--activate cost
 	local e4=Effect.CreateEffect(c)
@@ -33,14 +34,14 @@ function c19962006.initial_effect(c)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e4:SetTargetRange(0,1)
-	e4:SetCost(c19962006.costchk)
-	e4:SetOperation(c19962006.costop)
+	e4:SetCost(s.costchk)
+	e4:SetOperation(s.costop)
 	c:RegisterEffect(e4)
 end
-function c19962006.skipcon(e,tp,eg,ep,ev,re,r,rp)
+function s.skipcon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_RITUAL)==SUMMON_TYPE_RITUAL
 end
-function c19962006.skipop(e,tp,eg,ep,ev,re,r,rp)
+function s.skipop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -49,30 +50,30 @@ function c19962006.skipop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
 	Duel.RegisterEffect(e1,tp)
 end
-function c19962006.distg(e,c)
+function s.distg(e,c)
 	return c:IsType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ)
 end
-function c19962006.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c19962006.filter(c)
-	return c:IsSetCard(0x7cc) and not c:IsCode(19962006) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+function s.filter(c)
+	return c:IsSetCard(0x7cc) and not c:IsCode(id) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function c19962006.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c19962006.filter,tp,LOCATION_DECK,0,1,nil) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c19962006.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c19962006.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c19962006.costchk(e,te_or_c,tp)
+function s.costchk(e,te_or_c,tp)
 	return Duel.CheckLPCost(tp,500)
 end
-function c19962006.costop(e,tp,eg,ep,ev,re,r,rp)
+function s.costop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.PayLPCost(tp,500)
 end
