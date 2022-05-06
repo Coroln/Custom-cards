2d0164f4dc6e4ef20c7934128a2860e22196a8e4
Coroln <76746930+Coroln@users.noreply.github.com>
2022-05-06 12:49:10 +0200

    Update c19962007.lua

--- script/c19962007.lua
+++ script/c19962007.lua
@@ -1,65 +1,66 @@
 --PM Gluraknit x
-function c19962007.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c19962007.target)
-	e1:SetOperation(c19962007.activate)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--salvage
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetDescription(aux.Stringid(19962007,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_CUSTOM+19962007)
+	e2:SetCode(EVENT_CUSTOM+id)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCondition(c19962007.thcon)
-	e2:SetCost(c19962007.thcost)
-	e2:SetTarget(c19962007.thtg)
-	e2:SetOperation(c19962007.thop)
+	e2:SetCondition(s.thcon)
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 	--event
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_CHAIN_END)
 	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCondition(c19962007.evcon)
-	e3:SetOperation(c19962007.evop)
+	e3:SetCondition(s.evcon)
+	e3:SetOperation(s.evop)
 	c:RegisterEffect(e3)
 	e1:SetLabelObject(e3)
 end
-c19962007.fit_monster={19962006}
-function c19962007.filter(c,e,tp,m,ft)
+s.fit_monster={19962006}
+function s.filter(c,e,tp,m,ft)
 	if not c:IsCode(19962006) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
 	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
 	if ft>0 then
 		return mg:CheckWithSumEqual(Card.GetRitualLevel,7,1,99,c)
 	else
-		return mg:IsExists(c19962007.mfilterf,1,nil,tp,mg,c)
+		return mg:IsExists(s.mfilterf,1,nil,tp,mg,c)
 	end
 end
-function c19962007.mfilterf(c,tp,mg,rc)
+function s.mfilterf(c,tp,mg,rc)
 	if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) then
 		Duel.SetSelectedCard(c)
 		return mg:CheckWithSumEqual(Card.GetRitualLevel,7,0,99,rc)
 	else return false end
 end
-function c19962007.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg=Duel.GetRitualMaterial(tp)
 		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		return ft>-1 and Duel.IsExistingMatchingCard(c19962007.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
+		return ft>-1 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c19962007.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetRitualMaterial(tp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,c19962007.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft)
+	local tg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft)
 	local tc=tg:GetFirst()
 	if tc then
 		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
@@ -69,7 +70,7 @@ function c19962007.activate(e,tp,eg,ep,ev,re,r,rp)
 			mat=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,7,1,99,tc)
 		else
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			mat=mg:FilterSelect(tp,c19962007.mfilterf,1,1,nil,tp,mg,tc)
+			mat=mg:FilterSelect(tp,s.mfilterf,1,1,nil,tp,mg,tc)
 			Duel.SetSelectedCard(mat)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 			local mat2=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,7,0,99,tc)
@@ -83,38 +84,38 @@ function c19962007.activate(e,tp,eg,ep,ev,re,r,rp)
 		e:GetLabelObject():SetLabelObject(tc)
 	end
 end
-function c19962007.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return re:GetHandler()==e:GetHandler()
 end
-function c19962007.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsAbleToRemove() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c19962007.thfilter(c,e,tp)
+function s.thfilter(c,e,tp)
 	return c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and c:IsAbleToHand() and c:IsCanBeEffectTarget(e)
 end
-function c19962007.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tc=eg:GetFirst()
 	local mat=tc:GetMaterial()
-	if chkc then return mat:IsContains(chkc) and c19962007.thfilter(chkc,e,tp) end
-	if chk==0 then return mat:IsExists(c19962007.thfilter,1,nil,e,tp) end
+	if chkc then return mat:IsContains(chkc) and s.thfilter(chkc,e,tp) end
+	if chk==0 then return mat:IsExists(s.thfilter,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=mat:FilterSelect(tp,c19962007.thfilter,1,1,nil,e,tp)
+	local g=mat:FilterSelect(tp,s.thfilter,1,1,nil,e,tp)
 	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
 end
-function c19962007.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
 	end
 end
-function c19962007.evcon(e,tp,eg,ep,ev,re,r,rp)
+function s.evcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetLabelObject()~=nil
 end
-function c19962007.evop(e,tp,eg,ep,ev,re,r,rp)
+function s.evop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
-	Duel.RaiseEvent(tc,EVENT_CUSTOM+19962007,e,0,tp,tp,0)
+	Duel.RaiseEvent(tc,EVENT_CUSTOM+id,e,0,tp,tp,0)
 	e:SetLabelObject(nil)
 end
