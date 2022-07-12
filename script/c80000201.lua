e57e520dbc42e70d4efd0865cc5c1459339d70b0
Coroln <76746930+Coroln@users.noreply.github.com>
2022-07-12 12:42:07 +0200

    Add files via upload
    Fix special summon and update code

--- script/c80000201.lua
+++ script/c80000201.lua
@@ -1,16 +1,16 @@
 --TimeGear Paragon der Mächtige
-function c80000201.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(80000201,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(c80000201.cost)
-	e1:SetTarget(c80000201.sptg)
-	e1:SetOperation(c80000201.spop)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--Activate
 	local e2=Effect.CreateEffect(c)
@@ -19,43 +19,45 @@ function c80000201.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
 	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetTarget(c80000201.target)
-	e2:SetOperation(c80000201.activate)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
 	c:RegisterEffect(e2)
 end
-function c80000201.spfilter(c)
-	return c:IsSetCard(0x1bbf) and not c:IsCode(80000201) and c:IsAbleToRemoveAsCost()
+function s.spfilter(c)
+	return c:IsSetCard(0x1bbf) and c:IsMonster() and c:IsAbleToRemoveAsCost()
 end
-function c80000201.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c80000201.spfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
-	Duel.RemoveHand(tp,c80000201.spfilter,1,1,REASON_COST+REASON_REMOVE,e:GetHandler())
+function s.spcon(e,c)
+     if c==nil then return true end
+     local tp=c:GetControler()
+     return Duel.GetLocationCount(tp,LOCATION_MZONE)<0
+        and  Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,c)
 end
-function c80000201.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local rg=Duel.GetMatchingGroup(c80000201.spfilter,tp,LOCATION_HAND,0,nil)
-	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_REMOVE,nil,nil,true)
-	if #g>0 then
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c,must,og,min,max)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,true,c)
+	if g and #g>0 then
 		g:KeepAlive()
 		e:SetLabelObject(g)
 		return true
 	end
 	return false
 end
-function c80000201.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 	g:DeleteGroup()
 end
-function c80000201.filter(c)
+function s.filter(c)
 	return c:IsPosition(POS_FACEDOWN) and c:IsAbleToHand()
 end
-function c80000201.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c80000201.filter,tp,LOCATION_REMOVED,0,1,nil) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
 end
-function c80000201.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c80000201.filter,tp,LOCATION_REMOVED,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
