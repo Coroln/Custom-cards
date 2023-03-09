8b177dea8f385753cc70a41a071e138899b21553
Coroln <janpackebusch0077@gmail.com>
2023-03-09 13:57:07 +0100

    Update c80000001.lua

--- script/c80000001.lua
+++ script/c80000001.lua
@@ -68,6 +68,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 			if #g>0 then
+			for tc do
 				Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
@@ -84,6 +85,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		end
 		Duel.SpecialSummonComplete()
 	end
+end
 function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	if not re then return false end
 	local rc=re:GetHandler()
