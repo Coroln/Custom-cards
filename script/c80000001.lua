45b20c24e71db9f6d93680cef94680d2d79c476d
Coroln <janpackebusch0077@gmail.com>
2023-03-09 13:55:02 +0100

    Update c80000001.lua

--- script/c80000001.lua
+++ script/c80000001.lua
@@ -67,7 +67,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-			if #g1>0 then
+			if #g>0 then
 				Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
