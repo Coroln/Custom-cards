95e4a7acc6aa3dce13efd48bd990496052b780c7
Coroln <janpackebusch0077@gmail.com>
2023-08-17 15:24:40 +0200

    Update c69696975.lua

--- script/c69696975.lua
+++ script/c69696975.lua
@@ -1,15 +1,20 @@
 --Kaiserwaffe Incursio (Schwert)
 local s,id=GetID()
 function s.initial_effect(c)
-	--Your monster can make a second attack
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--double attack
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCondition(s.condition)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetRange(LOCATION_SZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(s.filter)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x69AA))
+	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetDescription(3207)
@@ -21,23 +26,3 @@ function s.initial_effect(c)
 end
 s.listed_series={0x69AA}
 s.listed_names={id}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsAbleToEnterBP()
-end
-function s.filter(c)
-	return c:IsSetCard(0x69AA) and c:GetAttack()<2500 and c:IsFaceUp() and c:CanAttack()
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		--Can make a second attack
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(3201)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_EXTRA_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-	end
-end
