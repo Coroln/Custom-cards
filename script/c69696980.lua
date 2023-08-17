03fa84c64a7f8262505dce9a6c00b3aa4a4c7e3f
Coroln <janpackebusch0077@gmail.com>
2023-08-17 15:50:07 +0200

    Update c69696980.lua

--- script/c69696980.lua
+++ script/c69696980.lua
@@ -6,37 +6,26 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCategory(CATEGORY_CONTROL)
-	e1:SetCondition(s.spcon)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	-- Release control during the End Phase
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_PHASE + PHASE_END)
-    e2:SetCountLimit(1)
-    e2:SetOperation(s.releaseControl)
-    c.RegisterEffect(e2)
 end
 s.listed_series={0x69AA}
 s.listed_names={id}
-
-function s.spfilter(c,sp)
-	return c:GetSummonPlayer() == sp and c:IsType(TYPE_FUSION + TYPE_SYNCHRO + TYPE_XYZ + TYPE_LINK)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsSummonLocation,1,nil,LOCATION_EXTRA)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.spfilter,1,nil,1-tp)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=eg:GetFirst()
-	if chk==0 then return tc:IsAbleToChangeControler() end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return eg:GetFirst():IsCanBeEffectTarget(e) and eg:GetFirst():IsControlerCanBeChanged() end
 	Duel.SetTargetCard(eg)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,eg,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
 		Duel.GetControl(tc,tp)
+		aux.DelayedOperation(tc,PHASE_DRAW,id,e,tp,function(ag) Duel.SendtoDeck(ag,nil,SEQ_DECKSHUFFLE,REASON_EFFECT) end,nil,0,0,aux.Stringid(id,2))
 	end
 end
-
