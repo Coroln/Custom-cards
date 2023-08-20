37430a9f7b6a7cf6bb8dbea0e097c8f5082a8607
creasycat <129886096+creasycat@users.noreply.github.com>
2023-08-20 14:41:16 +0200

    Update c69696970.lua

--- script/c69696970.lua
+++ script/c69696970.lua
@@ -1,51 +1,48 @@
 --Kaiserwaffe Cross Tail
 local s, id = GetID()
 function s.initial_effect(c)
-    -- -500 ATK
+    --Make 1 of the opponent's monsters lose 200 ATK/DEF
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_ATKCHANGE)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCountLimit(1,id)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetTarget(s.tg)
-    e1:SetOperation(s.atkop)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.target)
+    e1:SetValue(s.atkval)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
     c:RegisterEffect(e1)
-    -- +800 DEF
-    local e2=e1:Clone()
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_DEFCHANGE)
-    e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetOperation(s.defop)
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_ATKCHANGE)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.target)
+    e2:SetValue(s.defval)
+    e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x69AA}
 s.listed_names={id}
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
-    if chk==0 then return true end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-    Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-end
-
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkval(e,c,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-        local value = -250
-        if tc:IsAttribute(ATTRIBUTE_DARK) then
-            value = -500 -- Set the value to -500 for DARK monsters
-        end
-    end
+    if not tc:Is_Attribute(DARK) then
+        return -250
+    else
+        return -500
 end
-
-function s.defop(e,tp,eg,ep,ev,re,r,rp)
+function s.defval(e,c,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-        local value = 400
-        if tc:IsAttribute(ATTRIBUTE_DARK) then
-            value = 800 -- Set the value to 800 for DARK monsters
-        end
-    end
+    if not tc:Is_Attribute(DARK) then
+        return 400
+    else
+        return 800
+end
+function s.filter(c)
+    return c:IsFaceup()
+end
+    --Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE) end
 end
