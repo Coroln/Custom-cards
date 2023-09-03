c1792c8a2de11ef41eab621635288a10539ca699
creasycat <129886096+creasycat@users.noreply.github.com>
2023-09-03 19:13:16 +0200

    Update c69696969.lua

--- script/c69696969.lua
+++ script/c69696969.lua
@@ -2,46 +2,77 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- Effekt aktivieren
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_CHAINING)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1, id)
-    e1:SetCondition(s.condition1)
-    e1:SetTarget(s.target1)
-    e1:SetOperation(s.operation1)
-    c:RegisterEffect(e1)
+    --Upon normal summon, add 1 "Kaiserwaffe" monster
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
+    --Set 2 Spells (from deck and GY) with different names
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(aux.selfreleasecost)
+    e2:SetTarget(s.target)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
 end
 s.listed_series={0x69AA}
 s.listed_names={id}
-function s.condition1(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.GetChainInfo(ev, CHAININFO_TRIGGERING_LOCATION) == LOCATION_MZONE
-        and Duel.GetChainInfo(ev, CHAININFO_TRIGGERING_CONTROLER) == 1 - tp
+-- e1:
+function s.filter(c)
+    return c:IsMonster() and c:IsSetCard(0x69AA) and c:IsAbleToHand()
 end
-
-function s.target1(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chkc then return chkc:IsOnField() and chkc:IsCanBeEffectTarget(ev) end
-    if chk == 0 then return Duel.IsExistingTarget(Card.IsOnField, tp, 0, LOCATION_ONFIELD, 1, nil) end
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TARGET)
-    local g = Duel.SelectTarget(tp, Card.IsOnField, tp, 0, LOCATION_ONFIELD, 1, 1, nil)
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-
-function s.operation1(e, tp, eg, ep, ev, re, r, rp)
-    local tg = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
-    local etc = tg:Filter(Card.IsRelateToEffect, nil, e)
-    if #etc > 0 then
-        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TARGET)
-        local g = Duel.SelectMatchingCard(tp, aux.TRUE, tp, LOCATION_ONFIELD, 0, 1, 1, nil)
-        if #g > 0 then
-            local tc = g:GetFirst()
-            local newtg = Duel.GetChainInfo(ev, CHAININFO_TARGET_CARDS)
-            newtg:Clear()
-            newtg:AddCard(tc)
-            Duel.ChangeTargetCard(ev, newtg)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+-- e2:
+    --Check for "Hole" normal traps
+function s.setfilter(c)
+    return c:IsSpell() and c:IsSSetable() and c:IsSetCard(0x69AA)
+end
+    --The 2 cards have different names from each other
+function s.setcheck(sg,e,tp,mg)
+    return sg:GetClassCount(Card.GetLocation)>=#sg and sg:GetClassCount(Card.GetCode)>=#sg
+end
+    --Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local sg=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,nil)
+        return Duel.GetLocationCount(tp,LOCATION_SZONE)>=2
+            and aux.SelectUnselectGroup(sg,e,tp,2,2,s.setcheck,0) end
+    Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,0)
+end
+    --Set 2 spells with different names from deck and GY, banish them when they leave
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<2 then return end
+    local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.setfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,nil)
+    if #sg==0 then return end
+    local rg=aux.SelectUnselectGroup(sg,e,tp,2,2,s.setcheck,1,tp,HINTMSG_SET,s.setcheck)
+    if #rg>0 then
+        Duel.SSet(tp,rg)
+        for tc in rg:Iter() do
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+            e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+            e1:SetValue(LOCATION_REMOVED)
+            e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+            tc:RegisterEffect(e1)
         end
     end
 end
