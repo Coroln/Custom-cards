85ae41dc729762b9e11a647ea37c545a659e79ba
Coroln <janpackebusch0077@gmail.com>
2023-09-09 13:29:51 +0200

    Update c53790726.lua

--- script/c53790726.lua
+++ script/c53790726.lua
@@ -1,24 +1,26 @@
 --Magnificent Chimera the Mythical Phantom Beast
-function c53790726.initial_effect(c)
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,true,true,5818798,1,aux.FilterBoolFunctionEx(Card.IsSetCard,0x1B),2)
 	--equip
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(53790726,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c53790726.eqtg)
-	e1:SetOperation(c53790726.eqop)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
 	--attack all
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_ATTACK_ALL)
-	e2:SetCondition(c53790726.atcon)
+	e2:SetCondition(s.atcon)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
 	--pierce
@@ -27,21 +29,21 @@ function c53790726.initial_effect(c)
 	e3:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e3)
 end
-function c53790726.eqfilter(c,tp)
+function s.eqfilter(c,tp)
 	return c:IsFaceup() and not c:IsRace(RACE_BEAST_WARRIOR) and (c:IsControler(tp) or c:IsAbleToChangeControler())
 end
-function c53790726.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c53790726.eqfilter(chkc,tp) end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.eqfilter(chkc,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c53790726.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler(),tp) end
+		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler(),tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c53790726.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler(),tp)
+	local g=Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler(),tp)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
-function c53790726.eqlimit(e,c)
+function s.eqlimit(e,c)
 	return e:GetOwner()==c
 end
-function c53790726.eqop(e,tp,eg,ep,ev,re,r,rp)
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsType(TYPE_MONSTER) and not tc:IsRace(RACE_BEAST_WARRIOR) and tc:IsRelateToEffect(e) then
@@ -53,17 +55,17 @@ function c53790726.eqop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
 			e1:SetCode(EFFECT_EQUIP_LIMIT)
 			e1:SetReset(RESET_EVENT+0x1fe0000)
-			e1:SetValue(c53790726.eqlimit)
+			e1:SetValue(s.eqlimit)
 			tc:RegisterEffect(e1)
 			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_EQUIP)
 			e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-			e2:SetCode(53790726)
+			e2:SetCode(id)
 			e2:SetReset(RESET_EVENT+0x1fe0000)
 			tc:RegisterEffect(e2)
 		else Duel.SendtoGrave(tc,REASON_EFFECT) end
 	end
 end
-function c53790726.atcon(e)
-	return e:GetHandler():IsHasEffect(53790726)
+function s.atcon(e)
+	return e:GetHandler():IsHasEffect(id)
 end
