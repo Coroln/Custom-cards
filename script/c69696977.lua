ff27f19329bb16cc833adee1a0e72b4acfaa98a7
Coroln <janpackebusch0077@gmail.com>
2023-09-25 14:36:30 +0200

    Update c69696977.lua

--- script/c69696977.lua
+++ script/c69696977.lua
@@ -42,7 +42,7 @@ s.listed_series={0x69AA}
 s.listed_names={id}
 --Search
 function s.filter(c,lv)
-    return c:IsSetCard(0x69AA) and c:IsMonster() and c:IsAbleToHand() and c:GetLevel() and c:IsLevelBelow(function(tc) return tc:GetLevel())
+    return c:IsSetCard(0x69AA) and c:IsMonster() and c:IsAbleToHand() and c:GetLevel() and c:IsLevelBelow(function(tc) return tc:GetLevel() end)
 end
 function s.filter1(c)
     -- Define a lambda function to get the level of the targeted monster (c)
