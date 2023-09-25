d917f15f2d61b56099154cdb46bd87f8c63b2fd3
Coroln <janpackebusch0077@gmail.com>
2023-09-25 12:49:25 +0200

    Update c69696977.lua

--- script/c69696977.lua
+++ script/c69696977.lua
@@ -57,7 +57,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
     if #g> 0 then
-        Duel.SendtoHand(g nil,REASON_EFFECT)
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
     end
 end
