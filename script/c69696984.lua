a462ce18f547e05a37d582834ba7ad5a1919b5ce
Coroln <janpackebusch0077@gmail.com>
2023-10-12 13:51:13 +0200

    adad

--- script/c69696984.lua
+++ script/c69696984.lua
@@ -12,30 +12,19 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
-	--Banish itself and negate an attack
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_REMOVE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(s.nacon)
-	e3:SetTarget(s.natg)
-	e3:SetOperation(s.naop)
-	c:RegisterEffect(e3)
-	--Special Summon itself during the End Phase
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
-	e4:SetRange(LOCATION_REMOVED)
-	e4:SetCountLimit(1)
-	e4:SetCondition(s.spcon)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop)
-	c:RegisterEffect(e4)
+	--remove
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(TIMING_BATTLE_PHASE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.rmcon)
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
 	--Multiple Tuners
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
@@ -46,35 +35,61 @@ end
 s.listed_series={0x69AA}
 s.listed_names={id}
 function s.atkval(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsType,LOCATION_GRAVE,LOCATION_GRAVE,0,TYPE_TUNER)*500
+	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,LOCATION_GRAVE,nil,TYPE_TUNER)*500
 end
-function s.nacon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttacker():IsControler(1-tp)
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsBattlePhase() and not e:GetHandler():IsStatus(STATUS_CHAINING)
 end
-function s.natg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc==Duel.GetAttacker() end
-	if chk==0 then return e:GetHandler():IsAbleToRemove() and Duel.GetAttacker():IsCanBeEffectTarget(e)
-		and not e:GetHandler():IsStatus(STATUS_CHAINING) end
-	Duel.SetTargetCard(Duel.GetAttacker())
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
-end
-function s.naop(e,tp,eg,ep,ev,re,r,rp,chk)
-	Duel.NegateAttack()
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.Remove(c,POS_FACEUP,REASON_EFFECT)~=0 then
-		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,0,1)
+	local bc=c:GetBattleTarget()
+	if chk==0 then return bc and bc:IsOnField() and bc:IsCanBeEffectTarget(e) and c:IsAbleToRemove() and bc:IsAbleToRemove() end
+	Duel.SetTargetCard(bc)
+	local g=Group.FromCards(c,bc)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,2,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) then return end
+	local g=Group.FromCards(c,tc)
+	local mcount=0
+	if tc:IsFaceup() then mcount=tc:GetOverlayCount() end
+	if Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+		local og=Duel.GetOperatedGroup()
+		if not og:IsContains(tc) then mcount=0 end
+		local oc=og:GetFirst()
+		for oc in aux.Next(og) do
+			oc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		end
+		og:KeepAlive()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+		e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+		e1:SetLabel(mcount)
+		e1:SetCountLimit(1)
+		e1:SetLabelObject(og)
+		e1:SetOperation(s.retop)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(id)~=0
+function s.retfilter(c)
+	return c:GetFlagEffect(id)~=0
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local sg=g:Filter(s.retfilter,nil)
+	g:DeleteGroup()
+	for tc in aux.Next(sg) do
+		if Duel.ReturnToField(tc) and tc==e:GetOwner() and tc:IsFaceup() and e:GetLabel()~=0 then
+			local e1=Effect.CreateEffect(e:GetOwner())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+			e1:SetValue(e:GetLabel()*500)
+			e:GetOwner():RegisterEffect(e1)
+		end
 	end
 end
