f6c10f7c5111564190c8a378472c1b6ad26b810e
Coroln <janpackebusch0077@gmail.com>
2023-10-12 12:44:18 +0200

    Update c69696985.lua

--- script/c69696985.lua
+++ script/c69696985.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsLevel,6),2)
+	Fusion.AddProcMixN(c,false,false,aux.FilterBoolFunction(Card.IsLevel,6),2)
 	--cannot be fusion material
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
