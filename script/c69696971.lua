43050874eeb40bb5fdc1fea7610492c5269a6de0
creasycat <129886096+creasycat@users.noreply.github.com>
2023-11-05 15:41:25 +0100

    Update c69696971.lua

--- script/c69696971.lua
+++ script/c69696971.lua
@@ -1,8 +1,24 @@
 --Kaiserwaffe Pumpkin
 local s, id = GetID()
 function s.initial_effect(c)
-	--Increase ATK
+	--destroy
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--atk lose
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+--[[local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
@@ -21,13 +37,31 @@ function s.initial_effect(c)
 	e1:SetCondition(s.tpcon)
 	e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
 	e3:SetValue(aux.ChangeBattleDamage(1,HALF_DAMAGE))
-	c:RegisterEffect(e3)
+	c:RegisterEffect(e3)]]
 end
 s.listed_series={0x69AA}
 s.listed_names={id}
-function s.con(e)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	if chk==0 then return #g>0 end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local tc=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Damage(p,tc:GetBaseAttack(),REASON_EFFECT)
+	end
+end
+function s.atkval(e,c)
+	return Duel.GetFieldGroupCount(0,LOCATION_ONFIELD,LOCATION_ONFIELD)*-100
+end
+--[[function s.con(e)
 	return Duel.GetTurnPlayer()==e:GetHandlerPlayer()
 end
 function s.tpcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
-end
+end]]
