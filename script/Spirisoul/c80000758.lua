24a3379a0cf203ef109cc7cbf71bb10298393db4
Coroln <76746930+Coroln@users.noreply.github.com>
2024-08-28 13:44:38 +0200

    added the last card of the first wave
    Added Spirisoul Vampire, Alucard

---
+++ script/Spirisoul/c80000758.lua
@@ -0,0 +1,89 @@
+--Spirisoul Vampire, Alucard
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+    --spirit return
+	Spirit.AddProcedure(c,EVENT_SPSUMMON_SUCCESS)
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_EARTH),5,2,s.ovfilter,aux.Stringid(id,0))
+	c:EnableReviveLimit()
+    --destroy and sp summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+    --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_DECK)
+	e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.damcon)
+	e2:SetTarget(s.damtg2)
+	e2:SetOperation(s.damop2)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x356,SET_VAMPIRE}
+s.listed_names={80000758}
+--xyz summon
+function s.ovfilter(c,tp,lc)
+	return c:IsFaceup() and c:IsRankBelow(4) and c:IsRace(RACE_ZOMBIE,lc,SUMMON_TYPE_XYZ,tp)
+end
+--destroy and sp summon
+function s.filter(c,e,tp)
+    return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsType,TYPE_MONSTER),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g2=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g2,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc1=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc2=g:GetFirst()
+	if tc2==tc1 then tc2=g:GetNext() end
+	if tc1:IsRelateToEffect(e) and tc1:IsFaceup() and Duel.Destroy(tc1,REASON_EFFECT)~=0 and tc2:IsRelateToEffect(e) then
+        Duel.SpecialSummon(tc2,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--special summon
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE)
+end
+function s.damfilter(c,e,tp)
+	return c:IsSetCard({0x356,SET_VAMPIRE}) and c:IsLinkBelow(2) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_LINK,tp,false,false)
+end
+function s.damtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.damfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.damop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.damfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp):GetFirst()
+	if g then
+        Duel.SpecialSummon(g,SUMMON_TYPE_LINK,tp,tp,false,false,POS_FACEUP)
+        g:CompleteProcedure()
+    end
+end
