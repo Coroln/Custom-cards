55fd5165b0f8311db88243fa54054f1c1afc4b2c
Coroln <76746930+Coroln@users.noreply.github.com>
2024-08-29 13:46:31 +0200

    added function to Spirisoul's Revitalizing Spirit

--- script/Spirisoul/c80000759.lua
+++ script/Spirisoul/c80000759.lua
@@ -3,11 +3,54 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--During your Main Phase, you can Normal Summon 1 Spirit in addition to your Normal Summon/Set
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_HAND|LOCATION_MZONE,0)
+	e1:SetTarget(function(e,c) return c:IsType(TYPE_SPIRIT) end)
 	c:RegisterEffect(e1)
-   
+   --Gain LP (extra handling from Ghost Sister & Spooky Dogwood 儚無みずき)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_HAND)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.condition)
+	e2:SetOperation(s.recop1)
+	c:RegisterEffect(e2)
 end
 s.listed_names={0x356}
-
+--gain LP
+function s.cfilter(c,tp)
+	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+		and (c:GetType())~=TYPE_SPIRIT
+		and c:IsPreviousPosition(POS_FACEUP)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.sum(c)
+	if c:IsLocation(LOCATION_MZONE) then
+		return c:GetAttack()
+	else
+		return c:GetTextAttack()
+	end
+end
+function s.recop1(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.cfilter,nil,1-tp)
+	if #g>0 then
+		local sum=g:GetSum(s.sum)
+		Duel.Hint(HINT_CARD,0,id)
+		if Duel.Recover(tp,sum,REASON_EFFECT)~=0 then 
+			Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
+		end
+	end
+end
