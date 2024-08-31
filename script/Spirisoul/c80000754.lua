a43c577659b94613538de82fbd489161e87e18bf
Coroln <76746930+Coroln@users.noreply.github.com>
2024-09-01 00:08:25 +0200

    Update c80000754.lua

--- script/Spirisoul/c80000754.lua
+++ script/Spirisoul/c80000754.lua
@@ -3,10 +3,10 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
