16ee2ad7cb4a8567f40c3420c97a79fdd7751f18
Coroln <76746930+Coroln@users.noreply.github.com>
2024-09-07 21:02:22 +0200

    Added Script for Spirisoul Doll, Conzai
    and fixed something small with Spirisoul Doll, Canzai

---
+++ script/Spirisoul/c80000768.lua
@@ -0,0 +1,64 @@
+--Spirisoul Doll, Conzai
+--Script by Coroln
+Duel.LoadScript("customutility2.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+    --spirit return
+	aux.EnableSpirisoulReturn(c)
+    --special summon
+	aux.AddLavaProcedure(c,1,POS_FACEUP)
+    --recover
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_TO_HAND)
+    e1:SetCondition(s.descon)
+	e1:SetTarget(s.rectg)
+	e1:SetOperation(s.recop)
+	c:RegisterEffect(e1)
+    --hand des
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_HANDES)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_HAND)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.descon2)
+	e2:SetTarget(s.efftg)
+	e2:SetOperation(s.effop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={id}
+s.listed_series={0x356}
+--recover
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetHandler():IsPreviousControler(1-tp)
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(3000)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,1-tp,3000)
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
+--hand des
+function s.descon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetHandler():IsPreviousControler(tp)
+end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,1-tp,1)
+end
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	local g=Duel.GetFieldGroup(p,LOCATION_HAND,0):RandomSelect(p,d)
+	Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)
+end
