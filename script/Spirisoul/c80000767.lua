20b0d8708f1bc18e1381351a8c560c3f41b6673c
Coroln <76746930+Coroln@users.noreply.github.com>
2024-09-07 20:38:48 +0200

    Added Script for Spirisoul Doll, Canzai

---
+++ script/Spirisoul/c80000767.lua
@@ -0,0 +1,66 @@
+--Spirisoul Doll, Canzai
+--Script by Coroln
+Duel.LoadScript("customutility2.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+    --spirit return
+	aux.EnableSpirisoulReturn(c)
+    --special summon
+	aux.AddLavaProcedure(c,1,POS_FACEUP)
+    --draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_TO_HAND)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.descon)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+    --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,5))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_HAND)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.descon2)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={id}
+s.listed_series={0x356}
+--draw
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetHandler():IsPreviousControler(1-tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,2)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(1-tp,2,REASON_EFFECT)
+end
+--special summon
+function s.descon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetHandler():IsPreviousControler(tp)
+end
+function s.filter(c,e,tp)
+	return c:IsRace(RACE_ZOMBIE) and not c:IsCode(id) and c:IsType(TYPE_TUNER) and c:IsLevelBelow(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
