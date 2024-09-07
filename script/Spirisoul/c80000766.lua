239c1adb334bc09e549d4bf977b534c89fb837b3
Coroln <76746930+Coroln@users.noreply.github.com>
2024-09-07 15:19:12 +0200

    Update c80000766.lua

--- script/Spirisoul/c80000766.lua
+++ script/Spirisoul/c80000766.lua
@@ -66,7 +66,7 @@ function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.filter2(c,e,tp)
-	return c:IsSetCard(0x356) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x356) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.filter3 (c,e,tp)
     return not c:IsRace(RACE_ZOMBIE) and c:IsMonster()
