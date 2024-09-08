31ec8eb20abae04c2e3b3e42c3af6d358b766880
Coroln <76746930+Coroln@users.noreply.github.com>
2024-09-08 17:48:34 +0200

    Update c80000754.lua

--- script/Spirisoul/c80000754.lua
+++ script/Spirisoul/c80000754.lua
@@ -54,7 +54,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	--if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)>0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,1,nil,e,tp)
