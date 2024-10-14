ad7a3c5f31778deb6b0e7e30442eab49262d8b98
Coroln <76746930+Coroln@users.noreply.github.com>
2024-10-14 20:33:19 +0200

    More HEROes
    Added "Supreme HERO Divine Sprinter"

---
+++ script/Supreme HERO/c90000015.lua
@@ -0,0 +1,83 @@
+--Supreme HERO Divine Sprinter
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--code
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_ADD_SETCODE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_ALL)
+	e0:SetValue(0x9008)
+	c:RegisterEffect(e0)
+    local e1=e0:Clone()
+    e1:SetValue(0xA008)
+    c:RegisterEffect(e1)
+    --race
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_ADD_RACE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_ALL)
+	e2:SetValue(RACE_FIEND)
+    c:RegisterEffect(e2)
+    --Special Summon this card from your hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_SPSUMMON_PROC)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetCondition(s.spcon)
+	c:RegisterEffect(e3)
+    --Special Summon from Deck
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCost(s.spcost)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x8}
+--Special Summon this card from your hand
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+end
+--Special Summon from Deck
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end 
+function s.spfilter(c,e,tp,chk)
+	return c:IsSetCard(0x8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
+		and (not chk or Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,c))
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x46) and c:IsSpell() and c:IsAbleToHand()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,true) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end 
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler(c)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local ag=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+			if #ag>0 then
+				Duel.BreakEffect()
+				Duel.SendtoHand(ag,tp,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,ag)
+			end
+		end
+	end
+end
