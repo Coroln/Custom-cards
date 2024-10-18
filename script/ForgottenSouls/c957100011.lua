b92222c46514dece3c8f7ec331eebd89d47d7cea
Rika <113550549+RikaCPP@users.noreply.github.com>
2024-10-18 23:39:27 +0200

    Bearing Chaos Soul Rat (Bug fix)
    Player couldn´t change Battle Position of Token before the Bug Fix

--- script/ForgottenSouls/c957100011.lua
+++ script/ForgottenSouls/c957100011.lua
@@ -5,6 +5,7 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
@@ -51,6 +52,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,1650,1850,4,RACE_BEAST,ATTRIBUTE_LIGHT,POS_FACEUP_DEFENSE) then
 	local token=Duel.CreateToken(tp,id+1)
 	Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	Duel.SpecialSummonComplete()
 	end
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
 	for tc in aux.Next(g) do
