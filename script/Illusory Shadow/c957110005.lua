bd5eafaae020f063c017b2aa55710bca89d9ce82
Rika <113550549+RikaCPP@users.noreply.github.com>
2024-11-26 17:52:08 +0100

    Added Illusory Shadow Caster

--- script/Illusory Shadow/c957110005.lua
+++ script/Illusory Shadow/c957110005.lua
@@ -1,106 +1,106 @@
-
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
-	--special summon
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EVENT_DESTROYED)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e1)
+    --special summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCode(EVENT_DESTROYED)
     e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1)
+    e2:SetCountLimit(1)
     e2:SetCondition(s.skcon)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
-	c:RegisterEffect(e2)
+    e2:SetTarget(s.target)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
     --recover
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_RECOVER)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_BATTLE_DAMAGE)
-	e3:SetCondition(s.reccon)
-	e3:SetTarget(s.rectg)
-	e3:SetOperation(s.recop)
-	c:RegisterEffect(e3)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_RECOVER)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCode(EVENT_BATTLE_DAMAGE)
+    e3:SetCondition(s.reccon)
+    e3:SetTarget(s.rectg)
+    e3:SetOperation(s.recop)
+    c:RegisterEffect(e3)
 end
 --e2
 function s.skconfilter(c,tp)
-	local re=c:GetReasonEffect()
-	return re:IsMonsterEffect() and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp)
+    --if  not c:IsReason(REASON_EFFECT) then return false end
+    local re=c:GetReasonEffect()
+    return re:IsMonsterEffect() and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp)
 end
 function s.skcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.skconfilter,1,nil,tp)
+    return eg:IsExists(s.skconfilter,1,nil,tp)
 end
 
 function s.cfilter(c,e,tp)
-	local vDebug2 = 0
-	return c:IsSetCard(0xBBB) and c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE) and (c:GetPreviousSetCard()&0xBBB)~=0 and
-			Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,c:GetAttack(),c:GetAttribute(),e,tp)
+    local vDebug2 = 0
+    return c:IsSetCard(0xBBB) and c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE) and (c:GetPreviousSetCard()&0xBBB)~=0 and
+            Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,c:GetAttack(),c:GetAttribute(),e,tp)
 end
 function s.filter(c,atk,att,e,tp)
-	local a=c:GetAttack()
-	return a>=0 and a<atk and c:IsSetCard(0xBBB) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    local a=c:GetAttack()
+    return a>=0 and a<atk and c:IsSetCard(0xBBB) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and eg:IsExists(s.cfilter,1,nil,e,tp)
-	end
-	Duel.SetTargetCard(eg)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+    if chk==0 then
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and eg:IsExists(s.cfilter,1,nil,e,tp)
+    end
+    Duel.SetTargetCard(eg)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.cfilter2(c,e,tp)
-	return c:IsSetCard(0xBBB) and c:IsControler(tp) and c:IsRelateToEffect(e)
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,c:GetAttack(),nil,e,tp)
+    return c:IsSetCard(0xBBB) and c:IsControler(tp) and c:IsRelateToEffect(e)
+        and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,c:GetAttack(),nil,e,tp)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local sg=eg:Filter(s.cfilter2,nil,e,tp)
-	if #sg==1 then
-		local tc=sg:GetFirst()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tc:GetAttack(),nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
-	else
-		local tc=sg:GetFirst()
-		if not tc then return end
-		local atk=tc:GetAttack()
-		tc=sg:GetNext()
-		if tc then
-			if tc:GetAttack()>atk then atk=tc:GetAttack() end
-		end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,atk,nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
-	end
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    local sg=eg:Filter(s.cfilter2,nil,e,tp)
+    if #sg==1 then
+        local tc=sg:GetFirst()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tc:GetAttack(),nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
+    else
+        local tc=sg:GetFirst()
+        if not tc then return end
+        local atk=tc:GetAttack()
+        tc=sg:GetNext()
+        if tc then
+            if tc:GetAttack()>atk then atk=tc:GetAttack() end
+        end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,atk,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
 end
 
 --e3
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and eg:GetFirst():IsControler(tp) and eg:GetFirst():IsSetCard(0xBBB)
+    return ep~=tp and eg:GetFirst():IsControler(tp) and eg:GetFirst():IsSetCard(0xBBB)
 end
 function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(ev)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,ev)
+    if chk==0 then return true end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(ev)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,ev)
 end
 function s.recop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Recover(p,d,REASON_EFFECT)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+    Duel.Recover(p,d,REASON_EFFECT)
 end
---
+++ script/Illusory Shadow/c957110007.lua
@@ -0,0 +1,93 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCountLimit(1,id)
+	e1:SetTarget(s.desmtg)
+	e1:SetOperation(s.desmop)
+	c:RegisterEffect(e1)
+    --Special Summon 1 Level 4 or lower monster from your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.dessttg)
+	e2:SetOperation(s.desstop)
+	c:RegisterEffect(e2)
+    --Neither monster can be destroyed by battle
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(s.indestg)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+    --destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,2})
+	e4:SetCondition(s.descone4)
+	e4:SetTarget(s.destge4)
+	e4:SetOperation(s.desope4)
+	c:RegisterEffect(e4)
+end
+--e1
+function s.desmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsMonster,tp,0,LOCATION_MZONE,1,1,nil,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+--e2
+function s.filter(c)
+	return c:IsSpellTrap() and c:IsDestructable()
+end
+function s.dessttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return s.pfilter(chkc) and chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.pfilter,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local tc=Duel.SelectTarget(tp,s.pfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,tp,LOCATION_ONFIELD)
+end
+function s.desstop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+--e3
+function s.indestg(e,c)
+	local handler=e:GetHandler()
+	return c==handler or c==handler:GetBattleTarget()
+end
+--e4
+function s.descone4(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.destge4(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+end
+function s.desope4(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	end
+end
