58bf030ac62d502b4d4c52f52521a388ec12a078
Rika <113550549+RikaCPP@users.noreply.github.com>
2024-12-01 20:12:41 +0100

    Added Illusory Anomaly

---
+++ script/Illusory Shadow/c957110008.lua
@@ -0,0 +1,99 @@
+--Illusory Shadow Anomaly
+local s,id=GetID()
+function s.initial_effect(c)
+    --Special summon 1 Level 5 or lower "Illusory" monster from your Deck or GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Destroy + Negate + Shuffle 
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_TODECK+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(s.DNScon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.DNStg)
+	e2:SetOperation(s.DNDop)
+	c:RegisterEffect(e2)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+
+	--Check “Illusory” monster
+function s.filter(c,e,tp)
+    return c:IsSetCard(0xBBB) and c:IsLevelBelow(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+     --Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+end
+
+function s.ACfilter(c,e,tp)
+    return c:IsCode(957110005)
+end
+    	--Special summon 1 “Illusory” monster from deck
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP) == 0 then return end
+	end
+    local ct=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	--if not ct or ct<=0 or ft<ct or ct>1 then return end
+    if ft <= 0 then return end
+	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.ACfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,nil,e,tp)
+	if #sg<ct or not Duel.SelectYesNo(tp,aux.Stringid(id,3)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local ssg=sg:Select(tp,ct,ct,nil)
+	if #ssg==ct then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	    local tc=Duel.SelectMatchingCard(tp,s.ACfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
+	    Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+        local te=tc:GetActivateEffect()
+		local tep=tc:GetControler()
+    end
+end
+--e2
+function s.DNScon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsActiveType(TYPE_MONSTER) and re:IsMonsterEffect() and ep==1-tp
+end
+
+function s.setfilter(c,e,tp)
+	return c:IsSetCard(0xBBB)
+end
+
+function s.DNStg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsSetCard(0xBBB) end
+	if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+
+function s.DNDop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+		Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,#eg,0,0)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,#eg,0,0)
+		if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+			Duel.SendtoDeck(re:GetHandler(),nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		end
+	end
+end
