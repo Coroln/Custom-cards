034a04cf459ff3190cda06ec71cdb8880d7688fb
Rika <113550549+RikaCPP@users.noreply.github.com>
2024-12-15 10:35:08 +0100

    Added "Dark Blade Commander" (Retrain)

---
+++ script/Retrains/c957120005.lua
@@ -0,0 +1,90 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--Equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(Cyberdark.EquipTarget(s.eqfilter,true,true))
+	e1:SetOperation(Cyberdark.EquipOperation(s.eqfilter,s.equipop,true))
+	c:RegisterEffect(e1)
+    local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	aux.AddEREquipLimit(c,nil,s.eqval,s.equipop,e1)
+
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCost(s.descost)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+
+	--change name
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_CHANGE_CODE)
+	e4:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e4:SetValue(11321183)
+	c:RegisterEffect(e4)
+end
+
+function s.eqfilter(c)
+	return c:IsRace(RACE_DRAGON)
+end
+function s.eqval(ec,c,tp)
+	return ec:IsControler(tp) and ec:IsRace(RACE_DRAGON)
+end
+function s.equipop(c,e,tp,tc)
+	local atk=tc:GetTextAttack()
+	if atk<0 then atk=0 end
+	if not c:EquipByEffectAndLimitRegister(e,tp,tc) then return end
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetProperty(EFFECT_FLAG_OWNER_RELATE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(EFFECT_SET_ATTACK)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e2:SetValue(atk)
+	tc:RegisterEffect(e2)
+
+	tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+end
+
+--e3
+function s.descostfilter(c,tp)
+	return c:HasFlagEffect(id) and c:IsMonsterCard() and c:IsAbleToGraveAsCost()
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local eqg=e:GetHandler():GetEquipGroup():Filter(s.descostfilter,nil,tp)
+	if chk==0 then return #eqg>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tc=eqg:Select(tp,1,1,nil):GetFirst()
+	e:SetLabel(tc:GetTextAttack())
+	Duel.SendtoGrave(tc,REASON_COST)
+end
+function s.spfilter(c,e,tp)
+	return (c:IsCode(47415292)or c:IsCode(84814897)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then
+		ft=ft+1
+	end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then Debug.Message("spop_2") return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
