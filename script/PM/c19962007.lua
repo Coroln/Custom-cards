42f135709b85d9933233f1dd58cfabea569ba03c
Coroln <76746930+Coroln@users.noreply.github.com>
2024-12-30 01:06:35 +0100

    Update c19962007.lua
    Finally fixed "PM Charizardite X"

--- script/PM/c19962007.lua
+++ script/PM/c19962007.lua
@@ -2,120 +2,50 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
-	--salvage
+	Ritual.AddProcEqual{handler=c,filter=s.ritualfil,matfilter=s.matfilter,nil}
+	--Material Return
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_CUSTOM+id)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCondition(s.thcon)
-	e2:SetCost(s.thcost)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
+	e2:SetCondition(s.ritual_summon_condition)
+	e2:SetCost(aux.SelfBanishCost)
+	e2:SetTarget(s.ritual_summon_target)
+	e2:SetOperation(s.ritual_summon_operation)
 	c:RegisterEffect(e2)
-	--event
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_CHAIN_END)
-	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCondition(s.evcon)
-	e3:SetOperation(s.evop)
-	c:RegisterEffect(e3)
-	e1:SetLabelObject(e3)
 end
 s.fit_monster={19962006}
-function s.filter(c,e,tp,m,ft)
-	if not c:IsCode(19962006) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-	if ft>0 then
-		return mg:CheckWithSumEqual(Card.GetRitualLevel,7,1,99,c)
-	else
-		return mg:IsExists(s.mfilterf,1,nil,tp,mg,c)
-	end
+--Ritual Summon
+function s.ritualfil(c)
+	return c:IsCode(19962006) and c:IsRitualMonster()
 end
-function s.mfilterf(c,tp,mg,rc)
-	if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) then
-		Duel.SetSelectedCard(c)
-		return mg:CheckWithSumEqual(Card.GetRitualLevel,7,0,99,rc)
-	else return false end
+function s.matfilter(c)
+	return c:IsCode(19962005)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local mg=Duel.GetRitualMaterial(tp)
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		return ft>-1 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
+	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetRitualMaterial(tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft)
-	local tc=tg:GetFirst()
-	if tc then
-		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
-		local mat=nil
-		if ft>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			mat=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,7,1,99,tc)
-		else
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			mat=mg:FilterSelect(tp,s.mfilterf,1,1,nil,tp,mg,tc)
-			Duel.SetSelectedCard(mat)
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			local mat2=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,7,0,99,tc)
-			mat:Merge(mat2)
-		end
-		tc:SetMaterial(mat)
-		Duel.ReleaseRitualMaterial(mat)
-		Duel.BreakEffect()
-		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-		tc:CompleteProcedure()
-		e:GetLabelObject():SetLabelObject(tc)
-	end
+--Material Return
+-- Condition to check if this card is in the GY
+function s.ritual_summon_condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_SZONE)
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandler()==e:GetHandler()
+-- Targeting a monster tributed for the Ritual Summon
+function s.ritual_summon_target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tributed_filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tributed_filter,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.SelectTarget(tp,s.tributed_filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsAbleToRemove() end
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+-- Filter for tributed monsters
+function s.tributed_filter(c)
+	return c:IsReason(REASON_RITUAL) and c:IsAbleToHand()
 end
-function s.thfilter(c,e,tp)
-	return c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and c:IsAbleToHand() and c:IsCanBeEffectTarget(e)
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local tc=eg:GetFirst()
-	local mat=tc:GetMaterial()
-	if chkc then return mat:IsContains(chkc) and s.thfilter(chkc,e,tp) end
-	if chk==0 then return mat:IsExists(s.thfilter,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=mat:FilterSelect(tp,s.thfilter,1,1,nil,e,tp)
-	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+-- Returning the target to the hand
+function s.ritual_summon_operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
 	end
-end
-function s.evcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject()~=nil
-end
-function s.evop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	Duel.RaiseEvent(tc,EVENT_CUSTOM+id,e,0,tp,tp,0)
-	e:SetLabelObject(nil)
 end
