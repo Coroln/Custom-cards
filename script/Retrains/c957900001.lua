fd4f219fb73b34e3a86f0727b748cb5102a44638
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-01-04 19:35:16 +0100

    Added "Disposal of the Weak Twin"

---
+++ script/Retrains/c957900001.lua
@@ -0,0 +1,44 @@
+--竜の霊廟
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.tgfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsAbleToGrave()
+end
+function s.tgfilter2(c,tc)
+	return c:IsType(TYPE_EFFECT) and
+            (c:GetLevel() == tc:GetLevel()) and
+            (c:GetAttribute() == tc:GetAttribute()) and
+            (c:GetRace() == tc:GetRace()) and
+            (c:GetAttack() == tc:GetAttack()) and
+            (c:GetDefense() == tc:GetDefense())
+            and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE)
+		and Duel.IsExistingMatchingCard(s.tgfilter2,tp,LOCATION_DECK,0,1,nil,tc)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g1=Duel.SelectMatchingCard(tp,s.tgfilter2,tp,LOCATION_DECK,0,1,1,nil,tc)
+        if #g1>0 then
+            Duel.SendtoHand(g1,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,g1)
+        end
+	end
+end
