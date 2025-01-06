7104b3473566ccbbbecd92e7919e6f96ae48d076
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-01-06 23:59:14 +0100

    added "Chaos Gold Luster Soldier"

---
+++ script/All that is Golden/c957130005.lua
@@ -0,0 +1,98 @@
+--ファントム・オブ・ユベル
+--Phantom of Yubel
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Materials and Special Summon Procedure
+	Fusion.AddProcMix(c,true,true,CARD_DARK_MAGICIAN,957130001)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,true)
+
+    --immune spell
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+
+    --extra att
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+
+    --Shuffle 1 Normal Monster from your GY into the Deck and Special Summon this card
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetCondition(function(e) return e:GetHandler():IsLocation(LOCATION_GRAVE) end)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.tdop)
+    e3:SetCost(aux.SelfBanishCost)
+    c:RegisterEffect(e3)
+
+end
+s.listed_names={CARD_DARK_MAGICIAN,957130001}
+s.material={CARD_DARK_MAGICIAN,957130001}
+s.material_setcode={0x10a2}
+--Summon Proc
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToDeckOrExtraAsCost,tp,LOCATION_MZONE|LOCATION_GRAVE,0,nil)
+end
+function s.contactop(g,tp)
+	local fu,fd=g:Split(Card.IsFaceup,nil)
+	if #fu>0 then Duel.HintSelection(fu,true) end
+	if #fd>0 then Duel.ConfirmCards(1-tp,fd) end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST|REASON_MATERIAL)
+end
+function s.yubelfilter(c)
+	return c:IsSetCard(SET_YUBEL) and c:IsMonster() and (c:IsFaceup() or not c:IsOnField())
+end
+function s.chngtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.yubelfilter,tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK,0,1,e:GetHandler()) end
+end
+function s.chngop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Group.CreateGroup()
+	Duel.ChangeTargetCard(ev,g)
+	Duel.ChangeChainOperation(ev,s.repop)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(1-tp,s.yubelfilter,1-tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT,LOCATION_GRAVE,1-tp)
+	end
+end
+
+--e1
+
+function s.efilter(e,te)
+	return te:IsActiveType(TYPE_SPELL) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+end
+
+--e3
+
+function s.tgfilter(c)
+	return c:IsNormalSpell() and c:IsSSetable()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:GetControler()==tp and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and tc:IsSSetable() then
+        Duel.SSet(tp,tc)
+    end
+end
