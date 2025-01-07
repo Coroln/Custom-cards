1378cf262c00e72bb9933a216e0f76d368c72272
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-01-07 22:29:23 +0100

    added Elemental Trick Monsters

---
+++ script/Elemental Tricks/c957130008.lua
@@ -0,0 +1,71 @@
+Duel.LoadScript("proc_trick2.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Trick Summon
+	Trick.AddProcedure(c,nil,nil,{{s.tfilter,1,1}},{{s.tfilter2,1,1}})
+	c:EnableReviveLimit()
+    --cannot select battle target
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e1:SetValue(s.atlimit)
+	c:RegisterEffect(e1)
+
+    --atkdown
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+    e2:SetCost(s.spcost)
+    e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+
+function s.atlimit(e,c)
+	return c~=e:GetHandler() and c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and not c:IsCode(id)
+end
+
+--Trick Summon
+--Monster filter
+function s.tfilter(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+--Trap filter
+function s.tfilter2(c)
+	return c:IsTrap()
+end
+
+--e2
+function s.cfilter(c)
+	return c:IsTrap() and c:IsFacedown() and c:IsAbleToGraveAsCost()
+end
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+
+function s.tgfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler(),e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsAttribute(ATTRIBUTE_LIGHT) and tc:IsLevelBelow(4) and tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
