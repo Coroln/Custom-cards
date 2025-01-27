b76f94f86a358ba5c68d7e66f3b0b201b83c55e1
Coroln <76746930+Coroln@users.noreply.github.com>
2025-01-27 23:12:49 +0100

    Added "Fairy of the Forest"
    1 Fairy monster + Counter Trap
    If this card is Trick Summoned: You can move this card to another Main Monster Zone. This Effect cannot be negated. Once per turn, while "The Sanctuary in the Sky" is face-up on the field: You can send 1 Fairy monster from your Deck to the GY; add 1 Counter Trap from your Deck to your hand. You can only use each of the following effects of "Fairy of the Forest" once per turn. During your Main Phase: You can discard 1 card; either add to your hand or Special Summon 1 Level 4 LIGHT Fairy monster from your Deck or GY. If this card is destroyed: You can target 1 Counter Trap in your GY; Set it.

---
+++ script/Counter Fairy/c76175568.lua
@@ -0,0 +1,144 @@
+--Fairy of the Forest
+--Script by Coroln
+Duel.LoadScript("proc_trick2.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Trick Summon
+	Trick.AddProcedure(c,nil,nil,{{s.filter,1,1}},{{s.filter2,1,1}})
+	c:EnableReviveLimit()
+    --move zone
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_TRICK) end)
+	e1:SetOperation(s.tkop)
+	c:RegisterEffect(e1)
+    --Search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+    e2:SetCondition(s.thcon)
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+    --to hand or spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(s.thspcost)
+	e3:SetTarget(s.thsptg)
+	e3:SetOperation(s.thspop)
+	c:RegisterEffect(e3)
+    --Set 1 Counter Trap from your GY
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,4))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetTarget(s.settg)
+	e4:SetOperation(s.setop)
+	c:RegisterEffect(e4)
+end
+s.listed_names={CARD_SANCTUARY_SKY}
+--Trick Summon
+--Monster filter
+function s.filter(c)
+	return c:IsRace(RACE_FAIRY)
+end
+--Trap filter
+function s.filter2(c)
+	return c:IsTrap() and c:IsType(TYPE_COUNTER)
+end
+--test
+function s.tkop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+	local seq=math.log(Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0),2)
+	Duel.MoveSequence(c,seq)
+end
+--search
+function s.thcon(e)
+	return Duel.IsEnvironment(CARD_SANCTUARY_SKY)
+end
+function s.costfilter(c)
+	return c:IsRace(RACE_FAIRY) and c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.thfilter(c)
+	return c:IsTrap() and c:IsType(TYPE_COUNTER) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+--to hand or spsummon
+function s.thspcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.thspfilter(c,ft,e,tp)
+	return c:IsRace(RACE_FAIRY) and c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_LIGHT)
+		and (c:IsAbleToHand() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+end
+function s.thsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		return Duel.IsExistingMatchingCard(s.thspfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,ft,e,tp)
+	end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.thspop(e,tp,eg,ep,ev,re,r,rp)
+local c=e:GetHandler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thspfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,ft,e,tp):GetFirst()
+	if not sc then return end
+	aux.ToHandOrElse(sc,tp,
+		function(sc)
+			return ft>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		end,
+		function(sc)
+			return Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+		end,
+		aux.Stringid(id,3))
+end
+--Set 1 Counter Trap from your GY
+function s.setfilter(c)
+	return c:IsType(TYPE_COUNTER) and c:IsTrap() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,0)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.setfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+end
