6f5528d067bb529c1af838ac67ba96a642b36b8d
Coroln <76746930+Coroln@users.noreply.github.com>
2025-01-29 23:25:17 +0100

    Added " Shard of Hecatrice"
    This card can be used to Ritual Summon any Fairy Ritual Monster. You must also send Synchro Monsters from your Extra Deck to the GY whose total Level equal or exceed the Level of the Ritual Monster you Ritual Summon. You cannot Summon monsters the turn you activate this card, except Fairy monsters. You can banish this card from your GY; add 1 Level 4 or lower LIGHT Fairy monster from your Deck to your hand. You can only use this effect of " Shard of Hecatrice" once per turn. You can only activate 1 "Shard of Hecatrice" per turn.

---
+++ script/Counter Fairy/c61363113.lua
@@ -0,0 +1,46 @@
+-- Shard of Hecatrice
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=aux.FilterBoolFunction(Card.IsRace,RACE_FAIRY),extrafil=s.extrafil,extraop=s.extraop,matfilter=s.forcedgroup})
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+    --Search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,{id,1})
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+--Ritual Summon
+function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
+	return Duel.GetFieldGroup(tp,LOCATION_EXTRA,0)
+end
+function s.extraop(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+	return Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+end
+function s.forcedgroup(c,e,tp)
+	return c:IsLocation(LOCATION_EXTRA) and c:IsType(TYPE_SYNCHRO) and c:IsAbleToGrave()
+end
+--Search
+function s.thfilter(c)
+	return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevelBelow(4) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
