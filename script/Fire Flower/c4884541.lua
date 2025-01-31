20a7e7fe53270e7ff676fa89dbc2e6150028fecc
Coroln <76746930+Coroln@users.noreply.github.com>
2025-01-31 18:13:10 +0100

    Fixed "Martyr Rose"

--- script/Fire Flower/c4884541.lua
+++ script/Fire Flower/c4884541.lua
@@ -21,21 +21,17 @@ function s.spfilter(c,e,tp,pos)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_PLANT) and c:IsLevelBelow(4)
         and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
 end
-function s.sptg(pos)
-	return function(e,tp,eg,ep,ev,re,r,rp,chk)
-		if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,pos) end
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_GRAVE)
-	end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc,pos)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-function s.spop(pos)
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,POS_FACEUP)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			Duel.ConfirmCards(1-tp,g)
-		end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
