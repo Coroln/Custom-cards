f0bcb86181a28773c1ecd0c9a2807f7a0f37ab3c
Coroln <76746930+Coroln@users.noreply.github.com>
2025-01-31 18:15:03 +0100

    Update c4884541.lua

--- script/Fire Flower/c4884541.lua
+++ script/Fire Flower/c4884541.lua
@@ -17,11 +17,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 --special summon
-function s.spfilter(c,e,tp,pos)
+function s.spfilter(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_PLANT) and c:IsLevelBelow(4)
         and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc,pos)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
@@ -31,7 +31,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
