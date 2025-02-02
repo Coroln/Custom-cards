2c1964d657bd630fe82c3e8cbbb0c75c6699f40e
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-02-02 13:32:45 +0100

    Added "XYZ-Destroyer Slasher"

---
+++ script/XYZ_Destroyer/c957132003.lua
@@ -0,0 +1,65 @@
+--ヴェルズ・カストル
+--Evilswarm Castor
+local s,id=GetID()
+function s.initial_effect(c)
+	--extra summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetOperation(s.sumop)
+	c:RegisterEffect(e1)
+
+	-- Quick Effect: Gain ATK, DEF, and Level
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id)
+    e2:SetCost(s.cost)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
+end
+s.listed_series={0xa}
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)~=0 then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,0x1))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+end
+
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local g=Group.CreateGroup()
+	local mg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	for tc in aux.Next(mg) do
+		g:Merge(tc:GetOverlayGroup())
+	end
+	if chk==0 then return #g>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVEXYZ)
+	local sc=g:Select(tp,1,1,nil):GetFirst()
+
+	Duel.SendtoGrave(sc,REASON_COST)
+	Duel.RaiseSingleEvent(e:GetHandler(),EVENT_DETACH_MATERIAL,e,0,0,0,0)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.IsTurnPlayer(tp) and 3 or 2
+	--Gains ATK equal to those monsters' total original ATK
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(800)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END,ct)
+	e:GetHandler():RegisterEffect(e1)
+end
