3816bd374bd92f9e607b42a7319979e5a2dd0a55
Coroln <76746930+Coroln@users.noreply.github.com>
2025-02-02 14:53:14 +0100

    Added "Empress of the Abyss"
    [Pendulum Effect]
    You cannot Pendulum Summon. During your Main Phase: You can Special Summon 1 Reptile monster from your hand, except "Empress of the Abyss". If a Reptile monster you control is destroyed by battle, except "Empress of the Abyss": You can discard 1 card; Special Summon this card from your Pendulum Zone. You can only use each effect of "Empress of the Abyss" once per turn.
    ----------------------------
    [Monster Effect]
    Cannot be Normal Summoned/Set. Cannot be Pendulum Summoned. If this card is Special Summoned: You can send 1 monster from your Deck to the GY, then target 1 card on the field; shuffle it into the Deck. If this card you control is destroyed: You can target 1 Level 4 Reptile monster in your GY; Special Summon it, and if you do, place this card in your Pendulum Zone.

---
+++ script/Reptile Supp/c14378254.lua
@@ -0,0 +1,160 @@
+--Empress of the Abyss
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableUnsummonable()
+	Pendulum.AddProcedure(c)
+	--cannot pendulum summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetRange(LOCATION_PZONE)
+	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e0:SetTargetRange(1,0)
+	e0:SetTarget(s.splimit)
+	c:RegisterEffect(e0)
+    --Special summon 1 reptile from your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1,id)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+    --Special summon from Pendulum Scale
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1,{id,1})
+    e2:SetCost(s.spcost)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+    --Cannot be Pendulum Summoned
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e3:SetCode(EFFECT_SPSUMMON_CONDITION)
+    e3:SetValue(s.plimit)
+    c:RegisterEffect(e3)
+    --shuffle card into the deck
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetCost(s.tdcost)
+	e4:SetTarget(s.tdtg)
+	e4:SetOperation(s.tdop)
+	c:RegisterEffect(e4)
+    --special summon from gy
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_DESTROYED)
+	e5:SetCondition(s.sppcon)
+	e5:SetTarget(s.spptg)
+	e5:SetOperation(s.sppop)
+	c:RegisterEffect(e5)
+end
+s.listed_names={id}
+--cannot pendulum summon
+function s.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return (sumtype&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
+end
+--cannot be Pendulum Summoned
+function s.plimit(e,se,sp,st)
+    return se:IsHasType(EFFECT_TYPE_ACTIONS)
+end
+--Special summon 1 reptile from your hand
+function s.filter(c,e,sp)
+	return c:IsRace(RACE_REPTILE) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--Special summon from Pendulum Scale
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function s.cfilter(c,tp)
+	return c:IsRace(RACE_REPTILE) and not c:IsCode(id) and c:GetPreviousControler()==tp
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+end
+--shuffle card into the deck
+function s.cosfilter(c)
+    return c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cosfilter,tp,LOCATION_HAND,0,1,nil) end
+    local g1=Duel.SelectMatchingCard(tp,s.cosfilter,tp,LOCATION_DECK,0,1,1,nil)
+    Duel.SendtoGrave(g1,REASON_COST)
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToDeck() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+        Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
+--special summon from gy
+function s.sppcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) and e:GetHandler():IsPreviousControler(tp)
+end
+function s.pfilter(c,e,tp)
+	return c:IsRace(RACE_REPTILE) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.pfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.CheckPendulumZones(tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.sppop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 and not not Duel.CheckPendulumZones(tp) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.pfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) then
+            Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+        end
+	end
+end
