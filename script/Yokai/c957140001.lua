a0474844fcf72ac6994ee8be41c1c265f72ae04c
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-02-09 16:20:54 +0100

    "Chon Chon the Bitterfly" bug fix
    Used code from "Wind-Up Rabbit" as the reference

--- script/Yokai/c957140001.lua
+++ script/Yokai/c957140001.lua
@@ -47,26 +47,38 @@ end
 
 --e2
 function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-	local ph=Duel.GetCurrentPhase()
-	return not e:GetHandler():IsStatus(STATUS_CHAINING)
+	return e:GetHandler():IsAbleToRemove()
 end
 function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return e:GetHandler():IsAbleToRemove() end
 end
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		if Duel.Remove(c,0,REASON_EFFECT+REASON_TEMPORARY)==0 then return end
-		local e1=Effect.CreateEffect(c)
+	if c:IsRelateToEffect(e) and Duel.Remove(c,c:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)~=0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e1:SetRange(LOCATION_REMOVED)
 		e1:SetCountLimit(1)
-		e1:SetLabelObject(c)
+		if Duel.GetTurnPlayer()==tp then
+			if Duel.GetCurrentPhase()==PHASE_DRAW then
+				e1:SetLabel(Duel.GetTurnCount())
+			else
+				e1:SetLabel(Duel.GetTurnCount()+2)
+			end
+		else
+			e1:SetLabel(Duel.GetTurnCount()+1)
+		end
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetCondition(s.retcon)
 		e1:SetOperation(s.retop)
-		e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
-		Duel.RegisterEffect(e1,tp)
+		c:RegisterEffect(e1)
 	end
 end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==e:GetLabel()
+end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ReturnToField(e:GetLabelObject())
+	Duel.ReturnToField(e:GetHandler())
+	e:Reset()
 end
