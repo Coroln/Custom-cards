fb2d132b77a7f81c5eceff1a467b640d9fad230b
Coroln <76746930+Coroln@users.noreply.github.com>
2025-02-13 14:43:20 +0100

    Added 2 new monster
    "Harpie Lady Elders"
    Cannot be Normal Summoned/Set. Must first be Special Summoned by its own effect. After applying the effect of "Time Wizard" in which you called the coin toss right, you can: Tribute 1 "Harpie Lady"; Special Summon this card from your hand or Deck. This card's name becomes "Harpie Lady" while on the field or in the GY.  During your Main Phase: You can discard 1 card; return 1 monster on the field or GY to the hand. You can only use this effect of "Harpie Lady Elders" once per turn.
    ------------------------------
    "Dark Seeress"
    Cannot be Normal Summoned/Set. Must first be Special Summoned by its own effect. After applying the effect of "Time Wizard" in which you called the coin toss right, you can: Tribute 1 "Dark Magician Girl"; Special Summon this card from your hand or Deck. If this card is Special Summoned: You can add 1 Spell from your GY to your hand. You can only use this effect of "Dark Seeress" once per turn.

---
+++ script/Time Roulette/c41564202.lua
@@ -0,0 +1,90 @@
+--Harpie Lady Elders
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EVENT_CUSTOM+71625222)
+	e1:SetCondition(s.spcon)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetRange(LOCATION_DECK)
+	c:RegisterEffect(e2)
+    --Name becomes "Harpie Lady" while on the field or in the GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_CHANGE_CODE)
+	e3:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e3:SetValue(CARD_HARPIE_LADY)
+	c:RegisterEffect(e3)
+    --tohand
+    local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+    e4:SetCost(s.thcost)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+end
+s.listed_names={CARD_HARPIE_LADY,id}
+--Special summon
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp
+end
+function s.costfilter(c,ft,tp)
+	return c:IsCode(CARD_HARPIE_LADY)
+		and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5))
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil,ft,tp) end
+	if e:GetHandler():IsLocation(LOCATION_DECK) then
+		Duel.ConfirmCards(1-tp,e:GetHandler())
+	end
+	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil,ft,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,e:GetHandler():GetLocation())
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)~=0 then
+		c:CompleteProcedure()
+	end
+end
+--tohand
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.thfilter(c)
+    return c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
