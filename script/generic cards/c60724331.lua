93f4de2102242d2eb9e91f9c9d8d158d8a7295ef
Coroln <76746930+Coroln@users.noreply.github.com>
2025-02-23 12:12:17 +0100

    Added "Chaos Archfiend Nehemoth"
    [Pendulum Effect]
    You can Special Summon this card (from your Pendulum Zone) by banishing 1 LIGHT and 1 DARK monster from your GY. If this card is Special Summoned from your Pendulum Zone it gains this effect.
    ●When a card or effect is activated (Quick Effect): You can banish 1 card from your GY; negate the activation, and if you do, destroy it. You can only use this effect of "Chaos Archfiend Nehemoth" once per turn.
    ---------------------------------
    [Monster Effect]
    [1 LIGHT Tuner + 1+ LIGHT non-Tuner OR 1 DARK non-Tuner - 1 Dark Tuner]
    (This card is also Level -8)
    If this card is Synchro Summoned using a LIGHT monster as material it gains these effect.
    ●This card is treated as a LIGHT monster while face-up on the field.
    ●You can send 1 DARK monster from your Deck to the GY; Special Summon 1 LIGHT monster from your hand or GY.
    If this card is Synchro Summoned using a DARK monster as material it gains this effect.
    ●You can banish 1 LIGHT monster from your GY; add 1 DARK monster with the same Level from your Deck to your hand, then take 500 damage. You can only use the above effects of "Chaos Archfiend Nehemoth" once per turn. If this card is destroyed: You can place this card in your Pendulum Zone.

---
+++ script/generic cards/c60724331.lua
@@ -0,0 +1,259 @@
+--Chaos Archfiend Nehemoth
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+    c:AddSetcodesRule(id,false,0x601)
+	c:EnableReviveLimit()
+    --pendulum summon
+	Pendulum.AddProcedure(c,false)
+	--Synchro Summon Procedure
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),1,1,Synchro.NonTunerEx(Card.IsAttribute,ATTRIBUTE_LIGHT),1,99)
+    --dark synchro summon
+	Synchro.AddDarkSynchroProcedure(c,Synchro.NonTuner(Card.IsAttribute,ATTRIBUTE_DARK),nil,8)
+    --Special summon procedure from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.spcon1)
+	e1:SetTarget(s.sptg1)
+	e1:SetOperation(s.spop1)
+	c:RegisterEffect(e1)
+    --negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetRange(LOCATION_MZONE)
+    e2:SetCost(s.discost)
+	e2:SetCondition(s.discon)
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+    --Attribute LIGHT
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e3:SetRange(LOCATION_MZONE)
+    e3:SetCondition(s.thcon)
+	e3:SetValue(ATTRIBUTE_LIGHT)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_REMOVE_ATTRIBUTE)
+	e4:SetValue(ATTRIBUTE_DARK)
+	c:RegisterEffect(e4)
+    local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(s.valcheck)
+	e0:SetLabelObject(e3)
+	c:RegisterEffect(e0)
+    local e0a=e0:Clone()
+    e0a:SetLabelObject(e4)
+    c:RegisterEffect(e0a)
+    --Special Summon
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,{id,1})
+    e5:SetCondition(s.thcon)
+    e5:SetCost(s.cost)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+    local e0b=e0:Clone()
+    e0b:SetLabelObject(e5)
+    c:RegisterEffect(e0b)
+    --Search on being destroyed
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,1))
+	e6:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCountLimit(1,{id,2})
+	e6:SetCondition(s.thcon2)
+	e6:SetCost(s.thcost)
+	e6:SetTarget(s.thtg)
+	e6:SetOperation(s.thop)
+	c:RegisterEffect(e6)
+    local e0c=Effect.CreateEffect(c)
+	e0c:SetType(EFFECT_TYPE_SINGLE)
+	e0c:SetCode(EFFECT_MATERIAL_CHECK)
+	e0c:SetValue(s.valcheck2)
+	e0c:SetLabelObject(e6)
+	c:RegisterEffect(e0c)
+    --pendulum
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,2))
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e7:SetCode(EVENT_DESTROYED)
+	e7:SetProperty(EFFECT_FLAG_DELAY)
+	e7:SetCondition(s.pencon)
+	e7:SetTarget(s.pentg)
+	e7:SetOperation(s.penop)
+	c:RegisterEffect(e7)
+end
+--Special summon procedure from hand
+function s.rescon(sg,e,tp,mg)
+	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(s.atchk1,1,nil,sg)
+end
+function s.atchk1(c,sg)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and sg:FilterCount(Card.IsAttribute,c,ATTRIBUTE_DARK)==1
+end
+function s.spfilter1(c,att)
+	return c:IsAttribute(att) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.spcon1(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg1=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,ATTRIBUTE_LIGHT)
+	local rg2=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,ATTRIBUTE_DARK)
+	local rg=rg1:Clone()
+	rg:Merge(rg2)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-2 and #rg1>0 and #rg2>0 and aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,0)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local g=nil
+	local rg=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
+	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	g:DeleteGroup()
+end
+--negate
+function s.disfilter(c)
+	return c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.disfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.disfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) and e:GetHandler():IsSummonLocation(LOCATION_PZONE)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+--Attribute LIGHT
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT) then
+		e:GetLabelObject():SetLabel(1)
+	else
+		e:GetLabelObject():SetLabel(0)
+	end
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) and e:GetLabel()==1
+end
+--Special Summon
+function s.filter1(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoGrave(g:GetFirst(),REASON_COST)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--Search
+function s.valcheck2(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_DARK) then
+		e:GetLabelObject():SetLabel(2)
+	else
+		e:GetLabelObject():SetLabel(0)
+	end
+end
+function s.thcon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) and e:GetLabel()==2
+end
+function s.remfilter(c,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToRemoveAsCost() and c:IsMonster() and aux.SpElimFilter(c,true,false)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,c:GetLevel())
+end
+function s.thfilter(c,lv)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToHand() and c:IsMonster() and c:IsLevel(lv)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.remfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.SelectMatchingCard(tp,s.remfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
+	Duel.Remove(tc,POS_FACEUP,REASON_COST)
+	e:SetLabel(tc:GetLevel())
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local lv=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,lv)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+        Duel.Damage(tp,500,REASON_EFFECT)
+	end
+end
+--pendulum
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0 and c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckPendulumZones(tp) end
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckPendulumZones(tp) then return false end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
