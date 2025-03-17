0ca609d5d3691193aac509d725f35ac64dbca884
Coroln <76746930+Coroln@users.noreply.github.com>
2025-03-18 00:49:36 +0100

    Added "God Maju Garzett"
    Cannot be Normal Summoned/Set. Must be Special Summoed (from your hand) by Tributing up to 5 monsters you control. This card's ATK becomes the combined original ATK of the tributed monsters. Unaffected by your opponent's Monster and Spell effects. Once per turn: You can discard 1 card and target 1 monster your opponent controls; take control of that monster until the end of this turn. During the Standby Phase: Both player draw 1 card. At the start of your Main Phase 1: You can target 2 Fiend or Zombie monsters in your GY; Special Summon both of those targets. Immediately after this effect resolves, Xyz Summon 1 Xyz Monster using those 2 monsters only. You can only use this effect of "God Maju Garzett" once per turn. Once per turn: You can destroy all cards your opponent controls, and if you do, inflict 200 damage for each card destroyed. Once per turn: You can discard 1 card; Special Summon 1 Fiend or Zombie Tuner from your Deck or GY. Once per turn: You can add 1 "Ghost Fusion" from your Deck or GY. If this card is destroyed: You can Special Summon up to 3 Fiend or Zombie monsters from your hand or GY, but you cannot Special Summon Link Monsters for the rest of this turn, except Fiend or Zombie monsters.

---
+++ script/generic cards/c84377469.lua
@@ -0,0 +1,309 @@
+--God Maju Garzett
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+    --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(83965310,0))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+    --immune
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(s.efilter)
+	c:RegisterEffect(e3)
+    --Take control of an opponent's monster until end phase
+	local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_CONTROL)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetCost(s.sumcost)
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.activate)
+	c:RegisterEffect(e4)
+    --Draw
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_DRAW)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetCode(EVENT_PHASE+PHASE_STANDBY)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCountLimit(1)
+	e5:SetTarget(s.dtg)
+	e5:SetOperation(s.dop)
+	c:RegisterEffect(e5)
+    --Special Summon, then XYZ
+    local e6=Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id,2))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_MZONE)
+    e6:SetCountLimit(1,id)
+    e6:SetCondition(s.xyzcon)
+	e6:SetTarget(s.xyztg)
+	e6:SetOperation(s.xyzop)
+	c:RegisterEffect(e6)
+    --destroy, then burn
+    local e7=Effect.CreateEffect(c)
+    e7:SetDescription(aux.Stringid(id,3))
+	e7:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e7:SetType(EFFECT_TYPE_IGNITION)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetTarget(s.destg)
+	e7:SetOperation(s.desop)
+	c:RegisterEffect(e7)
+    --Special Summon a Tuner from Deck or GY
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,4))
+	e8:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e8:SetType(EFFECT_TYPE_IGNITION)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetCountLimit(1)
+	e8:SetCost(s.spdcost)
+	e8:SetTarget(s.spdtg)
+	e8:SetOperation(s.spdop)
+	c:RegisterEffect(e8)
+    --to hand
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,5))
+	e9:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e9:SetType(EFFECT_TYPE_IGNITION)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetCountLimit(1)
+	e9:SetTarget(s.thtg)
+	e9:SetOperation(s.thop)
+	c:RegisterEffect(e9)
+    --SP when Destroyed
+	local e10=Effect.CreateEffect(c)
+	e10:SetDescription(aux.Stringid(id,6))
+	e10:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e10:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e10:SetCode(EVENT_DESTROYED)
+	e10:SetProperty(EFFECT_FLAG_DELAY)
+	e10:SetTarget(s.sptg2)
+	e10:SetOperation(s.spop2)
+	c:RegisterEffect(e10)
+end
+--special summon
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.CheckReleaseGroup(c:GetControler(),aux.TRUE,1,false,5,true,c,c:GetControler(),nil,false,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,aux.TRUE,1,5,false,true,true,c,nil,nil,false,nil)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	local atk=0
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local batk=tc:GetTextAttack()
+		if batk>0 then
+			atk=atk+batk
+		end
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(atk)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	c:RegisterEffect(e1)
+	g:DeleteGroup()
+end
+--immune
+function s.efilter(e,te)
+	return (te:IsActiveType(TYPE_SPELL) or te:IsActiveType(TYPE_MONSTER)) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+end
+--Take control of an opponent's monster until end phase
+function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsControlerCanBeChanged() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.GetControl(tc,tp,PHASE_END,1)
+	end
+end
+--Draw
+function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.dop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+    Duel.Draw(1-tp,d,REASON_EFFECT)
+end
+--Special Summon, then XYZ
+function s.xyzcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsPhase(PHASE_MAIN1) and not Duel.CheckPhaseActivity()
+end
+function s.filter(c,e,tp)
+	return (c:IsRace(RACE_FIEND) or c:IsRace(RACE_ZOMBIE)) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.xyzfilter(c,mg,tp,chk)
+	return c:IsXyzSummonable(nil,mg,2,2) and (not chk or Duel.GetLocationCountFromEx(tp,tp,mg,c)>0)
+end
+function s.mfilter1(c,mg,exg,tp)
+	return mg:IsExists(s.mfilter2,1,c,c,exg,tp)
+end
+function s.zonecheck(c,tp,g)
+	return Duel.GetLocationCountFromEx(tp,tp,g,c)>0 and c:IsXyzSummonable(nil,g)
+end
+function s.mfilter2(c,mc,exg,tp)
+	local g=Group.FromCards(c,mc)
+	return exg:IsExists(s.zonecheck,1,nil,tp,Group.FromCards(c,mc),tp)
+end
+function s.xyztg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	local exg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,mg)
+	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
+		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and mg:IsExists(s.mfilter1,1,nil,mg,exg,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg1=mg:FilterSelect(tp,s.mfilter1,1,1,nil,mg,exg,tp)
+	local tc1=sg1:GetFirst()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg2=mg:FilterSelect(tp,s.mfilter2,1,1,tc1,tc1,exg,tp)
+	sg1:Merge(sg2)
+	Duel.SetTargetCard(sg1)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg1,2,0,0)
+end
+function s.filter2(c,e,tp)
+	return c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.filter2,nil,e,tp)
+	if #g<2 then return end
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	Duel.BreakEffect()
+	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,g,tp,true)
+	if #xyzg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
+		Duel.XyzSummon(tp,xyz,nil,g)
+	end
+end
+--damage, then burn
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,sg:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,sg:GetCount()*200)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	local sg=Duel.Destroy(sg,REASON_EFFECT)
+	Duel.Damage(1-tp,sg*200,REASON_EFFECT)
+end
+--Special Summon a Tuner from Deck or GY
+function s.spdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.spdfilter(c,e,tp)
+	return (c:IsRace(RACE_FIEND) or c:IsRace(RACE_ZOMBIE)) and c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spdfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_DECK)
+end
+function s.spdop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spdfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--tohand
+function s.filter(c)
+	return c:IsCode(35705817) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+--SP when Destroyed
+function s.spfiltermax(c,e,tp)
+	return (c:IsRace(RACE_FIEND) or c:IsRace(RACE_ZOMBIE)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfiltermax,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local ct=3
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
+	ct=math.min(ct,Duel.GetLocationCount(tp,LOCATION_MZONE))
+	if ct<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfiltermax),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,ct,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetTargetRange(1,0)
+		e1:SetTarget(s.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+    return c:IsType(TYPE_LINK) and not (c:IsRace(RACE_FIEND) or c:IsRace(RACE_ZOMBIE))
+end
