7a1c80df3343def17743ae37437f5abb2bcbb3e6
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-03-23 16:38:19 +0100

    added Storm Dragon Card (No Archtype currently)

---
+++ script/Storming Dragons/c957130007.lua
@@ -0,0 +1,177 @@
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Quick-Play aktivieren
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_DAMAGE)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(0,TIMING_END_PHASE)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+end
+
+---------------------------------------------
+-- 1) Hilfsfunktionen/Filter
+---------------------------------------------
+-- Filter A: Für das Senden als Kosten (Hand/Deck/Feld)
+function s.cost_filter(c)
+    return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL) and c:IsAbleToGraveAsCost()
+end
+
+-- Filter B: Für das Zählen von Drachen-Normalen im GY
+function s.gy_filter(c)
+    return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL)
+end
+
+-- Filter zum Hinzufügen auf die Hand (für Option 4)
+function s.thfilter(c)
+    return c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+
+---------------------------------------------
+-- 2) Zielauswahl (target)
+---------------------------------------------
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        -- Meistens reicht hier "return true", 
+        -- da wir immer aktivieren dürfen.
+        return true
+    end
+    
+    -- Wir wählen bis zu 3 passende Monster (Kosten)
+    local g=Duel.SelectMatchingCard(tp,s.cost_filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_ONFIELD,0,1,3,nil)
+    local ct=#g
+    if ct>0 then
+        Duel.SendtoGrave(g,REASON_COST)
+    end
+    
+    -- Wir legen ein Lua-table an, um ct und op_sel zu speichern
+    local data={}
+    data.ct = ct       -- Anzahl gesendeter Monster
+    data.op_sel = 0    -- welche Effektoption der Spieler wählt (Standard: 0 = keine)
+
+    -- Nur wenn genau 3 Monster gesendet wurden, fragen wir die Bonuseffekte ab
+    if ct==3 then
+        -- Zähle jetzt alle Drachen-Normalen im Friedhof
+        local gc=Duel.GetMatchingGroupCount(s.gy_filter,tp,LOCATION_GRAVE,0,nil)
+        
+        -- Liste der möglichen Effekte
+        local op_table={}
+        local op_val={}
+        
+        -- 3+ : Ziehe 1 Karte
+        if gc>=3 then
+            table.insert(op_table,aux.Stringid(id,0)) -- "Draw 1 card"
+            table.insert(op_val,1)
+        end
+        -- 5+ : Special Summon aus GY
+        if gc>=5 then
+            table.insert(op_table,aux.Stringid(id,1)) -- "Special Summon"
+            table.insert(op_val,2)
+        end
+        -- 7+ : Zerstöre 1 Karte
+        if gc>=7 then
+	        if Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) then
+                table.insert(op_table,aux.Stringid(id,2)) -- "Destroy 1 card"
+                table.insert(op_val,3)
+            end
+        end
+        -- 10+ : Monster suchen + Burn
+        if gc>=10 and Duel.IsExistingMatchingCard(s.Searchfilter,tp,LOCATION_DECK,0,1,nil) then
+            table.insert(op_table,aux.Stringid(id,3)) -- "Add 1 monster + burn"
+            table.insert(op_val,4)
+        end
+        -- 20+ : Du gewinnst das Duell
+        if gc==20 then
+            table.insert(op_table,aux.Stringid(id,4)) -- "Win the Duel"
+            table.insert(op_val,5)
+        end
+        
+        -- Wenn mindestens eine Option möglich ist
+        if #op_table>0 then
+            Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,5))
+            local sel=Duel.SelectOption(tp,table.unpack(op_table))
+            data.op_sel = op_val[sel+1] -- Effektwahl speichern
+        end
+    end
+    
+    -- Speichere das gesamte table in der LabelObject
+    e:SetLabelObject(data)
+end
+
+function s.Searchfilter(c)
+	return c:IsType(TYPE_MONSTER)
+end
+
+---------------------------------------------
+-- 3) Effekt ausführen (activate)
+---------------------------------------------
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    -- Das table auslesen
+    local data=e:GetLabelObject()
+    if not data then return end
+    
+    local ct = data.ct
+    local op_sel = data.op_sel
+    
+    -- Wenn nicht genau 3 gesendet oder keine Option gewählt
+    if ct~=3 or op_sel==0 then return end
+    
+    -- Nun je nach op_sel den Effekt ausführen
+    if op_sel==1 then
+        -- 1) Ziehe 1 Karte
+        Duel.Draw(tp,1,REASON_EFFECT)
+        
+    elseif op_sel==2 then
+        -- 2) Special Summon 1 Drachen-Normalmonster in DEF
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.gy_filter,tp,LOCATION_GRAVE,0,1,1,nil)
+        if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 then
+            local tc=g:GetFirst()
+            -- Kann diese Runde nicht angreifen
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_CANNOT_ATTACK)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e1)
+            -- Kann nicht als Material für Fusion/Synchro/Xyz/Link benutzt werden
+            local e2=Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+            e2:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+            e2:SetValue(function(e,rc,sumtype)
+                return (sumtype&SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+                    or (sumtype&SUMMON_TYPE_SYNCHRO)==SUMMON_TYPE_SYNCHRO
+                    or (sumtype&SUMMON_TYPE_XYZ)==SUMMON_TYPE_XYZ
+                    or (sumtype&SUMMON_TYPE_LINK)==SUMMON_TYPE_LINK
+            end)
+            e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e2)
+        end
+        
+    elseif op_sel==3 then
+        -- 3) Zerstöre 1 gegnerische Karte
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+        local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+        if #dg>0 then
+            Duel.HintSelection(dg)
+            Duel.Destroy(dg,REASON_EFFECT)
+        end
+        
+    elseif op_sel==4 then
+        -- 4) Füge 1 Monster (Deck/GY) deiner Hand hinzu und füge Burn-Damage zu
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local tg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+        if #tg>0 and Duel.SendtoHand(tg,nil,REASON_EFFECT)>0 then
+            Duel.ConfirmCards(1-tp,tg)
+            -- 200 Schaden für jede Karte auf dem Spielfeld
+            local count=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+            Duel.Damage(1-tp,count*200,REASON_EFFECT)
+        end
+        
+    elseif op_sel==5 then
+        -- 5) Du gewinnst das Duell
+        Duel.Win(tp,0x60) -- oder Duel.Win(tp,0)
+    end
+end
---
+++ script/Storming Dragons/c957130017.lua
@@ -0,0 +1,110 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--send to grave
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+
+    --to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_TO_GRAVE)
+    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetCountLimit(1,{id,1})
+	e4:SetTarget(s.targete4)
+	e4:SetOperation(s.operatione4)
+	c:RegisterEffect(e4)
+
+    --atk,def up
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e5:SetCode(EVENT_BATTLE_START)
+	e5:SetHintTiming(TIMING_BATTLE_PHASE)
+	e5:SetRange(LOCATION_GRAVE)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e5:SetCountLimit(1,{id,2})
+	e5:SetCondition(s.conditione5)
+	e5:SetCost(aux.SelfBanishCost)
+	e5:SetOperation(s.operatione5)
+	c:RegisterEffect(e5)
+end
+function s.tgfilter(c)
+	return c:IsMonster() and c:IsType(TYPE_NORMAL) and c:IsAbleToGrave()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+        if e:GetHandler():IsAttackPos() then
+            Duel.BreakEffect()
+            Duel.ChangePosition(e:GetHandler(),POS_FACEUP_DEFENSE)
+        end
+	end
+end
+--e4
+function s.filter(c)
+	return c:IsAttackBelow(2700) and c:IsRace(RACE_DRAGON) and c:IsAbleToHand()
+end
+function s.targete4(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED)
+end
+function s.operatione4(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
+	if tc~=nil then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+
+--e5
+
+function s.filtere5(c)
+	return c:IsRace(RACE_DRAGON) and c:IsAttackBelow(2700)
+end
+function s.conditione5(e,tp,eg,ep,ev,re,r,rp)
+	local phase=Duel.GetCurrentPhase()
+	if phase~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return (a:GetControler()==tp and s.filtere5(a) and a:IsRelateToBattle())
+		or (d and d:GetControler()==tp and d:IsFaceup() and s.filtere5(d) and d:IsRelateToBattle())
+end
+function s.operatione5(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	if Duel.GetTurnPlayer()~=tp then a=Duel.GetAttackTarget() end
+	if not a:IsRelateToBattle() then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END, 2)
+	e1:SetValue(1800)
+	a:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	a:RegisterEffect(e2)
+end
