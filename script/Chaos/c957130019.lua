c7b07ceb86fedec74e7cd4426dfb131e631b6943
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-04-20 19:40:05 +0200

    added "Mythical Chaos Angel - Belias Thode"

---
+++ script/Chaos/c957130019.lua
@@ -0,0 +1,130 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+    --win
+	local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.e1Con)
+    e1:SetCost(s.e1Cost)
+    e1:SetTarget(s.e1Target)
+	e1:SetOperation(s.e1OP)
+	c:RegisterEffect(e1)
+
+    --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+    e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.e2Con)
+	e2:SetTarget(s.e2Target)
+	e2:SetOperation(s.e2OP)
+	c:RegisterEffect(e2)
+
+    
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,2})
+    e3:SetCost(aux.SelfBanishCost)
+	e3:SetTarget(s.e3thtg)
+	e3:SetOperation(s.e3thop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x21cf}
+s.listed_series={0xcf}
+function s.e1Con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetBattledGroupCount()==0
+end
+function s.cfilter(c)
+	return c:IsMonster() and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) and c:IsRace(RACE_FAIRY) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c)
+end
+function s.e1Cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.e1Target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToGrave()end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+
+function s.e1OP(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        local opCount = Duel.GetFieldGroupCount(1-tp, LOCATION_ONFIELD, 0)
+        local tpCount = Duel.GetFieldGroupCount(tp, LOCATION_ONFIELD, 0)
+        if opCount > tpCount and Duel.SelectYesNo(tp, aux.Stringid(id, 0)) then
+            Duel.Remove(tc, POS_FACEUP, REASON_EFFECT)
+        else
+            Duel.SendtoGrave(tc, REASON_EFFECT)
+        end
+    end
+end
+
+--e2
+
+function s.e2Con(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsLocation(LOCATION_GRAVE) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+
+function s.thfilter(c)
+	return c:IsMonster() and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) and c:IsRace(RACE_FAIRY) and c:HasLevel() and c:IsLevelBelow(4)
+		and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.spfilter(c,e,tp)
+	return c:IsMonster() and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) and c:IsRace(RACE_FAIRY) and c:HasLevel() and c:IsLevelBelow(4)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp)
+		and not c:IsCode(id) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_REMOVED,0,1,c)
+end
+function s.e2Target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
+end
+function s.e2OP(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD)>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g1=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+        if #g1==0 then return end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g2=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,g1)
+        if #g2==0 then return end
+        Duel.SpecialSummon(g1,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE)
+        Duel.SendtoHand(g2,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g2)
+    end
+end
+
+--e3
+function s.e3filter(c)
+	return c:IsRitualSpell() and c:IsAbleToHand() and c:IsSetCard(0xCF)
+end
+function s.e3thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.e3filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.e3thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.e3filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
