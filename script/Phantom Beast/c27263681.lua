7ce07a0a1fecc2a2e7bdf0342f760896fd71bc40
Coroln <76746930+Coroln@users.noreply.github.com>
2025-04-23 00:33:14 +0200

    Added "Phantom Beast Gryphon"
    You can Special Summon this card (from your hand) by sending 1 "Phantom Beast" monster , Beast, Illusion, or Level 5 Fiend monster from your Deck to the GY, except "Phantom Beast Gryphon". If this card battles a monster, neither can be destroyed by that battle. Once per turn: You can Special Summon 1 "Phantom Beast" monster from your hand, GY, or Banishment, except "Phantom Beast Gryphon". While this card is your GY, all Beast-Warrior monsters you control, in your hand, GY, or Banishment, become Beast monster, also all monster gain 300 ATK/DEF.

---
+++ script/Phantom Beast/c27263681.lua
@@ -0,0 +1,89 @@
+--Phantom Beast Gryphon
+--Sript by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+    --special summon (self)
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+    --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+    --atkup
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetValue(300)
+	c:RegisterEffect(e3)
+    --change race
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_FIELD)
+    e4:SetCode(EFFECT_CHANGE_RACE)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_HAND,0)
+    e4:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_BEASTWARRIOR))
+    e4:SetValue(RACE_BEAST)
+    c:RegisterEffect(e4)
+end
+s.listed_names={id}
+s.listed_series={SET_PHANTOM_BEAST}
+--special summon (self)
+function s.spfilter(c)
+    return (c:IsRace(RACE_BEAST) or c:IsRace(RACE_ILLUSION) or (c:IsSetCard(SET_PHANTOM_BEAST) and c:IsMonster()) or (c:IsRace(RACE_FIEND) and c:IsLevel(5))) and not c:IsCode(id)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil)
+	return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0,c)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_DISCARD|REASON_COST)
+	g:DeleteGroup()
+end
+--special summon
+function s.filter(c,e,sp)
+	return c:IsSetCard(0x1B) and c:IsMonster() and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
