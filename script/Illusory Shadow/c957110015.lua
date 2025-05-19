82e0c2734433272d8bc74b6680b7b39cd5c4a144
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-05-19 16:29:34 +0200

    added "Illussion Nightmare Creation"

---
+++ script/Illusory Shadow/c957110015.lua
@@ -0,0 +1,120 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+	--GY Effect: Special Summon from S&T Zone
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_LEAVE_GRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.coste2)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+
+	--To Deck
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TODECK)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(Cost.SelfToDeck)
+    e3:SetTarget(s.e3tg)
+	e3:SetOperation(s.e3op)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0xBBB}
+function s.costfilter(c)
+	return c:IsSetCard(0xBBB) and c:IsMonster() and c:IsDiscardable()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) and Duel.IsPlayerCanDiscardDeck(tp,2)
+		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=4 end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+	Duel.BreakEffect()
+	Duel.DiscardDeck(tp,2,REASON_EFFECT)
+end
+
+--Effect 2: GY – Special Summon monster from S/T Zone
+function s.filter(c,e,sp)
+	return c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,sp,true,false)
+end
+function s.coste2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Debug.Message("co chk")
+	Duel.Remove(c,POS_FACEUP,REASON_COST)
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST|REASON_DISCARD,nil)
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_SZONE,0,1,nil,e,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_SZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP) then
+		if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+			Duel.SSet(tp,c)
+			-- Banish when it leaves the field
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(3300)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+			e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+			e1:SetValue(LOCATION_REMOVED)
+			c:RegisterEffect(e1)
+		end
+	end
+end
+--e3
+function s.e3filter(c)
+	return c:IsMonster() and c:IsSetCard(0xBBB) and c:IsAbleToDeck()
+end
+function s.e3tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and s.e3filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.e3filter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.e3filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,g,1,tp,LOCATION_GRAVE)
+end
+function s.e3op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc then
+		Duel.ShuffleDeck(tp)
+		Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)
+		Duel.ConfirmDecktop(tp,1)
+	end
+end
