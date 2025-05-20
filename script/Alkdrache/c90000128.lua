861b1eaf298f6397e774363e52992a9a98c17d66
creasycat <129886096+creasycat@users.noreply.github.com>
2025-05-21 00:35:13 +0200

    Added 2 new Alcdragon Cards

---
+++ script/Alkdrache/c90000128.lua
@@ -0,0 +1,44 @@
+--Alcdragon Grenouille de Champagne
+local s,id=GetID()
+function s.initial_effect(c)
+	--Equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_HAND|LOCATION_MZONE)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
+	c:RegisterEffect(e1)
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp)
+		 and c~=chkc and chkc:IsFaceup() and chkc:IsSetCard(0x14ec) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsSetCard,0x14ec),tp,LOCATION_MZONE,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsSetCard,0x14ec),tp,LOCATION_MZONE,0,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if c:IsLocation(LOCATION_MZONE) and c:IsFacedown() then return end
+	local tc=Duel.GetFirstTarget()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and tc
+		and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Equip(tp,c,tc,true)
+		--Equip limit
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetValue(function(e,c)return c==e:GetLabelObject()end)
+		e1:SetLabelObject(tc)
+		c:RegisterEffect(e1)
+	else
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
---
+++ script/Alkdrache/c90000129.lua
@@ -0,0 +1,66 @@
+--Alcdragon Jonas the Jäger
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add 1 "Alcdragon" monsters from the Deck or GY to the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+
+function s.rf(c) --returnfilter
+	return c:IsSetCard(0x14ec) and c:IsMonster() and c:IsAbleToDeck()
+end
+function s.sf(c) --sendfilter
+	return c:IsSetCard(0x14ec) and c:IsMonster() and c:IsAbleToGrave()
+end
+function s.filter(c)
+	return c:IsSetCard(0x14ec) and c:IsLevelBelow(4) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+			Duel.ConfirmCards(1-tp,g)
+			local b1=Duel.IsExistingMatchingCard(s.rf,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+			local b2=Duel.IsExistingMatchingCard(s.sf,tp,LOCATION_DECK,0,1,nil,e,tp)
+			if not ((b1 or b2) and Duel.SelectYesNo(tp,aux.Stringid(id,1))) then return end
+			local op=Duel.SelectEffect(tp,
+				{b1,aux.Stringid(id,2)},
+				{b2,aux.Stringid(id,3)})
+			if op==1 then
+				-- Target 1 "Alcdragon" monster in your GY; return that target to the Deck.
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+				local g=Duel.SelectMatchingCard(tp,s.rf,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+				if #g>0 then
+					Duel.BreakEffect()
+					Duel.SendtoDeck(g,nil,2,REASON_EFFECT) --2=Deck+Shuffle
+				end
+			elseif op==2 then
+				-- Send 1 "Alcdragon" monster from your Deck to the GY.
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+				local sc=Duel.SelectMatchingCard(tp,s.sf,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+				if not sc then return end
+				Duel.BreakEffect()
+				Duel.SendtoGrave(sc,REASON_EFFECT,tp)
+			end
+		end
+	end
+end
