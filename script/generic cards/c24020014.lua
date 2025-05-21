316fbb745366382deefbbe4c66c2477ce077b73f
Coroln <76746930+Coroln@users.noreply.github.com>
2025-05-21 11:51:17 +0200

    fixed bug

--- script/generic cards/c24020014.lua
+++ script/generic cards/c24020014.lua
@@ -38,6 +38,7 @@ function s.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_DESTROYED)
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e4:SetCondition(s.eqcon)
 	e4:SetTarget(s.eqtg)
 	e4:SetOperation(s.eqop)
 	c:RegisterEffect(e4)
@@ -74,6 +75,9 @@ function s.val(e,c)
 	return Duel.GetMatchingGroupCount(s.filter,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil)*200
 end
 --equip
+function s.eqcon(c)
+	return c:IsPreviousLocation(LOCATION_MZONE)
+end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
