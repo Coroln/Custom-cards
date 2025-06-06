0851ae3f73c99adbc3001527def77a929cdbe9de
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-06-06 16:36:53 +0200

    "Wonders of the Ocean" balancing update
    Discard 1 Water Monster; Add 1 "Umiiruka" from your Deck to your hand, also, Set this card instead of sending it to the GY.
    If this card is used as material for the Summon of a WATER Trick monster: That monster gains the following effects.
    ● Once per turn, if this card would be destroyed, you can discard 1 Water monster instead.

--- script/Elemental Tricks/c957130031.lua
+++ script/Elemental Tricks/c957130031.lua
@@ -22,7 +22,7 @@ function s.initial_effect(c)
     e2:SetOperation(s.opEffGain)
     c:RegisterEffect(e2)
 end
-s.listed_names={02084239,CARD_UMI}
+s.listed_names={82999629}
 function s.costfilter(c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsDiscardable()
 end
@@ -32,7 +32,7 @@ function s.sgcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.filter(c)
-	return (c:IsCode(02084239) or c:IsCode(CARD_UMI)) and c:IsAbleToHand()
+	return c:IsCode(82999629) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
@@ -64,86 +64,34 @@ end
 function s.opEffGain(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local trickmon=c:GetReasonCard()
-    --Destroy replace
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e1:SetCondition(s.damcon)
-	e1:SetOperation(s.damop)
-    e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-	trickmon:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetCode(EFFECT_DESTROY_REPLACE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(s.reptg)
-	e2:SetOperation(s.repop)
-    e2:SetReset(RESET_EVENT|RESETS_STANDARD)
-	trickmon:RegisterEffect(e2)
-    
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetCode(EFFECT_UPDATE_ATTACK)
-	e3:SetValue(200)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(function(e)return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_UMI),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) or Duel.IsEnvironment(CARD_UMI)or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,02084239),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) or Duel.IsEnvironment(02084239)end)
-    e3:SetReset(RESET_EVENT|RESETS_STANDARD)
-	trickmon:RegisterEffect(e3)
 
-    local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e4:SetCode(EFFECT_UPDATE_ATTACK)
-	e4:SetValue(200)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetReset(RESET_EVENT|RESETS_STANDARD)
-	trickmon:RegisterEffect(e4)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1)) -- Neue Beschreibung
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.reptg_new)
+	e1:SetOperation(s.repop_new)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	trickmon:RegisterEffect(e1)
 end
 
---e2
-
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+function s.reptg_new(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local bc=c:GetBattleTarget()
-	if not bc or c:GetEffectCount(EFFECT_INDESTRUCTABLE_BATTLE)>0 then return false end
-	if bc==Duel.GetAttackTarget() and bc:IsDefensePos() then return false end
-	if c:IsAttackPos() and bc:IsDefensePos() and bc:GetEffectCount(EFFECT_DEFENSE_ATTACK)==1
-		and c:GetAttack()<=bc:GetDefense() then return true end
-	if c:IsAttackPos() and (bc:IsAttackPos() or bc:IsHasEffect(EFFECT_DEFENSE_ATTACK))
-		and c:GetAttack()<=bc:GetAttack() then return true end
-	if c:IsDefensePos() and bc:IsDefensePos() and bc:GetEffectCount(EFFECT_DEFENSE_ATTACK)==1
-		and c:GetDefense()<bc:GetDefense() then return true end
-	if c:IsDefensePos() and (bc:IsAttackPos() or bc:IsHasEffect(EFFECT_DEFENSE_ATTACK))
-		and c:GetDefense()<bc:GetAttack() then return true end
+	if chk==0 then
+		return not c:IsReason(REASON_REPLACE)
+			and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_HAND,0,1,nil)
+			and c:GetFlagEffect(id)==0
+	end
+	if Duel.SelectEffectYesNo(tp,c,aux.Stringid(id,1)) then
+		c:RegisterFlagEffect(id,RESET_PHASE+PHASE_END,0,1) -- Soft once per turn
+		return true
+	end
 	return false
 end
-function s.repfilter(c)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsDiscardable()
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToBattle() and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_HAND,0,1,c)
-		and Duel.SelectEffectYesNo(tp,c,96) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-        Duel.DiscardHand(tp,s.repfilter,1,1,REASON_EFFECT|REASON_DISCARD)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-		e1:SetValue(1)
-		e1:SetReset(RESET_PHASE|PHASE_DAMAGE)
-		c:RegisterEffect(e1)
-	end
-end
-function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT)
-		and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_HAND,0,1,c) end
-	if Duel.SelectEffectYesNo(tp,c,96) then return true
-	else return false end
-end
-function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-	Duel.DiscardHand(tp,s.repfilter,1,1,REASON_EFFECT|REASON_DISCARD)
+
+function s.repop_new(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	Duel.DiscardHand(tp,s.repfilter,1,1,REASON_EFFECT+REASON_DISCARD)
 end
