256b708496df967c186118cfadf8833e114cafc4
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-06-06 19:46:54 +0200

    added "Quaking the Earth" (Normal Trap/EARTH Trick monster Support)
    Change all monsters in this column to face-down Defense Position (if any), also swap the ATK and DEF of all monsters until the End Phase of the next turn, also neither player takes battle damage this turn. If this card is used as material for the Summon of an EARTH Trick monster: That monster gains the following effect:
    ● If it attacks a Defense Position monster, inflict piercing battle damage to your opponent.
    ● Each time a Trap Card is activated, this card gains 200 ATK until the End Phase of the fourth turn after this effect resolves (including this turn).

---
+++ script/Elemental Tricks/c957130032.lua
@@ -0,0 +1,111 @@
+local s,id=GetID()
+function s.initial_effect(c)
+--Change all monsters that were not Normal/Special Summoned this turn to Defense Position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_BE_MATERIAL)
+    e2:SetCondition(s.conEffGain)
+    e2:SetOperation(s.opEffGain)
+    c:RegisterEffect(e2)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE)
+		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SetTargetCard(g)
+end
+function s.thfilter(c,g)
+	return c:IsAttackPos() and c:IsCanChangePosition() and g:IsContains(c)
+end
+function s.filter(c,e)
+	return c:IsFaceup() and c:IsRelateToEffect(e) and not c:IsImmuneToEffect(e)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local cg=c:GetColumnGroup()
+	cg:AddCard(c)
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,cg)
+		if #g>0 then
+		    Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+	    end
+	end
+
+    local sg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
+	local tc=sg:GetFirst()
+	for tc in aux.Next(sg) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SWAP_BASE_AD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+		tc:RegisterEffect(e1)
+	end
+
+    local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetValue(0)
+	e2:SetReset(RESET_PHASE|PHASE_END,2)
+	Duel.RegisterEffect(e2,tp)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	e3:SetReset(RESET_PHASE|PHASE_END,2)
+	Duel.RegisterEffect(e3,tp)
+	local e4=Effect.CreateEffect(e:GetHandler())
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetReset(RESET_PHASE|PHASE_END,2)
+	e4:SetTargetRange(1,1)
+	Duel.RegisterEffect(e4,tp)
+end
+
+--e2
+
+function s.conEffGain(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (r&REASON_TRICK) and not c:IsSummonType(SUMMON_TYPE_MAXIMUM) and c:GetReasonCard():IsAttribute(ATTRIBUTE_EARTH)
+end
+
+function s.opEffGain(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local trickmon=c:GetReasonCard()
+	local e1=Effect.CreateEffect(c)
+	e1:SetRange(LOCATION_MZONE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_PIERCE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	trickmon:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_CHAIN_SOLVED)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.acop)
+	trickmon:RegisterEffect(e2)
+end
+
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+    c=e:GetHandler()
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsTrapEffect() then
+		local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetReset(RESETS_STANDARD_PHASE_END,4)
+        e1:SetValue(200)
+        c:RegisterEffect(e1)
+	end
+end
