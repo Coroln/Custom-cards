02a7b44dd3b6ea1a628a2d969b52fda91845915a
Coroln <76746930+Coroln@users.noreply.github.com>
2025-06-09 19:20:05 +0200

    Added "Chaos Kuriboh"
    Cannot be used as Link material, except for the Link Summon of a "Kuriboh" monster. While face-up on the field or in your GY, this card is also DARK-Attribute. When you would take damage (by battle or effect): You can discard this card; You take no damage. When an attack is declared: You can banish this card from your field or GY; Special Summon 1 "Kuriboh" monster from your GY or Banishment, except "Chaos Kuriboh". You can only use this effect of "Chaos Kuriboh" once per turn.

---
+++ script/Kuriboh/c41672374.lua
@@ -0,0 +1,118 @@
+--Chaos Kuriboh
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--link limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e1:SetValue(s.linklimit)
+	c:RegisterEffect(e1)
+	--attribute
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e2:SetValue(ATTRIBUTE_LIGHT)
+	c:RegisterEffect(e2)
+	--no damage (battle)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_QUICK_O+EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e3:SetCondition(s.con1)
+	e3:SetCost(s.cost)
+	e3:SetOperation(s.op1)
+	c:RegisterEffect(e3)
+	--no damage (effect)
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetCondition(aux.damcon1)
+	e4:SetCost(s.cost)
+	e4:SetOperation(s.op2)
+	c:RegisterEffect(e4)
+	--spsummon
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e5:SetCountLimit(1,id)
+	e5:SetCost(s.spcost)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0xA4}
+s.listed_names={id}
+--link limit
+function s.linklimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(0xA4)
+end
+--no damage (battle)
+function s.con1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetBattleDamage(tp)>0
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e1:SetOperation(s.damop)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
+end
+--no damage (effect)
+function s.op2(e,tp,eg,ep,ev,re,r,rp,val,r,rc)
+	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetLabel(cid)
+	e1:SetValue(s.refcon)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.refcon(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or r&REASON_EFFECT==0 then return end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if cid==e:GetLabel() then e:SetLabel(val) return 0
+	else return val end
+end
+--spsummon
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemoveAsCost() and (c:IsLocation(LOCATION_GRAVE) or (c:IsLocation(LOCATION_ONFIELD) and c:IsPosition(POS_FACEUP))) end
+	Duel.Remove(c,POS_FACEUP,REASON_COST)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0xA4) c:IsMonster() and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
