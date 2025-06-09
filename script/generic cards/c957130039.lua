a9295fb9b9ffdbbf25619eae3b4102de46568596
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-06-09 11:31:04 +0200

    added "Attack Commander, Lodrian" (Trick)
    1 Effect Monster + 1 Trap
    If this card is Trick Summoned: You can move this card to another Main Monster Zone, then, Place 4 counters on it. When a monster declares an attack: You can remove 1 counter from this card; negate the attack, and if you do, increase this cards ATK by 300. An Xyz Monster that was Summoned using this card as Xyz Material gains this effect:
    ● This card can make a second attack during each Battle Phase.
    
    [ Artwork by: krysfun - https://www.deviantart.com/krysfun ]

---
+++ script/generic cards/c957130039.lua
@@ -0,0 +1,109 @@
+--Attack Commander, Lodrian
+--Script by Rika
+Duel.LoadScript("proc_trick2.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+    Trick.AddProcedure(c,nil,nil,{{s.tfilter,1,1}},{{s.tfilter2,1,1}})
+    c:EnableCounterPermit(0x0998)
+    --spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_EXTRA) and not e:GetHandler():IsReason(REASON_EFFECT) end)
+	e1:SetTarget(s.addct)
+	e1:SetOperation(s.addc)
+	c:RegisterEffect(e1)
+
+    --Disable attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetCost(s.negcost)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+
+    --effect gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(s.efcon)
+	e3:SetOperation(s.efop)
+	c:RegisterEffect(e3)
+end
+--Trick Summon
+--Monster filter
+function s.tfilter(c)
+	return c:IsType(TYPE_EFFECT)
+end
+--Trap filter
+function s.tfilter2(c)
+	return c:IsTrap()
+end
+
+--e1
+function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x0999 )
+end
+function s.addc(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+        local seq=math.log(Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0),2)
+		if  Duel.MoveSequence(c,seq) then
+            c:AddCounter(0x0998  ,4)
+        end
+	end
+end
+
+--e2
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x0998,1,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,0x0998,1,REASON_COST)
+end
+
+function s.negop(e)
+    c=e:GetHandler()
+    if Duel.NegateAttack() then
+        local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(300)
+		c:RegisterEffect(e1)
+    end
+end
+
+--e3
+function s.efcon(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_XYZ
+end
+
+function s.efop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+		rc:RegisterEffect(e2,true)
+	end
+end
