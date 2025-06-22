df583804190bda513e53efa43955769f956ee083
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-06-23 00:01:12 +0200

    added "Heart of Illusions" (Illusory Shadow)
    1 "Illusory Shadow" monster
    (This card is always treated as an "Illusory Shadow" card.)
    Can be also be Link Summoned by shuffling the listed material from your GY into the Deck. You can only Link Summon "Heart of Illusions" once per turn this way. Cannot be used as Link Material. When a monster effect is activated on your opponent's field or in their hand (Quick Effect): Tribute this face-up card; negate the activation, and if you do, destroy that monster. You can target 1 non-Link Illusion monster you control; equip this card from your field or GY to that target as an Equip Spell that gives it 600 ATK. You can only use this effect of "Heart of Illusions" once per turn.

---
+++ script/Illusory Shadow/c957110018.lua
@@ -0,0 +1,135 @@
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Custom Link Summon Procedure from GY by shuffling into Deck
+    Link.AddProcedure(c,s.matfilter,1,1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetRange(LOCATION_EXTRA)
+    e1:SetCondition(s.linkcon)
+    e1:SetOperation(s.linkop)
+    e1:SetCountLimit(1,id)
+    e1:SetValue(SUMMON_TYPE_LINK)
+    c:RegisterEffect(e1)
+
+    --Negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_F)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.condition)
+	e2:SetCost(Cost.SelfTribute)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+
+    --cannot link material
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+
+    local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_EQUIP)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE|LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.eqtg)
+	e3:SetOperation(s.eqop)
+	c:RegisterEffect(e3)
+end
+
+function s.matfilter(c,lc,sumtype,tp)
+	return c:IsSetCard(0xBBB) and c:IsMonster()
+end
+
+-- Filter für gültige Linkmaterialien (z. B. Effektmonster)
+function s.matfilter(c,lc)
+    return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xBBB) and c:IsAbleToDeckAsCost() and c:IsCanBeLinkMaterial(lc)
+end
+
+-- Linkbedingung: mindestens X Monster im Friedhof
+function s.linkcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    local g=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_GRAVE,0,nil,c)
+    return #g>=1 -- z.B. Link-2
+end
+
+-- Linkoperation: Monster wählen und ins Deck zurückmischen
+function s.linkop(e,tp,eg,ep,ev,re,r,rp,c)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_GRAVE,0,1,1,nil,c) -- z.B. Link-2
+    c:SetMaterial(g)
+    Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_MATERIAL+REASON_LINK)
+end
+
+--e2
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsMonsterEffect() and re:GetHandler():GetControler()~=tp
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetCurrentChain()~=ev+1 then return	end
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+
+
+--e4
+function s.eqfilter(c)
+	return not c:IsLinkMonster() and c:IsRace(RACE_ILLUSION) and c:IsFaceup()
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.eqfilter(chkc) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,tp,0)
+	if c:IsLocation(LOCATION_GRAVE) then
+		Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,tp,0)
+	end
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and tc:IsFaceup() and tc:IsRelateToEffect(e)
+		and tc:IsControler(tp) and Duel.Equip(tp,c,tc) then
+		--Equip limit
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetValue(function(e,c) return c==tc end)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e1)
+		--The equipped monster gains 600 ATK
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetValue(600)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e2)
+	elseif c:IsLocation(LOCATION_MZONE) then
+		Duel.SendtoGrave(c,REASON_RULE,nil,PLAYER_NONE)
+	end
+end
