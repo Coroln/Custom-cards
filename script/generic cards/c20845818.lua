291e4d29efea8ff418cf87fbfb586f99fd2f6e1a
Coroln <76746930+Coroln@users.noreply.github.com>
2025-06-22 20:29:08 +0200

    Added "The Golden Cloak"
    If you control no cards, you can Special Summon this card (from your hand). If this card is Normal or Special Summoned: You can send 1 Level 4 or lower monster from your Deck to the GY, except "The Golden Cloak", and if you do, this card's name becomes the sent monster's, then if the sent monster was a Spellcaster, you can add 1 "Magical Doubling" from your Deck to your hand. You can only use this effect of "The Golden Cloak" once per turn.

---
+++ script/generic cards/c20845818.lua
@@ -0,0 +1,66 @@
+--The Golden Cloak
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon itself from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--name change
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(s.coscost)
+	e2:SetOperation(s.cosoperation)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+s.listed_names={15920609}
+--Special summon itself from hand
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and
+		Duel.GetFieldGroupCount(c:GetControler(),LOCATION_ONFIELD,0)==0
+end
+--name change
+function s.filter(c,code)
+	return c:IsAbleToGrave() and not c:IsOriginalCode(code) and c:IsMonster() and c:IsLevelBelow(4)
+end
+function s.coscost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e:GetHandler():GetCode()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.cosoperation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,c:GetCode())
+	local tc=g:GetFirst()
+	local name=tc:GetCode()
+	if #g==0 or Duel.SendtoGrave(g,REASON_EFFECT)~=g:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(name)
+	c:RegisterEffect(e1)
+	if tc:IsLocation(LOCATION_GRAVE) and tc:IsMonster() and tc:IsRace(RACE_SPELLCASTER) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK,0,1,1,nil,15920609)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
