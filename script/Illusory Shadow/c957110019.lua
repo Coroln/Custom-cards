f9a7860944eb5daded66f0b90f89ce97595359c9
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-06-30 15:11:47 +0200

    Added "Illusory Shadow Night" (Trap)
    Banish 1 "Illusory Shadow Kingdom" you control until the endphase; Destroy all monsters your opponent controls. If this card is in your GY, and you control "Illusory Shadow Kingdom": You can shuffle 1 "Illusory Shadow" monster from you control into the Deck or Extra Deck; Set this card, but banish it when it leaves the field. While this card is banished: You can shuffle it into the Deck, then target 1 "Illusory Shadow" monster in your GY; place it on top of your Deck.
    You can only use 1 "Illusory Shadow Night" effect per turn, and only once that turn.

---
+++ script/Illusory Shadow/c957110019.lua
@@ -0,0 +1,123 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_ATTACK,TIMINGS_CHECK_MONSTER_E|TIMING_ATTACK)
+    e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+    --Set this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_LEAVE_GRAVE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetHintTiming(TIMING_END_PHASE,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMING_BATTLE_END|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCondition(function(e,tp) return Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_SZONE,0,1,nil) end)
+    e2:SetCost(s.setcost)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+
+    --special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TODECK)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_REMOVED)
+    e3:SetCountLimit(1,id)
+	e3:SetCost(Cost.SelfToDeck)
+    e3:SetTarget(s.e3tg)
+	e3:SetOperation(s.e3op)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0xBBB}
+s.listed_names={957110005}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(957110005) and c:IsAbleToRemoveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_ONFIELD,0,1,1,nil):GetFirst()
+	if Duel.Remove(tc,POS_FACEUP,REASON_COST+REASON_TEMPORARY)>0 then
+		tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+		--Return it in the End Phase
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetLabelObject(tc)
+		e1:SetCountLimit(1)
+		e1:SetCondition(function(e) return e:GetLabelObject():HasFlagEffect(id) end)
+		e1:SetOperation(function(e) Duel.ReturnToField(e:GetLabelObject()) end)
+		e1:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	local sg=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	Duel.Destroy(sg,REASON_EFFECT)
+end
+
+--e2
+function s.setfilter(c,tp)
+	return c:IsSetCard(0xBBB) and c:IsFaceup() and c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_ONFIELD,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_ONFIELD,0,1,1,c)
+	Duel.HintSelection(g)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsSSetable() and Duel.SSet(tp,c)>0 then
+		--Banish it when it leaves the field
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3300)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+		c:RegisterEffect(e1)
+	end
+end
+
+--e3
+function s.e3filter(c)
+	return c:IsMonster() and c:IsSetCard(0xBBB) and c:IsAbleToDeck()
+end
+function s.e3tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and s.e3filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.e3filter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.e3filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,g,1,tp,LOCATION_GRAVE)
+end
+function s.e3op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc then
+		Duel.ShuffleDeck(tp)
+		Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)
+		Duel.ConfirmDecktop(tp,1)
+	end
+end
