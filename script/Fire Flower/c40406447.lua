c0f73af6e3cd4340c89f9e4b8346bb2279170d8d
Coroln <76746930+Coroln@users.noreply.github.com>
2025-08-30 15:52:17 +0200

    Added "Grandborg, Silent Guardian of the Forest"
    Cannot be Normal Summoned/Set. Must be Special Summoned by a card effect. If a face-up Plant, Beast, Beast-Warrior, or Insect monster you control is destroyed and sent to the GY, while this card is in your hand or GY: You can pay half of your Life Points; Special Summon this card. While this card is face-up on the field, other Plant, Beast, Beast-Warrior, or Insect monster you control are unaffected by your opponent's Spell effects. Once per turn, if a monster you control would be destroyed by battle, you can reduce this card's ATK by 500 instead. Once per turn, when a monster you control is destroyed by Battle or card effect: You can make this card gain 1000 ATK. You can only control 1 "Grandborg, Silent Guardian of the Forest"

---
+++ script/Fire Flower/c40406447.lua
@@ -0,0 +1,123 @@
+--Grandborg, Silent Guardian of the Forest
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	c:EnableUnsummonable()
+	c:AddMustBeSpecialSummonedByCardEffect()
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--immune
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.targeta)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+	--destroy replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
+	e3:SetOperation(s.repop)
+	c:RegisterEffect(e3)
+	--This card gains 1000 ATK
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_ATKCHANGE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.spcon)
+	e4:SetOperation(s.atkop)
+	c:RegisterEffect(e4)
+end
+s.listed_names={id}
+--spsummon
+function s.cfilter(c,tp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and (c:GetPreviousRaceOnField()&(RACE_BEAST|RACE_BEASTWARRIOR|RACE_PLANT|RACE_INSECT))~=0
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler()) and eg:IsExists(s.cfilter,1,nil,tp) and (r&REASON_DESTROY)~=0
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--immune
+function s.targeta(e,c)
+	return c:IsRace(RACE_BEAST|RACE_BEASTWARRIOR|RACE_PLANT|RACE_INSECT) and chkc~=e:GetHandler() and not c:IsCode(id)
+end
+function s.efilter(e,re)
+	return e:GetHandlerPlayer()~=re:GetOwnerPlayer()
+end
+--destroy replace
+function s.repfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and c:IsReason(REASON_BATTLE) and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAttackAbove(500) and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(-500)
+	c:RegisterEffect(e2)
+end
+--This card gains 1000 ATK
+function s.spfilter(c,tp)
+	return c:IsReason(REASON_DESTROY) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and c:IsMonster()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.spfilter,1,nil,tp)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		--Gains 1000 ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1000)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end
