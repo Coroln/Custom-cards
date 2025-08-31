53e3b5fd4ee8f3f10cdf4ad66168332b1619b39a
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-08-31 09:28:25 +0200

    Added "Chaos Rider"
    If this card is Summoned: You can banish 1 "Chaos" monster from your Deck. If this card is destroyed by battle: You can add 1 "Chaos" monster from your GY to your hand, except "Chaos Rider". You can banish this card from your GY; discard 1 card. You can only use each effect of "Chaos Rider" once per turn.
    
    [ Artwork by: krysfun - https://www.deviantart.com/krysfun ]

---
+++ script/Chaos/c957130056.lua
@@ -0,0 +1,70 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--send to grave
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+
+    --to grave
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_BATTLE_DESTROYED)
+	e4:SetCondition(function(e) return e:GetHandler():IsReason(REASON_BATTLE) end)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+    --e5
+    local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_GRAVE)
+    e5:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk) if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil,REASON_EFFECT) end Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1) return end)
+	e5:SetCost(Cost.SelfBanish)
+	e5:SetOperation(function(e,tp,eg,ep,ev,re,r,rp) Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_EFFECT|REASON_DISCARD,nil,REASON_EFFECT) end)
+	c:RegisterEffect(e5)
+end
+function s.tgfilter(c)
+	return c:IsMonster() and c:IsSetCard(0xcf) and c:IsAbleToRemove()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+        Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+
+--e4
+function s.thfilter(c)
+	return c:IsSetCard(0xcf) and c:IsMonster() and (not c:IsCode(id)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
