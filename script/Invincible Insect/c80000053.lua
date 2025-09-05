c2bc3a92dc0022765f3e8f0ed6939d06784ba6a9
Coroln <76746930+Coroln@users.noreply.github.com>
2025-09-06 00:38:00 +0200

    Reworked Archtype "Invincible Insect"

--- script/Invincible Insect/c80000053.lua
+++ script/Invincible Insect/c80000053.lua
@@ -1,22 +1,27 @@
 --Unschlagbares Insekt Master Beetle
-function c80000053.initial_effect(c)
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c80000053.spcon)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--search
+	--add back to hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(80000053,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetTarget(c80000053.tg)
-	e2:SetOperation(c80000053.op)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.op)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
@@ -24,59 +29,82 @@ function c80000053.initial_effect(c)
 	local e4=e2:Clone()
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e4)
-	--spsummon
+	--Return 1 face-up monster your opponent controls to the hand
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(80000053,1))
-	e5:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_TOHAND)
+	e5:SetType(EFFECT_TYPE_IGNITION)
 	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetCode(EVENT_TO_GRAVE)
-	e5:SetCondition(c80000053.sppcon)
-	e5:SetTarget(c80000053.sptg)
-	e5:SetOperation(c80000053.sppop)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,{id,1})
+	e5:SetCost(s.rthcost)
+	e5:SetTarget(s.rthtg)
+	e5:SetOperation(s.rthop)
 	c:RegisterEffect(e5)
 end
-function c80000053.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x2328) and c:GetCode()~=80000053
+s.listed_names={id}
+--special summon (from hand)
+function s.spcostfilter(c)
+	return c:IsRace(RACE_INSECT) and c:IsMonster() and c:IsDiscardable()
 end
-function c80000053.spcon(e,c)
+function s.spcon(e,c)
 	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and
-		Duel.IsExistingMatchingCard(c80000053.filter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spcostfilter,tp,LOCATION_HAND,0,2,c)
 end
-function c80000053.ffilter(c)
-	return c:IsSetCard(0x2328) and c:IsType(TYPE_MONSTER) and c:GetCode()~=80000053 and c:IsAbleToHand()
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.spcostfilter,tp,LOCATION_HAND,0,c)
+	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,nil,1,tp,HINTMSG_DISCARD,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
 end
-function c80000053.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c80000053.ffilter,tp,LOCATION_REMOVED,0,1,nil) end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_DISCARD|REASON_COST)
+	g:DeleteGroup()
+end
+--add back to hand
+function s.ffilter(c)
+	return c:IsRace(RACE_INSECT) and c:IsType(TYPE_MONSTER) and c:GetCode()~=id and c:IsAbleToHand()
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.ffilter,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
 end
-function c80000053.op(e,tp,eg,ep,ev,re,r,rp)
+function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c80000053.ffilter,tp,LOCATION_REMOVED,0,1,1,nil)
-	if g:GetCount()>0 then
+	local g=Duel.SelectMatchingCard(tp,s.ffilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c80000053.sppcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local crp=c:GetReasonPlayer()
-	return c:IsReason(REASON_DESTROY) and c:GetPreviousControler()==tp and tp~=crp and crp~=PLAYER_NONE
+--Return 1 face-up monster your opponent controls to the hand
+function s.rthcostfilter(c)
+	return c:IsRace(RACE_INSECT) and c:IsDiscardable()
 end
-function c80000053.spfilter(c,e,tp)
-	return c:IsSetCard(0x2328) and not c:IsCode(80000053) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.rthcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rthcostfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.rthcostfilter,1,1,REASON_COST|REASON_DISCARD)
 end
-function c80000053.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c80000053.spfilter(chkc,e,tp) end
-	if chk==0 then return true end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c80000053.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+function s.rthfilter(c)
+	return not c:IsRace(RACE_INSECT) and c:IsFaceup() and c:IsAbleToHand()
 end
-function c80000053.sppop(e,tp,eg,ep,ev,re,r,rp)
+function s.rthtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.rthfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.rthfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.rthfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
+end
+function s.rthop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
--- script/Invincible Insect/c80000058.lua
+++ script/Invincible Insect/c80000058.lua
@@ -1,20 +1,21 @@
 --Unschlagbares Insekt Käfertyrann
-function c80000058.initial_effect(c)
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_INSECT),12,3)
+	Xyz.AddProcedure(c,nil,12,3,s.ovfilter,aux.Stringid(id,0),2,s.xyzop)
 	c:EnableReviveLimit()
-	--pos
+	--Banish as many Special Summoned monsters then attach 1
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_POSITION)
-	e1:SetDescription(aux.Stringid(80000058,0))
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(c80000058.cost)
-	e1:SetTarget(c80000058.target)
-	e1:SetOperation(c80000058.operation)
-	c:RegisterEffect(e1,false,1)
+	e1:SetCountLimit(1)
+	e1:SetCost(Cost.DetachFromSelf(2,2,nil))
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
 	--actlimit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
@@ -22,72 +23,50 @@ function c80000058.initial_effect(c)
 	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,1)
-	e2:SetValue(c80000058.aclimit)
-	e2:SetCondition(c80000058.actcon)
+	e2:SetValue(s.aclimit)
+	e2:SetCondition(s.actcon)
 	c:RegisterEffect(e2)
-	--(5) Pierceing 
-  	local e3=Effect.CreateEffect(c)
-  	e3:SetType(EFFECT_TYPE_SINGLE)
-  	e3:SetCode(EFFECT_PIERCE)
-  	c:RegisterEffect(e3)
-  	local e4=Effect.CreateEffect(c)
-  	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-  	e4:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-  	e4:SetCondition(c80000058.damcon)
-  	e4:SetOperation(c80000058.damop)
-  	c:RegisterEffect(e4)
 end
-function c80000058.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+s.listed_series={0x2328}
+--xyz summon
+function s.ovfilter(c,tp,xyzc)
+	local rk=c:GetRank()
+	return c:IsFaceup() and c:IsType(TYPE_XYZ,xyzc,SUMMON_TYPE_XYZ,tp) and (rk==10 or rk==11)
 end
-function c80000058.filter(c)
-	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
+function s.xyzop(e,tp,chk)
+	if chk==0 then return true end
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|(RESETS_STANDARD_PHASE_END&~RESET_TOFIELD),0,1)
+	return true
 end
-function c80000058.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c80000058.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c80000058.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,c80000058.filter,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+--Banish as many Special Summoned monsters then attach 1
+function s.rmfilter(c)
+	return c:IsSpecialSummoned() and c:IsAbleToRemove() and c:IsMonster()
 end
-function c80000058.operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
-		tc:RegisterFlagEffect(80000058,RESET_EVENT+0x1fc0000+RESET_PHASE+PHASE_END,0,1)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetCountLimit(1)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		e1:SetCondition(c80000058.flipcon)
-		e1:SetOperation(c80000058.flipop)
-		e1:SetLabelObject(tc)
-		Duel.RegisterEffect(e1,tp)
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(id)==0
+		and Duel.IsExistingMatchingCard(s.rmfilter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_MZONE,nil)
+	if #g==0 then return end
+	local c=e:GetHandler()
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)==0 or not c:IsRelateToEffect(e) then return end
+	local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_REMOVED)
+	if #og>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+		local sg=og:Select(tp,1,1,nil)
+		Duel.BreakEffect()
+		Duel.Overlay(c,sg)
 	end
 end
-function c80000058.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	return tc:IsFacedown() and tc:GetFlagEffect(80000058)~=0
-end
-function c80000058.flipop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	Duel.ChangePosition(tc,POS_FACEUP_ATTACK)
-end
-function c80000058.aclimit(e,re,tp)
+--actlimit
+function s.aclimit(e,re,tp)
 	return (re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:IsActiveType(TYPE_MONSTER)) and not re:GetHandler():IsImmuneToEffect(e)
 end
-function c80000058.actcon(e)
+function s.actcon(e)
 	local tp=e:GetHandlerPlayer()
 	local a=Duel.GetAttacker()
 	return a and a:IsSetCard(0x2328) and a:IsControler(tp)
-end
---(5) Pierceing
-function c80000058.damcon(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
-  return ep~=tp and c==Duel.GetAttacker() and Duel.GetAttackTarget() and Duel.GetAttackTarget():IsDefensePos()
-end
-function c80000058.damop(e,tp,eg,ep,ev,re,r,rp)
-  Duel.ChangeBattleDamage(ep,ev*3)
 end
