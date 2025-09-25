e273ac9ff7441576bbee557b8f518fe1dc55f933
Coroln <76746930+Coroln@users.noreply.github.com>
2025-09-25 19:09:34 +0200

    Change 2nd effect of "PM Charizardite X"
    OLD
    This card is used to Ritual Summon "PM Mega Charizard X". You must also Tribute "PM Charizard" from your hand or field whose total Levels equal exactly 7. When "PM Mega Charizard X" is Ritual Summoned by this card's effect: You can banish this card from your GY, then target 1 of the monsters in your GY that was Tributed for that Ritual Summon; return that target to your hand.
    
    NEW
    This card is used to Ritual Summon "PM Mega Charizard X". You must also Tribute "PM Charizard" from your hand or field whose total Level equal exactly 7. During the Standby Phase, if you control a "PM" Ritual Monster: You can banish this card from your GY; Special Summon 1 non-Ritual "PM" monster from your Deck in Defense Position.

--- script/PM/c19962007.lua
+++ script/PM/c19962007.lua
@@ -3,18 +3,22 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	Ritual.AddProcEqual{handler=c,filter=s.ritualfil,matfilter=s.matfilter,nil}
-	--Material Return
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCondition(s.ritual_summon_condition)
-	e2:SetCost(aux.SelfBanishCost)
-	e2:SetTarget(s.ritual_summon_target)
-	e2:SetOperation(s.ritual_summon_operation)
-	c:RegisterEffect(e2)
+	--Special Summon 1 non-Ritual "PM" from your Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE|PHASE_STANDBY)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.spcond)
+	e1:SetCost(Cost.SelfBanish)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
 end
+s.listed_names={19962006}
+s.listed_series={0x7CC}
 s.fit_monster={19962006}
 --Ritual Summon
 function s.ritualfil(c)
@@ -26,26 +30,24 @@ end
 function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
 	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
 end
---Material Return
--- Condition to check if this card is in the GY
-function s.ritual_summon_condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_SZONE)
+--Special Summon 1 non-Ritual "PM" from your Deck
+function s.spcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
--- Targeting a monster tributed for the Ritual Summon
-function s.ritual_summon_target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tributed_filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tributed_filter,tp,LOCATION_GRAVE,0,1,nil) end
-	local g=Duel.SelectTarget(tp,s.tributed_filter,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x7CC) and not c:IsRitualMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_DEFENSE)
 end
--- Filter for tributed monsters
-function s.tributed_filter(c)
-	return c:IsReason(REASON_RITUAL) and c:IsAbleToHand()
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
--- Returning the target to the hand
-function s.ritual_summon_operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_DEFENSE)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
