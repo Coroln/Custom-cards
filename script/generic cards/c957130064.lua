b13327920562fd179ff763bfeecef888dabffa03
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-10-05 11:26:28 +0200

    Added Magical Shining High Dragon (Normal Monster Support)
    2 Normal Monsters
    Cannot be destroyed by card effects. If this card is destroyed by battle: You can draw 1 card, reveal it, and if it is a Normal Monster, you can have your opponent reveal 2 cards from their hand. You can banish this card from your GY; Special Summon 1 Normal Monster from your hand in Defense Position. You cannot activate this effect the turn this card was sent to the GY. You can target 1 Normal Monster you control; return this banished card to the Extra Deck, and if you do, that target gains 500 ATK. You cannot activate this effect the turn this card was banished.

---
+++ script/generic cards/c957130064.lua
@@ -0,0 +1,119 @@
+--Magical Shining High Dragon
+--Scripted by ChatGPT
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Link Summon: 2 Normal Monsters
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_NORMAL),2,2)
+	c:EnableReviveLimit()
+
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(aux.exccon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.hsstg)
+	e2:SetOperation(s.hsop)
+	c:RegisterEffect(e2)
+
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(aux.exccon)
+	e3:SetTarget(s.bufftg)
+	e3:SetOperation(s.buffop)
+	c:RegisterEffect(e3)
+end
+
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
+	local g=Duel.GetOperatedGroup()
+	local dc=g:GetFirst()
+	if not dc then return end
+	Duel.ConfirmCards(1-tp,dc)
+	Duel.ShuffleHand(tp)
+	if dc:IsType(TYPE_NORMAL) then
+		local hc=Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)
+		if hc>=2 then
+			Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_CONFIRM)
+			local sg=Duel.SelectMatchingCard(1-tp,aux.TRUE,1-tp,LOCATION_HAND,0,2,2,nil)
+			if #sg>0 then
+				Duel.ConfirmCards(tp,sg)
+				Duel.ShuffleHand(1-tp)
+			end
+		end
+	end
+end
+
+function s.hssfilter(c,e,tp)
+	return c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.hsstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(s.hssfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.hsop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.hssfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
+
+function s.bufffilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_NORMAL)
+end
+function s.bufftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.bufffilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.bufffilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.bufffilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function s.buffop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not (tc and tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
+	if c:IsRelateToEffect(e) and Duel.SendtoDeck(c,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and c:IsLocation(LOCATION_EXTRA) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
