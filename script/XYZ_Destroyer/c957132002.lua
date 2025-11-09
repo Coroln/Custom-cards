b241ed0a1e931e84ec84369f770835f1a3d1d6fa
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-11-09 12:47:25 +0100

    Bugfix "Xyz-Destroyer Breaking"
    Attack gain/loss is now permanent

--- script/XYZ_Destroyer/c957132002.lua
+++ script/XYZ_Destroyer/c957132002.lua
@@ -1,10 +1,5 @@
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Trap-Effekt:
-    -- Du kannst 1 Material von einem Xyz-Monster auf dem Spielfeld ablösen;
-    -- beschwöre als Spezialbeschwörung 1 "XYZ-Destroyer" Monster von deinem Deck,
-    -- dann kannst du den ATK-Wert jenes Xyz-Monsters um 500 erhöhen oder verringern.
-
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
@@ -19,12 +14,10 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
--- Filter: Face-up Xyz-Monster mit mindestens 1 Material (egal von welcher Seite)
 function s.filterXyz(c)
     return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:GetOverlayCount()>0
 end
 
--- Kosten: Wähle ein Xyz-Monster auf dem Spielfeld und detach 1 Material von diesem
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then 
         return Duel.IsExistingMatchingCard(s.filterXyz,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
@@ -37,12 +30,10 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVEXYZ)
         local sg=og:Select(tp,1,1,nil)
         Duel.SendtoGrave(sg,REASON_COST)
-        -- Speichere das Xyz-Monster, von dem die Kosten bezahlt wurden
         e:SetLabelObject(tc)
     end
 end
 
--- Filter: Suche im Deck nach einem "XYZ-Destroyer" Monster
 function s.spfilter(c,e,tp)
     return c:IsMonster() and c:IsSetCard(0xCCC) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -56,10 +47,8 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    -- Hole das Xyz-Monster, von dem die Kosten bezahlt wurden
     local xyzMonster = e:GetLabelObject()
     if not (xyzMonster) then return end
-    -- Spezialbeschwöre einen "XYZ-Destroyer" Monster aus deinem Deck
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local g = Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
     local spMon = g:GetFirst()
@@ -67,14 +56,13 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
         Duel.SpecialSummon(spMon,0,tp,tp,false,false,POS_FACEUP)
     end
 
-    -- Wähle: Erhöhe oder verringere den ATK-Wert des verwendeten Xyz-Monsters um 500
     local op = Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
     local atkChange = (op==0) and 500 or -500
-
     local e1 = Effect.CreateEffect(e:GetHandler())
+
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetValue(atkChange)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
     xyzMonster:RegisterEffect(e1)
 end
