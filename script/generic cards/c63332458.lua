ea6782c8b2c9a5bf5dbe45efa6ee81c0f22ce30e
Coroln <76746930+Coroln@users.noreply.github.com>
2025-11-16 22:55:34 +0100

    Add files via upload

---
+++ script/generic cards/c63332458.lua
@@ -0,0 +1,30 @@
+--Pendulum Beyond
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+    --place into pendulum zone
+    local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.pentg)
+	e1:SetOperation(s.penop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={id}
+--place into pendulum zone
+function s.penfilter(c)
+	return c:IsType(TYPE_PENDULUM)
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.penfilter,tp,LOCATION_EXTRA,0,1,nil) end
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.penfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
