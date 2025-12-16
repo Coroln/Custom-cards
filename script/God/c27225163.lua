655d2199580fd03a564866b8fcf161ebe351fdf4
Coroln <76746930+Coroln@users.noreply.github.com>
2025-12-16 12:58:55 +0100

    Added "The Devourer"
    Cannot be Normal Summoned/Set. Must be Special Summoned (from your hand) by discarding all other cards from your hand and banishing 15 monsters from your Extra Deck face-down and by Tributing as many monster(s) you control and from your Deck as possible. This cards Summon cannot be negated. Cannot be destroyed by battle or card effects. This card's effects cannot be negated. Cannot be tributed. You can only control 1 "The Devourer". Twice per turn, when your opponent activates a card or effect (Quick Effect): Negate the activation, and if you do, banish that card. During your opponents End Phase: You win the Duel.

---
+++ script/God/c27225163.lua
@@ -0,0 +1,122 @@
+--The Devourer
+--Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	c:SetUniqueOnField(1,0,id)
+	--Special Summon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+	--Special Summon procedure
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e2)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_NEGATE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	c:RegisterEffect(e4)
+	--cannot release
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_NEGATE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
+	local e5a=e5:Clone()
+	e5a:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e5a)
+	--Negate
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,0))
+	e6:SetCategory(CATEGORY_DISABLE)
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetCode(EVENT_CHAINING)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_NEGATE)
+	e6:SetCountLimit(2)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.discon)
+	e6:SetTarget(s.distg)
+	e6:SetOperation(s.disop)
+	c:RegisterEffect(e6)
+	--win
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY+EFFECT_FLAG_UNCOPYABLE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetCode(EVENT_PHASE+PHASE_END)
+	e7:SetCountLimit(1)
+	e7:SetCondition(s.wincon)
+	e7:SetOperation(s.winop)
+	c:RegisterEffect(e7)
+end
+--special summon (from hand)
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	if Duel.GetMatchingGroupCount(Card.IsMonster,tp,LOCATION_EXTRA,0,nil)<15 then
+		return false
+	end
+	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)<=1 then
+		return false
+	end
+	return Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_MZONE+LOCATION_DECK,0,1,nil)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local hg=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	hg:RemoveCard(c)
+	Duel.SendtoGrave(hg,REASON_COST+REASON_DISCARD)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local edg=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,LOCATION_EXTRA,0,15,15,nil)
+	Duel.Remove(edg,POS_FACEDOWN,REASON_COST)
+	local tg=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_MZONE+LOCATION_DECK,0,nil)
+	if #tg>0 then
+		Duel.SendtoGrave(tg,REASON_COST+REASON_RELEASE)
+	end
+end
+--Negate
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainDisablable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.NegateEffect(ev) then
+		if re:GetHandler():IsRelateToEffect(re) then
+			Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
+        end
+    end
+end
+--win
+function s.wincon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsTurnPlayer(tp) and Duel.GetTurnCount()>e:GetHandler():GetTurnID()
+end
+function s.winop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Win(tp,0x97)
+end
