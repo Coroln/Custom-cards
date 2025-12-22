a003c978f58a8c1cbb7774af768bb0a3ee10064f
Coroln <76746930+Coroln@users.noreply.github.com>
2025-12-22 10:29:45 +0100

    Update c25126804.lua

--- script/Fantasy/c25126804.lua
+++ script/Fantasy/c25126804.lua
@@ -60,7 +60,7 @@ function s.thfilter(c)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
