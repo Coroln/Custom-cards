f18f62d0bcb3d7d3f2655dc38b023c6dcbd27b80
Rika <reinhardtrichard77@gmail.com>
2026-02-08 14:27:44 +0100

    Added Element Jewel Set
    Aquatic Jewel
    Darkness Jewel
    Shattering Jewel
    Storming Jewel
    Flare Jewel
    Shining Jewel

---
+++ script/generic cards/c957130105.lua
@@ -0,0 +1,62 @@
+--Shining Jewel
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+	--GY effect: Tribute 1 LIGHT monster; add this card to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+
+--Activation operation
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--ATK boost for all LIGHT monsters for the rest of the Duel
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.atktg)
+	e1:SetValue(200)
+	e1:SetReset(RESET_PHASE+PHASE_END,1)
+	Duel.RegisterEffect(e1,tp)
+	e1:SetReset(0)
+end
+
+function s.atktg(e,c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+
+--GY effect cost
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.CheckReleaseGroupCost(tp,Card.IsAttribute,1,false,nil,nil,ATTRIBUTE_LIGHT)
+	end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsAttribute,1,1,false,nil,nil,ATTRIBUTE_LIGHT)
+	Duel.Release(g,REASON_COST)
+end
+
+--GY effect target
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+
+--GY effect operation
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
+end
