7a1ed542851153e5fe0deeed961d3d7cbc68f290
Coroln <76746930+Coroln@users.noreply.github.com>
2026-04-29 17:33:11 +0200

    Added "The Great Ocean"
    Could you never decide which "Umi" you want to play? not any more now you can just play all of them as one card :D

---
+++ script/c67716561.lua
@@ -0,0 +1,320 @@
+--The Great Ocean
+--Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Atk (1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+	--Def (1)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	--level (1)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_UPDATE_LEVEL)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(LOCATION_HAND|LOCATION_MZONE,LOCATION_HAND|LOCATION_MZONE)
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
+	e4:SetValue(-1)
+	c:RegisterEffect(e4)
+	--Atk (2)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e5:SetValue(400)
+	c:RegisterEffect(e5)
+	--Def (2)
+	local e6=e5:Clone()
+	e6:SetCode(EFFECT_UPDATE_DEFENSE)
+	e6:SetValue(800)
+	c:RegisterEffect(e6)
+	--Increase Level (1)
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,1))
+	e7:SetCategory(CATEGORY_LVCHANGE)
+	e7:SetType(EFFECT_TYPE_IGNITION)
+	e7:SetRange(LOCATION_FZONE)
+	e7:SetCountLimit(1)
+	e7:SetTarget(s.lvtg)
+	e7:SetOperation(s.lvop)
+	c:RegisterEffect(e7)
+	--increase level (2)
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,2))
+	e8:SetType(EFFECT_TYPE_IGNITION)
+	e8:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e8:SetRange(LOCATION_FZONE)
+	e8:SetCountLimit(1)
+	e8:SetTarget(s.lvtg2)
+	e8:SetOperation(s.lvop2)
+	c:RegisterEffect(e8)
+	--banish
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,3))
+	e9:SetCategory(CATEGORY_REMOVE)
+	e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e9:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e9:SetProperty(EFFECT_FLAG_DELAY)
+	e9:SetRange(LOCATION_FZONE)
+	e9:SetCountLimit(1)
+	e9:SetCondition(s.rmcon)
+	e9:SetTarget(s.rmtg)
+	e9:SetOperation(s.rmop)
+	c:RegisterEffect(e9)
+	--Search 1 "Phantasm Spiral" card
+	local e10=Effect.CreateEffect(c)
+	e10:SetDescription(aux.Stringid(id,4))
+	e10:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e10:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e10:SetProperty(EFFECT_FLAG_DELAY)
+	e10:SetCode(EVENT_SUMMON_SUCCESS)
+	e10:SetRange(LOCATION_FZONE)
+	e10:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e10:SetCondition(s.thcon)
+	e10:SetTarget(s.thtg)
+	e10:SetOperation(s.thop)
+	c:RegisterEffect(e10)
+	local e11=e10:Clone()
+	e11:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e11)
+	--Special Summon 1 "Phantasm Spiral Token"
+	local e12=Effect.CreateEffect(c)
+	e12:SetDescription(aux.Stringid(id,5))
+	e12:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e12:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e12:SetProperty(EFFECT_FLAG_DELAY)
+	e12:SetCode(EVENT_CHAINING)
+	e12:SetRange(LOCATION_FZONE)
+	e12:SetCondition(s.spcon)
+	e12:SetTarget(s.sptg)
+	e12:SetOperation(s.spop)
+	c:RegisterEffect(e12)
+	--special summon "Ocean Ruler"
+	local e13=Effect.CreateEffect(c)
+	e13:SetDescription(aux.Stringid(id,6))
+	e13:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e13:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e13:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e13:SetRange(LOCATION_FZONE)
+	e13:SetCode(EVENT_TO_GRAVE)
+	e13:SetCountLimit(1)
+	e13:SetCondition(s.condition2)
+	e13:SetTarget(s.target2)
+	e13:SetOperation(s.operation2)
+	c:RegisterEffect(e13)
+end
+s.listed_names={CARD_UMI}
+--activate
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_DECK,0,nil,ATTRIBUTE_WATER)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+		local tc=g:Select(tp,1,1,nil):GetFirst()
+		Duel.ShuffleDeck(tp)
+		Duel.MoveSequence(tc,0)
+		Duel.ConfirmDecktop(tp,1)
+	end
+end
+--Atk and def (1)
+function s.val(e,c)
+	local r=c:GetRace()
+	if r&(RACE_FISH|RACE_SEASERPENT|RACE_THUNDER|RACE_AQUA)>0 then
+		return 200
+	elseif r&(RACE_MACHINE|RACE_PYRO)>0 then
+		return -200
+	else
+		return 0
+	end
+end
+--Increase Level (1)
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER) and c:HasLevel()
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local lv=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_WATER),tp,LOCATION_MZONE,0,nil)
+	if lv==0 then return end
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local _,fid=g:GetMaxGroup(Card.GetFieldID)
+	local c=e:GetHandler()
+	--Increase Level
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.efftg)
+	e1:SetValue(lv)
+	e1:SetLabel(fid)
+	e1:SetReset(RESETS_STANDARD_DISABLE_PHASE_END)
+	c:RegisterEffect(e1)
+end
+function s.efftg(e,c)
+	return c:GetFieldID()<=e:GetLabel() and c:IsAttribute(ATTRIBUTE_WATER) and c:HasLevel()
+end
+--increase level (2)
+function s.lvfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER) and c:IsLevelAbove(1)
+end
+function s.lvtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.lvfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.lvop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,7))
+		local lv=Duel.AnnounceLevel(tp,1,2)
+		local e1=Effect.CreateEffect(c)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		e1:SetValue(lv)
+		tc:RegisterEffect(e1)
+	end
+end
+--banish
+function s.rmcfilter(c,tp)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_SYNCHRO) and c:GetSummonPlayer()==tp
+end
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase() and Duel.IsTurnPlayer(tp) and eg:IsExists(s.rmcfilter,1,nil,tp)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_HAND)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local hg=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if Duel.IsPlayerAffectedByEffect(1-tp,30459350) or #hg==0 then return end
+	Duel.ConfirmCards(tp,hg)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=hg:Select(tp,1,1,nil)
+	local tc=g:GetFirst()
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	Duel.ShuffleHand(1-tp)
+	local c=e:GetHandler()
+	local fid=c:GetFieldID()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetLabel(fid)
+	e1:SetLabelObject(tc)
+	e1:SetCondition(s.retcon)
+	e1:SetOperation(s.retop)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1,fid)
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(id)==e:GetLabel() then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.SendtoHand(tc,nil,REASON_EFFECT)
+end
+--Search 1 "Phantasm Spiral" card
+function s.counterfilter(c)
+	return not c:IsType(TYPE_EFFECT)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	return #eg==1 and tc:IsSummonPlayer(tp) and tc:IsFaceup() and tc:IsType(TYPE_NORMAL)
+end
+function s.thfilter(c)
+	return c:IsSetCard(SET_PHANTASM_SPIRAL) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+--Special Summon 1 "Phantasm Spiral Token"
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp
+end
+function s.tknfilter(c)
+	return c:IsType(TYPE_TOKEN) or c:IsOriginalType(TYPE_TOKEN)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_PHANTASM_SPIRAL,TYPES_TOKEN,2000,2000,6,RACE_WYRM,ATTRIBUTE_WATER)
+		and e:GetHandler():GetFlagEffect(2819435)==0
+		and not Duel.IsExistingMatchingCard(s.tknfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	e:GetHandler():RegisterFlagEffect(2819435,RESET_EVENT|RESETS_STANDARD|RESET_CHAIN,0,1)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_PHANTASM_SPIRAL,TYPES_TOKEN,2000,2000,6,RACE_WYRM,ATTRIBUTE_WATER) then
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--special summon
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	local lv=0
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:IsReason(REASON_DESTROY) and tc:IsSetCard(0x1864) and tc:IsMonster() then
+			local tlv=tc:GetLevel()
+			if tlv>lv then lv=tlv end
+		end
+		tc=eg:GetNext()
+	end
+	if lv>0 then e:SetLabel(lv) end
+	return lv>0
+end
+function s.spfilter(c,e,tp,lv)
+	return c:IsLevelBelow(lv) and c:IsSetCard(0x1864) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) and not e:GetHandler():IsStatus(STATUS_CHAINING)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,e:GetLabel()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,e:GetLabel())
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
