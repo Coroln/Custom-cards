f588b0e4cab6aa6d4484b9a0d23fb0f671678bcb
Coroln <76746930+Coroln@users.noreply.github.com>
2026-06-13 14:09:54 +0200

    Added "Exiled HERO Vision Breaker"
    (This card is also treated as a "Vision HERO" card.)
    If this card is Summoned: You can banish 1 "HERO" monster from your hand; Special Summon 1 Warrior monster from your hand or GY, but banish it during End Phase of the next turn. If this card is banished from the hand: You can place this card into your Spell & Trap Zone as a face-up Continuous Trap. During the Main Phase, if this card is treated as a Continuous Trap: You can banish 1 "HERO" monster from your hand or GY; Draw 1 card. You can only use each effect of "Exiled HERO Vision Breaker" once per turn. If this card would be sent to the GY, it is banished instead.

---
+++ script/Exiled HERO/c81163831.lua
@@ -0,0 +1,149 @@
+--Exiled HERO Vision Breaker
+--Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.rmcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--place to S&T Zone
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCode(EVENT_REMOVE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCondition(s.pcon)
+	e4:SetTarget(s.ptg)
+	e4:SetOperation(s.pop)
+	c:RegisterEffect(e4)
+	--draw
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,{id,2})
+	e5:SetCondition(s.drawcon)
+	e5:SetCost(s.drawcost)
+	e5:SetTarget(s.drawtg)
+	e5:SetOperation(s.drawop)
+	c:RegisterEffect(e5)
+	--redirect
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetCondition(s.recon)
+	e6:SetValue(LOCATION_REMOVED)
+	c:RegisterEffect(e6)
+end
+--Special Summon
+function s.cfilter(c,tp)
+	return c:IsAbleToRemoveAsCost() and c:IsSetCard(0x8)
+end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
+	e:SetLabel(g:GetFirst():GetRace())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_WARRIOR) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE|LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE|LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE|LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_PHASE+PHASE_END)
+		e2:SetCountLimit(1)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetLabel(Duel.GetTurnCount()+1)
+		e2:SetLabelObject(tc)
+		e2:SetCondition(s.descon)
+		e2:SetOperation(s.desop)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(id)~=0 then
+		return Duel.GetTurnCount()==e:GetLabel()
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+end
+--place to S&T Zone
+function s.pcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_HAND)
+end
+function s.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.pop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local c=e:GetHandler()
+	Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCode(EFFECT_CHANGE_TYPE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
+	e1:SetValue(TYPE_TRAP|TYPE_CONTINUOUS)
+	c:RegisterEffect(e1)
+end
+--draw
+function s.drawcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase() and e:GetHandler():IsContinuousTrap()
+end
+function s.drawfilter(c)
+	return c:IsSetCard(0x8) and c:IsMonster() and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.drawcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.drawfilter,tp,LOCATION_HAND+LOCATION_MZONE|LOCATION_GRAVE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.drawfilter,tp,LOCATION_HAND+LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil,tp)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.drawtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drawop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+--redirect
+function s.recon(e)
+	return e:GetHandler()
+end
