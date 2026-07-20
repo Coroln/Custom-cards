421b17bbcedb326ce1d8a483c5cd35af1ca04794
Coroln <76746930+Coroln@users.noreply.github.com>
2026-07-20 12:29:21 +0200

    made an errata for "PM Copycat"
    new effect:
    Declare 1 "PM" Monster Card name; Special Summon this card as a Normal Monster with the same name, Level and type as the named monster (LIGHT/ATK 1000/DEF 1000). (This is still a Trap.) If this card is sent to the GY as material for the Trick Summon of a "PM" monster: You can banish 1 "PM" Ritual Spell from your hand or GY; this effect becomes that Spell's effect when that card is activated. You can only use this effect of "PM Copycat" once per turn.

--- script/UB PM/c19962044.lua
+++ script/UB PM/c19962044.lua
@@ -7,7 +7,6 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.spcost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -24,31 +23,22 @@ function s.initial_effect(c)
 end
 s.listed_series={0x7CC}
 --Special Summon
-function s.costfilter(c)
-	return c:IsSetCard(0x7CC) and c:HasLevel() and not c:IsPublic()
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local rc=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
-	Duel.ConfirmCards(1-tp,rc)
-	Duel.ShuffleHand(tp)
-	e:SetLabelObject(rc)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ec=e:GetLabelObject()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-	ec:CreateEffectRelation(e)
+	local code=e:GetHandler():GetCode()
+	s.announce_filter={0x7CC,OPCODE_ISSETCARD,code,OPCODE_ISCODE,OPCODE_NOT,OPCODE_AND,TYPE_MONSTER,OPCODE_ISTYPE,OPCODE_AND}
+	local ac=Duel.AnnounceCard(tp,table.unpack(s.announce_filter))
+	Duel.SetTargetParam(ac)
+	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local ec=e:GetLabelObject()
-	if not ec:IsRelateToEffect(e) then return end
+	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,ec:GetCode(),0,TYPE_MONSTER|TYPE_NORMAL,0,0,ec:GetLevel(),ec:GetRace(),ATTRIBUTE_LIGHT) then return end
-	c:AddMonsterAttribute(TYPE_NORMAL|TYPE_TRAP,ATTRIBUTE_LIGHT,ec:GetRace(),ec:GetLevel(),1000,1000)
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,ac,0,TYPE_MONSTER|TYPE_NORMAL,0,0,Duel.GetCardRaceFromCode(ac),Duel.GetCardLevelFromCode(ac),ATTRIBUTE_LIGHT) then return end
+	c:AddMonsterAttribute(TYPE_NORMAL|TYPE_TRAP,ATTRIBUTE_LIGHT,Duel.GetCardRaceFromCode(ac),Duel.GetCardLevelFromCode(ac),1000,1000)
 	Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
 	c:AddMonsterAttributeComplete()
 	--change code
@@ -56,7 +46,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CHANGE_CODE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetValue(ec:GetCode())
+	e1:SetValue(ac)
 	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 	c:RegisterEffect(e1,true)
 	Duel.SpecialSummonComplete()
