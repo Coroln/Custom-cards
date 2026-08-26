c3632b1c77e7885832d026b2d11bb2cbb7069bc9
Coroln <76746930+Coroln@users.noreply.github.com>
2026-08-26 17:30:00 +0200

    Update c30566774.lua

--- script/insect/c30566774.lua
+++ script/insect/c30566774.lua
@@ -34,7 +34,17 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 			and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 			Duel.BreakEffect()
-			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+			if Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) then
+				--Banish it when it leaves the field
+				local e1=Effect.CreateEffect(c)
+				e1:SetDescription(3300)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+				e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+				e1:SetValue(LOCATION_REMOVED)
+				e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+				c:RegisterEffect(e1)
+			end
 		end
 	end
 end
