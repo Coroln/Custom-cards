55e160f6664995d5f94e48f143fd6c3ff19e44be
Deprispinne <132221577+Deprispinne@users.noreply.github.com>
2026-09-10 19:35:27 +0200

    Update c30000378.lua

--- script/CraftbleCCS/c30000378.lua
+++ script/CraftbleCCS/c30000378.lua
@@ -1,57 +1,55 @@
-local s, id = GetID()
+local s,id=GetID()
 function s.initial_effect(c)
-    local e0 = Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e0:SetCode(EFFECT_UPDATE_ATTACK)
-    e0:SetRange(LOCATION_MZONE)
-    e0:SetValue(s.val)
-    c:RegisterEffect(e0)
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_COUNTER)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(s.cointg)
-	e1:SetOperation(s.coinop)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-    --Count coins
-    aux.GlobalCheck(s, function()
-        local ge1 = Effect.CreateEffect(c)
-        ge1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-        ge1:SetCode(EVENT_TOSS_COIN)
-        ge1:SetCondition(s.coincon)
-        ge1:SetOperation(s.checkop)
-        Duel.RegisterEffect(ge1, 0)
-    end)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.op)
 end
-
-function s.val(e)
-    local tails = Duel.GetFlagEffectLabel(0, id)
-    if not tails then
-        return 0
-    end
-    return tails * 200
+function s.filter(c)
+	return (c:IsSetCard(SET_CYBER) or c:IsSetCard(SET_CYBERNETIC)) and c:IsSpellTrap() and c:IsAbleToHand()
 end
-
-function s.coincon(e, tp, eg, ep, ev, re, r, rp)
-    return re:GetCode()~=EVENT_TOSS_COIN_NEGATE
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
-
-function s.checkop(e, tp, eg, ep, ev, re, r, rp)
-    if ev == 65537 then
-        if not Duel.GetFlagEffectLabel(0, id) then
-            Duel.RegisterFlagEffect(0, id, 0, 0, 0, 1)
-        else
-            Duel.SetFlagEffectLabel(0, id, Duel.GetFlagEffectLabel(0, id) + 1)
-        end
-    end
+function s.cfilter(c)
+	return c:IsFaceup() and (c:IsCode(CARD_CYBER_DRAGON) or (c:IsType(TYPE_FUSION) and c:ListsCodeAsMaterial(CARD_CYBER_DRAGON))) and c:IsAbleToGraveAsCost()
 end
-function s.cointg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function s.coinop(e,tp,eg,ep,ev,re,r,rp)
-	local res=Duel.TossCoin(tp,1)
-	if res==COIN_HEADS then Duel.Recover(tp,1000,REASON_EFFECT)
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end
